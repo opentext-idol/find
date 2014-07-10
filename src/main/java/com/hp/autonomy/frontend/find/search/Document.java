273d07fc02cb8c4cd8e438433eb8f9f13fe5ddfa
ivanoaly <alyona.ivanova@hp.com>
2014-07-10 12:02:13 +0100

    Add document entity classes.

--- src/main/java/com/hp/autonomy/frontend/find/search/Document.java
+++ src/main/java/com/hp/autonomy/frontend/find/search/Document.java
@@ -1,11 +1,46 @@
 package com.hp.autonomy.frontend.find.search;
 
-/**
- * Created with IntelliJ IDEA.
- * User: ivanoaly
- * Date: 10/07/14
- * Time: 10:44
- * To change this template use File | Settings | File Templates.
- */
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonPOJOBuilder;
+import lombok.Data;
+import lombok.Setter;
+
+import java.util.List;
+
+@Data
+@JsonDeserialize(builder = Document.Builder.class)
 public class Document {
+
+    private final String reference;
+    private final double weight;
+    private final List<String> links;
+    private final String index;
+    private final String title;
+    private final String summary;
+
+    private Document(final String reference, final double weight, final List<String> links, final String index, final String title, final String summary) {
+        this.reference = reference;
+        this.weight = weight;
+        this.links = links;
+        this.index = index;
+        this.title = title;
+        this.summary = summary;
+    }
+
+    @Setter
+    @JsonPOJOBuilder(withPrefix = "set")
+    public static class Builder {
+
+        private String reference;
+        private double weight;
+        private List<String> links;
+        private String index;
+        private String title;
+        private String summary;
+
+        public Document build() {
+            return new Document(reference, weight, links, index, title, summary);
+        }
+
+    }
 }
--- src/main/java/com/hp/autonomy/frontend/find/search/Documents.java
+++ src/main/java/com/hp/autonomy/frontend/find/search/Documents.java
@@ -1,11 +1,31 @@
 package com.hp.autonomy.frontend.find.search;
 
-/**
- * Created with IntelliJ IDEA.
- * User: ivanoaly
- * Date: 10/07/14
- * Time: 10:44
- * To change this template use File | Settings | File Templates.
- */
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonPOJOBuilder;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Setter;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@JsonDeserialize(builder = Documents.Builder.class)
 public class Documents {
+
+    private final List<Document> documents;
+
+    @Setter
+    @JsonPOJOBuilder(withPrefix = "set")
+    public static class Builder {
+
+        private List<Document> documents;
+
+        public Documents build() {
+            return new Documents(documents);
+        }
+
+    }
+
 }
