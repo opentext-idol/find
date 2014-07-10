ef199758c73b64836e17bd353ca38e986ebb5895
ivanoaly <alyona.ivanova@hp.com>
2014-07-10 14:47:12 +0100

    Restructure request mapping

--- src/main/java/com/hp/autonomy/frontend/find/search/DocumentsController.java
+++ src/main/java/com/hp/autonomy/frontend/find/search/DocumentsController.java
@@ -2,9 +2,9 @@ package com.hp.autonomy.frontend.find.search;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
@@ -16,10 +16,10 @@ public class DocumentsController {
     @Autowired
     private DocumentsService documentsService;
 
-    @RequestMapping(value = "/text/{text}", method = RequestMethod.GET)
+    @RequestMapping(method = RequestMethod.GET)
     @ResponseBody
     public List<Document> query(
-        @PathVariable("text") final String text
+        @RequestParam("text") final String text
     ) {
         return documentsService.queryTextIndex(text);
     }
