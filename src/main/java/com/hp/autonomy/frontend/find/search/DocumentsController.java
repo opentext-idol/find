1b2f2f0e52c8a0e773bd98a9e786691e3abda3c0
ivanoaly <alyona.ivanova@hp.com>
2014-07-10 12:08:01 +0100

    Implement DocumentsController.

--- src/main/java/com/hp/autonomy/frontend/find/search/DocumentsController.java
+++ src/main/java/com/hp/autonomy/frontend/find/search/DocumentsController.java
@@ -1,7 +1,27 @@
 package com.hp.autonomy.frontend.find.search;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/search/query-text-index")
 public class DocumentsController {
 
+    @Autowired
+    private DocumentsService documentsService;
 
+    @RequestMapping(value = "/text/{text}", method = RequestMethod.GET)
+    @ResponseBody
+    public List<Document> query(
+        @PathVariable("text") final String text
+    ) {
+        return documentsService.queryTextIndex(text);
+    }
 
 }
