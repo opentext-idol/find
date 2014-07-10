8160d42332f57100f99aaef1435841a3dc853384
ivanoaly <alyona.ivanova@hp.com>
2014-07-10 11:47:02 +0100

    Document placeholders.

---
+++ src/main/java/com/hp/autonomy/frontend/find/search/Document.java
@@ -0,0 +1,11 @@
+package com.hp.autonomy.frontend.find.search;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: ivanoaly
+ * Date: 10/07/14
+ * Time: 10:44
+ * To change this template use File | Settings | File Templates.
+ */
+public class Document {
+}
