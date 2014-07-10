3494dcdf5bd9f71d33f85e5f28991210261283c2
ivanoaly <alyona.ivanova@hp.com>
2014-07-10 12:04:14 +0100

    Implement DocumentsService

--- src/main/java/com/hp/autonomy/frontend/find/search/DocumentsService.java
+++ src/main/java/com/hp/autonomy/frontend/find/search/DocumentsService.java
@@ -1,11 +1,9 @@
 package com.hp.autonomy.frontend.find.search;
 
-/**
- * Created with IntelliJ IDEA.
- * User: ivanoaly
- * Date: 10/07/14
- * Time: 11:49
- * To change this template use File | Settings | File Templates.
- */
-public class DocumentsService {
+import java.util.List;
+
+public interface DocumentsService {
+
+    List<Document> queryTextIndex(String text);
+
 }
