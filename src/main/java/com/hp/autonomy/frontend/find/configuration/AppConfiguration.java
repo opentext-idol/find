0f7402b0661a9eb00e12389d923aa179f3faccdd
Brian Beckett <brian.beckett@hp.com>
2015-04-15 11:28:20 +0100

    Remove RestTemplate and DamnHandy URI templates as they have now been completely replaced by the Java IOD client

--- src/main/java/com/hp/autonomy/frontend/find/configuration/AppConfiguration.java
+++ src/main/java/com/hp/autonomy/frontend/find/configuration/AppConfiguration.java
@@ -21,33 +21,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.ClientHttpRequestFactory;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
-import org.springframework.web.client.RestTemplate;
 import retrofit.RestAdapter;
 import retrofit.client.ApacheClient;
 import retrofit.converter.JacksonConverter;
 
 @Configuration
 public class AppConfiguration {
-
-    @Bean
-    public RestTemplate restTemplate() {
-        final HttpClientBuilder httpClientBuilder = HttpClientBuilder.create();
-
-        httpClientBuilder
-            .setMaxConnPerRoute(20)
-            .setMaxConnTotal(120);
-
-        final String proxyHost = System.getProperty("find.https.proxyHost");
-
-        if(proxyHost != null) {
-            final Integer proxyPort = Integer.valueOf(System.getProperty("find.https.proxyPort", "80"));
-            httpClientBuilder.setProxy(new HttpHost(proxyHost, proxyPort));
-        }
-
-        final ClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory(httpClientBuilder.build());
-
-        return new RestTemplate(requestFactory);
-    }
 
     @Bean(name = "dispatcherObjectMapper")
     public ObjectMapper dispatcherObjectMapper() {
