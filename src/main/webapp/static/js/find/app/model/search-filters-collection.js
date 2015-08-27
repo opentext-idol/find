ec4ea4bcd3b15d39c4bf81eaf554fe86694e874f
Matthew Gordon <matthew.gordon2@hp.com>
2015-08-27 12:58:56 +0100

    Updated search filters collection comments to reflect changes. [rev: none]

--- src/main/webapp/static/js/find/app/model/search-filters-collection.js
+++ src/main/webapp/static/js/find/app/model/search-filters-collection.js
@@ -47,12 +47,10 @@ define([
         });
     }
 
-    // TODO: Update comments
-    // This collection backs the search filters display view. It monitors the query model and selected indexes collection
-    // and creates/removes it's own models when they change.
+    // This collection backs the search filters display view. It monitors the query model and selected parmaetric values
+    // collection and creates/removes it's own models when they change.
     // When a dates filter model is removed, it updates the appropriate request model attribute with a null value. However,
-    // this currently can't be done for the selected databases or parametric filters because the databases and parametric
-    // views aren't backed by a collection.
+    // this currently can't be done for the selected databases because the databases view isn't backed by a collection.
     return Backbone.Collection.extend({
         initialize: function(models, options) {
             this.queryModel = options.queryModel;
