655654eeb2969ca17fd781ef0b421716f89f03b3
Matthew Gordon <matthew.gordon2@hp.com>
2015-08-28 11:20:20 +0100

    Added copyright to new files. [rev: alex.scown]

--- src/main/java/com/hp/autonomy/frontend/find/similar/SimilarDocumentsServiceImpl.java
+++ src/main/java/com/hp/autonomy/frontend/find/similar/SimilarDocumentsServiceImpl.java
@@ -1,3 +1,8 @@
+/*
+ * Copyright 2015 Hewlett-Packard Development Company, L.P.
+ * Licensed under the MIT License (the "License"); you may not use this file except in compliance with the License.
+ */
+
 package com.hp.autonomy.frontend.find.similar;
 
 import com.hp.autonomy.hod.client.api.resource.ResourceIdentifier;
