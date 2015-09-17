3d33b1bfb0cc0a4028ae5e5528e02ea3803553f5
Fred Collman <frederick.collman@hpe.com>
2015-09-17 17:06:44 +0100

    Update FormInput to be a wrapper rather than extending AppElement

--- mockui/src/main/java/com/autonomy/abc/selenium/element/FormInput.java
+++ mockui/src/main/java/com/autonomy/abc/selenium/element/FormInput.java
@@ -4,15 +4,40 @@ import com.hp.autonomy.frontend.selenium.util.AppElement;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
-public class FormInput extends AppElement {
+public class FormInput {
+    private AppElement element;
+
+    public FormInput(AppElement element) {
+        this.element = element;
+    }
 
     public FormInput(WebElement element, WebDriver driver) {
-        super(element, driver);
+        this(new AppElement(element, driver));
+    }
+
+    public void clear() {
+        element.clear();
+    }
+
+    public void submit() {
+        element.submit();
+    }
+
+    public String getValue() {
+        return element.getText();
+    }
+
+    public void setValue(String value) {
+        element.clear();
+        element.sendKeys(value);
+    }
+
+    public WebElement getElement() {
+        return element;
     }
 
     public void setAndSubmit(String value) {
-        clear();
-        sendKeys(value);
-        submit();
+        setValue(value);
+        element.submit();
     }
 }
