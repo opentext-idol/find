ba972c0a7b1f83b0bc9ac0d4b86bd580ad47bccc
Fred Collman <frederick.collman@hpe.com>
2015-09-29 17:58:42 +0100

    Refactor interface Wizard to abstract class

--- mockui/src/main/java/com/autonomy/abc/selenium/actions/wizard/Wizard.java
+++ mockui/src/main/java/com/autonomy/abc/selenium/actions/wizard/Wizard.java
@@ -2,15 +2,50 @@ package com.autonomy.abc.selenium.actions.wizard;
 
 import com.autonomy.abc.selenium.actions.Action;
 
+import java.util.ArrayList;
 import java.util.List;
 
-public interface Wizard extends Action {
-    List<WizardStep> getSteps();
+public abstract class Wizard implements Action {
+    private List<WizardStep> steps;
+    private int currentStep = 0;
 
-    WizardStep getCurrentStep();
+    public Wizard() {
+        steps = new ArrayList<>();
+    }
 
-    void next();
+    public Wizard(List<WizardStep> steps) {
+        this.steps = steps;
+    }
 
-    void cancel();
+    public List<WizardStep> getSteps() {
+        return steps;
+    }
+
+    protected void add(WizardStep wizardStep) {
+        steps.add(wizardStep);
+    }
+
+    public WizardStep getCurrentStep() {
+        return steps.get(currentStep);
+    }
+
+    public boolean onFinalStep() {
+        return currentStep == steps.size() - 1;
+    }
+
+    protected void incrementStep() {
+        currentStep++;
+    }
+
+    public abstract void next();
+
+    public abstract void cancel();
+
+    public void apply() {
+        for (WizardStep wizardStep : steps) {
+            wizardStep.apply();
+            next();
+        }
+    }
 
 }
