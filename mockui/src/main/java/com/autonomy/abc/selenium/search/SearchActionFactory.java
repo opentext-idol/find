5e0af24244045e12aa92195ac867c49782c0f5ef
Fred Collman <frederick.collman@hpe.com>
2015-10-12 16:41:45 +0100

    Refactor SearchActionFactory to use a single AppBody (otherwise cannot makeSearch when on Find)

--- mockui/src/main/java/com/autonomy/abc/selenium/search/SearchActionFactory.java
+++ mockui/src/main/java/com/autonomy/abc/selenium/search/SearchActionFactory.java
@@ -2,14 +2,19 @@ package com.autonomy.abc.selenium.search;
 
 import com.autonomy.abc.selenium.actions.ActionFactory;
 import com.autonomy.abc.selenium.config.Application;
+import com.autonomy.abc.selenium.page.AppBody;
 import com.autonomy.abc.selenium.page.ElementFactory;
 
 public class SearchActionFactory extends ActionFactory {
+    // TODO: does this go stale?
+    private AppBody body;
+
     public SearchActionFactory(Application application, ElementFactory elementFactory) {
         super(application, elementFactory);
+        body = application.createAppBody(getDriver());
     }
 
     public Search makeSearch(String searchTerm) {
-        return new Search(getApplication().createAppBody(getDriver()), getElementFactory(), searchTerm);
+        return new Search(body, getElementFactory(), searchTerm);
     }
 }
