bb5b5b71678fd9847744798f178ec9554e89ec54
Fred Collman <frederick.collman@hpe.com>
2015-10-19 12:05:36 +0100

    Remove OP-specific pages from git

--- mockui/src/main/java/com/autonomy/abc/selenium/page/promotions/OPPromotionsPage.java
+++
@@ -1,9 +0,0 @@
-package com.autonomy.abc.selenium.page.promotions;
-
-import org.openqa.selenium.WebDriver;
-
-public class OPPromotionsPage extends PromotionsPage {
-    public OPPromotionsPage(WebDriver driver) {
-        super(driver);
-    }
-}
