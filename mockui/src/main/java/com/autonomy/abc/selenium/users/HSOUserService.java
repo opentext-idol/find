6b8fdb594a7920b92d456ad539212fe1cd42c2a3
Fred Collman <frederick.collman@hpe.com>
2015-10-20 11:40:02 +0100

    Fix merge so that everything compiles as expected

--- mockui/src/main/java/com/autonomy/abc/selenium/users/HSOUserService.java
+++ mockui/src/main/java/com/autonomy/abc/selenium/users/HSOUserService.java
@@ -4,8 +4,6 @@ import com.autonomy.abc.selenium.config.Application;
 import com.autonomy.abc.selenium.menu.NavBarTabId;
 import com.autonomy.abc.selenium.page.ElementFactory;
 import com.autonomy.abc.selenium.page.admin.UsersPage;
-import com.autonomy.abc.selenium.page.login.OPAccount;
-import com.hp.autonomy.frontend.selenium.sso.ApiKey;
 
 public class HSOUserService extends  UserService {
     public HSOUserService(Application application, ElementFactory elementFactory) {
