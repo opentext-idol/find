123cb0b29c9d4b8782ae0db28bdd77ce5e184ac8
Alex Scown <alex.scown@hpe.com>
2015-10-20 17:51:04 +0100

    Move non security beans into AppConfiguration. Remove from XML. Make AppConfiguration the primary part of the root context instead of the XML.

--- src/main/java/com/hp/autonomy/frontend/find/beanconfiguration/AppConfiguration.java
+++ src/main/java/com/hp/autonomy/frontend/find/beanconfiguration/AppConfiguration.java
@@ -10,20 +10,49 @@ import com.fasterxml.jackson.databind.ser.impl.SimpleBeanPropertyFilter;
 import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider;
 import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.ImmutableSet;
+import com.hp.autonomy.frontend.configuration.ConfigService;
+import com.hp.autonomy.frontend.configuration.filter.ConfigEnvironmentVariableFilter;
+import com.hp.autonomy.frontend.find.configuration.FindConfig;
 import com.hp.autonomy.frontend.find.configuration.TextEncryptorPasswordFactory;
+import com.hp.autonomy.frontend.logging.ApplicationStartLogger;
 import org.jasypt.util.text.BasicTextEncryptor;
 import org.jasypt.util.text.TextEncryptor;
 import org.springframework.beans.factory.BeanInitializationException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.config.PropertiesFactoryBean;
+import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.context.annotation.ImportResource;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
+import java.util.Locale;
 import java.util.Set;
 
 /**
  * Contains beans useful in all configurations
  */
 @Configuration
+@ComponentScan(
+    basePackages = "com.hp.autonomy.frontend.find",
+    excludeFilters = {
+        @ComponentScan.Filter(Controller.class),
+        @ComponentScan.Filter(RestController.class),
+        @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, value = DispatcherServletConfiguration.class)
+    }
+)
+@ImportResource("/WEB-INF/applicationContext.xml")
 public class AppConfiguration {
+
+    @Autowired
+    private ConfigService<FindConfig> configService;
 
     @Bean
     public TextEncryptor textEncryptor() {
@@ -54,4 +83,53 @@ public class AppConfiguration {
 
         return new SimpleFilterProvider(ImmutableMap.of("configurationFilter", filter));
     }
+
+    //TODO: merge properties files
+    @Bean
+    public PropertiesFactoryBean dispatcherProperties() {
+        return getPropertiesFactoryBean(new ClassPathResource("com/hp/autonomy/frontend/find/dispatcher.properties"));
+    }
+
+    @Bean
+    public PropertiesFactoryBean applicationProperties() {
+        return getPropertiesFactoryBean(new ClassPathResource("com/hp/autonomy/frontend/find/find.properties"));
+    }
+
+    private PropertiesFactoryBean getPropertiesFactoryBean(final ClassPathResource location) {
+        final PropertiesFactoryBean bean = new PropertiesFactoryBean();
+        bean.setLocation(location);
+
+        return bean;
+    }
+
+    @Bean
+    public ApplicationStartLogger applicationStartLogger() {
+        return new ApplicationStartLogger();
+    }
+
+    @Bean
+    public LocaleResolver localeResolver() {
+        final SessionLocaleResolver sessionLocaleResolver = new SessionLocaleResolver();
+        sessionLocaleResolver.setDefaultLocale(Locale.ENGLISH);
+
+        return sessionLocaleResolver;
+    }
+
+    @Bean
+    public MessageSource messageSource() {
+        final ResourceBundleMessageSource resourceBundleMessageSource = new ResourceBundleMessageSource();
+        resourceBundleMessageSource.setBasename("com.hp.autonomy.frontend.find.i18n");
+
+        return resourceBundleMessageSource;
+    }
+
+    @Bean
+    public ConfigEnvironmentVariableFilter configEnvironmentVariableFilter() {
+        final ConfigEnvironmentVariableFilter configEnvironmentVariableFilter = new ConfigEnvironmentVariableFilter();
+        configEnvironmentVariableFilter.setConfigPage("/configError");
+        configEnvironmentVariableFilter.setConfigService(configService);
+
+        return configEnvironmentVariableFilter;
+    }
+
 }
