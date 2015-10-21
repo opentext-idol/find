ea69dfbe426de711095971582c89336e0b4bdbff
Fred Collman <frederick.collman@hpe.com>
2015-10-21 11:02:38 +0100

    Add Matchers for waiting for an element to appear

--- integration-tests-common/src/test/java/com/autonomy/abc/matchers/ElementMatchers.java
+++ integration-tests-common/src/test/java/com/autonomy/abc/matchers/ElementMatchers.java
@@ -1,10 +1,15 @@
 package com.autonomy.abc.matchers;
 
+import com.hp.autonomy.frontend.selenium.util.AppElement;
 import org.hamcrest.Description;
 import org.hamcrest.Matcher;
 import org.hamcrest.TypeSafeMatcher;
 import org.openqa.selenium.By;
+import org.openqa.selenium.TimeoutException;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import static org.hamcrest.Matchers.containsString;
 
@@ -84,6 +89,54 @@ public class ElementMatchers {
 
             @Override
             public void describeMismatchSafely(final WebElement item, final Description description) {
+                description.appendText("element was ").appendText(item.toString());
+            }
+        };
+    }
+
+    public static Matcher<? super AppElement> appearsWithin(final int seconds) {
+        return new TypeSafeMatcher<AppElement>() {
+            @Override
+            protected boolean matchesSafely(AppElement item) {
+                try {
+                    new WebDriverWait(item.getDriver(), seconds).until(ExpectedConditions.visibilityOf(item));
+                    return true;
+                } catch (TimeoutException e) {
+                    return false;
+                }
+            }
+
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("an element appeared within " + seconds + " seconds");
+            }
+
+            @Override
+            protected void describeMismatchSafely(AppElement item, Description description) {
+                description.appendText("element was ").appendText(item.toString());
+            }
+        };
+    }
+
+    public static  Matcher<? super WebElement> appearsWithin(final int seconds, final WebDriver driver) {
+        return new TypeSafeMatcher<WebElement>() {
+            @Override
+            protected boolean matchesSafely(WebElement item) {
+                try {
+                    new WebDriverWait(driver, seconds).until(ExpectedConditions.visibilityOf(item));
+                    return true;
+                } catch (TimeoutException e) {
+                    return false;
+                }
+            }
+
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("an element appeared within " + seconds + " seconds");
+            }
+
+            @Override
+            protected void describeMismatchSafely(WebElement item, Description description) {
                 description.appendText("element was ").appendText(item.toString());
             }
         };
