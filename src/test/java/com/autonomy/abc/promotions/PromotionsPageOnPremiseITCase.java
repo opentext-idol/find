685436531b8d078824a3581a0ffcdefef421c82a
Fred Collman [IDOL QA] [Platform] <frederick.collman@hpe.com>
2015-10-22 10:15:40 +0000

    [abc] integration-tests fixed PromotionsPageOnPremiseITCase (field text)
    [git-p4: depot-paths = "//depot/products/frontend/abc/master/": change = 1250272]

--- src/test/java/com/autonomy/abc/promotions/PromotionsPageOnPremiseITCase.java
+++ src/test/java/com/autonomy/abc/promotions/PromotionsPageOnPremiseITCase.java
@@ -3,21 +3,31 @@ package com.autonomy.abc.promotions;
 import com.autonomy.abc.config.ABCTestBase;
 import com.autonomy.abc.selenium.config.ApplicationType;
 import com.autonomy.abc.config.TestConfig;
-import com.autonomy.abc.selenium.element.DatePicker;
+import com.autonomy.abc.selenium.element.Editable;
+import com.autonomy.abc.selenium.element.FormInput;
+import com.autonomy.abc.selenium.element.Removable;
+import com.autonomy.abc.selenium.page.promotions.OPPromotionsDetailPage;
 import com.autonomy.abc.selenium.page.promotions.OPPromotionsPage;
-import com.autonomy.abc.selenium.page.promotions.SchedulePage;
 import com.autonomy.abc.selenium.page.search.SearchPage;
-import com.autonomy.abc.selenium.promotions.PromotionService;
+import com.autonomy.abc.selenium.promotions.OPPromotionService;
+import com.autonomy.abc.selenium.promotions.Promotion;
+import com.autonomy.abc.selenium.promotions.SpotlightPromotion;
+import com.autonomy.abc.selenium.search.FieldTextFilter;
+import com.autonomy.abc.selenium.search.Search;
 import com.autonomy.abc.selenium.search.SearchActionFactory;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.Platform;
 
 import java.net.MalformedURLException;
-import java.util.regex.Pattern;
+import java.util.Arrays;
+import java.util.List;
 
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.junit.Assert.*;
+import static com.autonomy.abc.framework.ABCAssert.verifyThat;
+import static org.hamcrest.Matchers.*;
+import static org.openqa.selenium.lift.Matchers.displayed;
+
 
 public class PromotionsPageOnPremiseITCase extends ABCTestBase {
 
@@ -26,157 +36,152 @@ public class PromotionsPageOnPremiseITCase extends ABCTestBase {
 	}
 
 	private OPPromotionsPage promotionsPage;
+	private OPPromotionsDetailPage promotionsDetailPage;
 	private SearchPage searchPage;
-	private SchedulePage schedulePage;
-	private DatePicker datePicker;
-	private final Pattern pattern = Pattern.compile("\\s+");
-    private PromotionService promotionService;
+    private OPPromotionService promotionService;
     private SearchActionFactory searchActionFactory;
 
 	@Before
 	public void setUp() throws MalformedURLException {
-        promotionService = getApplication().createPromotionService(getElementFactory());
+		promotionService = (OPPromotionService) getApplication().createPromotionService(getElementFactory());
         searchActionFactory = new SearchActionFactory(getApplication(), getElementFactory());
 
-		promotionsPage = (OPPromotionsPage) promotionService.deleteAll();
+		promotionsPage = promotionService.deleteAll();
+	}
+
+    private void search(String term) {
+        searchPage = searchActionFactory.makeSearch(term).apply();
+    }
+
+	@Test
+	public void testInvalidFieldText() {
+		Promotion promotion = new SpotlightPromotion(Promotion.SpotlightType.HOTWIRE, "hot");
+		Search search = searchActionFactory.makeSearch("hot");
+		promotionService.setUpPromotion(promotion, search, 1);
+		List<String> badValues = Arrays.asList("", "bad", "<h1>h1</h1>");
+
+		promotionsDetailPage = promotionService.goToDetails(promotion);
+		FormInput inputBox = promotionsDetailPage.fieldTextInput();
+		Editable editableFieldText = promotionsDetailPage.editableFieldText();
+
+		for (String badValue : badValues) {
+			promotionsDetailPage.fieldTextAddButton().click();
+			verifyThat("error message not visible", promotionsDetailPage.getFieldTextError(), isEmptyOrNullString());
+			inputBox.setAndSubmit(badValue);
+			verifyThat("cannot add field text '" + badValue + "'", promotionsDetailPage.getFieldTextError(), containsString("SyntaxError"));
+			promotionsDetailPage.closeInputBox();
+		}
+
+		String goodText = "MATCH{good}:DRECONTENT";
+		promotionsDetailPage.addFieldText(goodText);
+		verifyThat("field text added", editableFieldText.getValue(), is(goodText));
+
+		for (String badValue : badValues) {
+			editableFieldText.editButton().click();
+			verifyThat("error message not visible", promotionsDetailPage.getFieldTextError(), isEmptyOrNullString());
+			inputBox.setAndSubmit(badValue);
+			verifyThat("cannot set field text to '" + badValue + "'", promotionsDetailPage.getFieldTextError(), containsString("SyntaxError"));
+			promotionsDetailPage.closeInputBox();
+		}
+
+		goodText = "NOT MATCH{bad}:DRECONTENT";
+		promotionsDetailPage.updateFieldText(goodText);
+		verifyThat("field text updated", editableFieldText.getValue(), is(goodText));
+
+		promotionsDetailPage.removeFieldText();
+		verifyThat("field text removed", promotionsDetailPage.fieldTextAddButton(), displayed());
+	}
+
+	private void verifyDisplayed(String searchTerm) {
+		search(searchTerm);
+		verifyThat("promotion displayed for search term '" + searchTerm + "'", searchPage.promotionsSummary(), displayed());
+	}
+
+	private void verifyNotDisplayed(String searchTerm) {
+		search(searchTerm);
+		verifyThat("promotion not displayed for search term '" + searchTerm + "'", searchPage.promotionsSummary(), not(displayed()));
 	}
 
 	@Test
 	public void testPromotionFieldTextRestriction() {
-//        Search search = searchActionFactory.makeSearch("hot");
-//        promotionService.setUpPromotion(new SpotlightPromotion(Promotion.SpotlightType.HOTWIRE, "hot"), search, 1);
-//
-//		promotionsPage.addFieldText("MATCH{hot}:DRECONTENT");
-//
-//		topNavBar.search("hot");
-//		searchPage.selectLanguage("English", getConfig().getType().getName());
-//		assertThat("Promoted Document should be visible", searchPage.promotionsSummary().isDisplayed());
-//
-//		topNavBar.search("hot pot");
-//		assertThat("Promoted Document should not be visible", !searchPage.promotionsSummary().isDisplayed());
-//
-//		topNavBar.search("hots");
-//		assertThat("Promoted Document should not be visible", !searchPage.promotionsSummary().isDisplayed());
-//
-//		navBar.switchPage(NavBarTabId.PROMOTIONS);
-//		promotionsPage.getPromotionLinkWithTitleContaining("hot").click();
-//		promotionsPage.loadOrFadeWait();
-//
-//		promotionsPage.removableFieldText().removeAndWait();
-//
-//		topNavBar.search("hot");
-//		searchPage.loadOrFadeWait();
-//		assertThat("Promoted Document should be visible", searchPage.promotionsSummary().isDisplayed());
-//
-//		topNavBar.search("hot chocolate");
-//		assertThat("Promoted Document should be visible", searchPage.promotionsSummary().isDisplayed());
-//
-//		topNavBar.search("hots");
-//		assertThat("Promoted Document should be visible", searchPage.promotionsSummary().isDisplayed());
-//
-//		navBar.switchPage(NavBarTabId.PROMOTIONS);
-//		promotionsPage.getPromotionLinkWithTitleContaining("hot").click();
-//		promotionsPage.loadOrFadeWait();
-//
-//		promotionsPage.fieldTextAddButton().click();
-//		promotionsPage.fieldTextInputBox().sendKeys("<h1>hi</h1>");
-//		promotionsPage.fieldTextTickConfirmButton().click();
-//		new WebDriverWait(getDriver(), 5).until(ExpectedConditions.visibilityOf(promotionsPage.fieldTextRemoveButton()));
-//		assertEquals("<h1>hi</h1>", promotionsPage.fieldTextValue());
-//
-//		promotionsPage.fieldTextEditButton().click();
-//		promotionsPage.fieldTextInputBox().clear();
-//		promotionsPage.fieldTextInputBox().sendKeys("MATCH{hot dog}:DRECONTENT");
-//		promotionsPage.fieldTextTickConfirmButton().click();
-//		promotionsPage.loadOrFadeWait();
-//		new WebDriverWait(getDriver(), 5).until(ExpectedConditions.visibilityOf(promotionsPage.fieldTextRemoveButton()));
-//
-//		topNavBar.search("hot dog");
-//		searchPage.loadOrFadeWait();
-//		assertThat("Promoted Document should be visible", searchPage.promotionsSummary().isDisplayed());
-//
-//		topNavBar.search("hot chocolate");
-//		assertThat("Promoted Document should not be visible", !searchPage.promotionsSummary().isDisplayed());
-//
-//		topNavBar.search("hot");
-//		assertThat("Promoted Document should not be visible", !searchPage.promotionsSummary().isDisplayed());
-//
-//		topNavBar.search("dog");
-//		assertThat("Promoted Document should not be visible", !searchPage.promotionsSummary().isDisplayed());
-//
-//		topNavBar.search("hot dogs");
-//		assertThat("Promoted Document should not be visible", !searchPage.promotionsSummary().isDisplayed());
-        fail("TODO");
+        Promotion promotion = new SpotlightPromotion(Promotion.SpotlightType.HOTWIRE, "hot");
+        Search search = searchActionFactory.makeSearch("hot");
+        promotionService.setUpPromotion(promotion, search, 1);
+
+        promotionsDetailPage = promotionService.goToDetails(promotion);
+		promotionsDetailPage.addFieldText("MATCH{hot}:DRECONTENT");
+
+		verifyDisplayed("hot");
+		for (String badValue : Arrays.asList("hot pot", "hots")) {
+			verifyNotDisplayed(badValue);
+		}
+
+		promotionsDetailPage = promotionService.goToDetails(promotion);
+
+		promotionsDetailPage.removableFieldText().removeAndWait();
+
+		for (String goodValue : Arrays.asList("hot", "hot chocolate", "hots")) {
+			verifyDisplayed(goodValue);
+		}
+
+		promotionsDetailPage = promotionService.goToDetails(promotion);
+		promotionsDetailPage.addFieldText("MATCH{temporary value}:DRECONTENT");
+		promotionsDetailPage.editableFieldText().setValueAndWait("MATCH{hot dog}:DRECONTENT");
+
+		verifyDisplayed("hot dog");
+		for (String badValue : Arrays.asList("hot chocolate", "hot", "dog", "hot dogs")) {
+			verifyNotDisplayed(badValue);
+		}
     }
 
 	@Test
 	public void testPromotionFieldTextOrRestriction() {
-//		promotionsPage.setUpANewPromotion("English", "road", "Hotwire", "highway street", getConfig().getType().getName());
-//
-//		promotionsPage.addFieldText("MATCH{highway}:DRECONTENT OR MATCH{street}:DRECONTENT");
-//
-//		topNavBar.search("highway street");
-//		searchPage.selectLanguage("English", getConfig().getType().getName());
-//		assertThat("Promoted Document should not be visible", !searchPage.promotionsSummary().isDisplayed());
-//
-//		topNavBar.search("road");
-//		assertThat("Promoted Document should not be visible", !searchPage.promotionsSummary().isDisplayed());
-//
-//		topNavBar.search("ROAD");
-//		assertThat("Promoted Document should not be visible", !searchPage.promotionsSummary().isDisplayed());
-//
-//		topNavBar.search("highway");
-//		assertThat("Promoted documents are not visible", searchPage.promotionsSummary().isDisplayed());
-//
-//		topNavBar.search("street");
-//		assertThat("Promoted documents are not visible", searchPage.promotionsSummary().isDisplayed());
-//
-//		topNavBar.search("highway street");
-//		assertThat("Promoted Document should not be visible", !searchPage.promotionsSummary().isDisplayed());
-//
-//		topNavBar.search("street highway");
-//		assertThat("Promoted Document should not be visible", !searchPage.promotionsSummary().isDisplayed());
-//
-//		topNavBar.search("street street");
-//		assertThat("Promoted Document should not be visible", !searchPage.promotionsSummary().isDisplayed());
-//
-//		topNavBar.search("highwaystreet");
-//		assertThat("Promoted Document should not be visible", !searchPage.promotionsSummary().isDisplayed());
-//
-//		topNavBar.search("highway AND street");
-//		assertThat("Promoted Document should not be visible", !searchPage.promotionsSummary().isDisplayed());
-        fail("TODO");
+		Promotion promotion = new SpotlightPromotion(Promotion.SpotlightType.HOTWIRE, "highway street");
+		promotionService.setUpPromotion(promotion, searchActionFactory.makeSearch("road"), 1);
+		promotionsDetailPage = promotionService.goToDetails(promotion);
+
+		promotionsDetailPage.addFieldText("MATCH{highway}:DRECONTENT OR MATCH{street}:DRECONTENT");
+
+		for (String goodTerm : Arrays.asList("highway", "street")) {
+			verifyDisplayed(goodTerm);
+		}
+		for (String badTerm : Arrays.asList("highway street", "road", "ROAD", "highway street", "street highway", "street street", "highwaystreet", "highway AND street", "highway OR street")) {
+			verifyNotDisplayed(badTerm);
+		}
 	}
 
 	@Test
 	public void testFieldTextSubmitTextOnEnter() {
-//		promotionsPage.setUpANewPromotion("English", "road", "Hotwire", "highway street", getConfig().getType().getName());
-//
-//		promotionsPage.fieldTextAddButton().click();
-//		promotionsPage.loadOrFadeWait();
-//		promotionsPage.fieldTextInputBox().sendKeys("TEST");
-//		promotionsPage.fieldTextInputBox().sendKeys(Keys.RETURN);
-//		new WebDriverWait(getDriver(), 5).until(ExpectedConditions.visibilityOf(promotionsPage.fieldTextRemoveButton()));
-//		assertTrue("Field text cannot be submitted with an enter key", promotionsPage.fieldTextRemoveButton().isDisplayed());
-        fail("TODO");
+		Promotion promotion = new SpotlightPromotion(Promotion.SpotlightType.HOTWIRE, "highway street");
+		promotionService.setUpPromotion(promotion, searchActionFactory.makeSearch("road"), 1);
+		promotionsDetailPage = promotionService.goToDetails(promotion);
+
+		promotionsDetailPage.fieldTextAddButton().click();
+		promotionsPage.loadOrFadeWait();
+		promotionsDetailPage.fieldTextInput().setValue("MATCH{test}:DRECONTENT" + Keys.RETURN);
+		promotionsDetailPage.waitForFieldTextToUpdate();
+		verifyThat("Added field text with RETURN", promotionsDetailPage.editableFieldText().getValue(), not(isEmptyOrNullString()));
 	}
 
+    // fails: CCUK-3250?
 	@Test
 	public void testCreateFieldTextField() {
-//		promotionsPage.setUpANewPromotion("Telugu", "మింగ్ వంశము", "Top Promotions", "Ming", getConfig().getType().getName());
-//
-//		promotionsPage.addFieldText("MATCH{Richard}:NAME");
-//		topNavBar.search("Ming");
-//		searchPage = getElementFactory().getSearchPage();
-//		assertThat("Promoted Document should not be visible", !searchPage.promotionsSummary().isDisplayed());
-//
-//		searchPage.expandFilter(SearchBase.Filter.FIELD_TEXT);
-//		searchPage.loadOrFadeWait();
-//		searchPage.fieldTextAddButton().click();
-//		searchPage.fieldTextInput().sendKeys("MATCH{Richard}:NAME");
-//		searchPage.fieldTextTickConfirm().click();
-//		searchPage.loadOrFadeWait();
-//		assertThat("Promoted Document should be visible", searchPage.promotionsSummary().isDisplayed());
-        fail("TODO");
+        Promotion promotion = new SpotlightPromotion(Promotion.SpotlightType.TOP_PROMOTIONS, "ming");
+        String fieldText = "RANGE{.,01/01/2010}:DREDATE";
+        FieldTextFilter filter = new FieldTextFilter(fieldText);
+        Search search = searchActionFactory.makeSearch("flash");//.applyFilter(filter);
+		searchPage = search.apply();
+		filter.apply(searchPage);
+		filter.clear(searchPage);
+		filter.clear(searchPage);
+
+		promotionService.setUpPromotion(promotion, search, 1);
+        promotionsDetailPage = promotionService.goToDetails(promotion);
+
+		promotionsDetailPage.addFieldText(fieldText);
+		verifyNotDisplayed("ming");
+
+		filter.clear(searchPage);
+		verifyThat(searchPage.promotionsSummary(), displayed());
 	}
 }
