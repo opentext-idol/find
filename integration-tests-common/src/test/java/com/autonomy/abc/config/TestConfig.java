e82954c84562d32a4fcf00b7beb8b40665f67abf
Fred Collman <frederick.collman@hpe.com>
2015-10-27 13:20:17 +0000

    Edit POM and TestConfig to support new configs via profiles/resource plugin

--- integration-tests-common/src/test/java/com/autonomy/abc/config/TestConfig.java
+++ integration-tests-common/src/test/java/com/autonomy/abc/config/TestConfig.java
@@ -5,6 +5,8 @@ import com.autonomy.abc.selenium.config.Browser;
 import com.autonomy.abc.selenium.users.User;
 import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.File;
 import java.io.IOException;
@@ -14,12 +16,23 @@ import java.util.Collection;
 import java.util.List;
 
 public class TestConfig {
-	private final static File DEFAULT_CONFIG_LOCATION = new File("../config/default.json");
+	private final static File BASE_CONFIG_LOCATION;
 	private final static File USER_CONFIG_LOCATION;
+	private final static Logger LOGGER = LoggerFactory.getLogger(TestConfig.class);
 
 	static {
+		String baseConfig = System.getProperty("com.autonomy.baseConfig");
+		if (baseConfig == null) {
+			// running in IDE
+			BASE_CONFIG_LOCATION = new File("../config/default.json");
+		} else {
+			// running via mvn, or specified config
+			BASE_CONFIG_LOCATION = new File(baseConfig);
+		}
+		LOGGER.info("Using base config " + BASE_CONFIG_LOCATION);
 		String configFile = System.getProperty("com.autonomy.configFile");
 		USER_CONFIG_LOCATION = (configFile == null) ? null : new File(configFile);
+		LOGGER.info("...overridden by " + USER_CONFIG_LOCATION);
 	}
 
 	private final JsonConfig jsonConfig;
@@ -68,10 +81,10 @@ public class TestConfig {
 
 	public static List<Object[]> readConfigs(final Collection<ApplicationType> applicationTypes) throws IOException {
 		List<Object[]> configs = new ArrayList<>();
-		JsonConfig defaultConfig = JsonConfig.readFile(DEFAULT_CONFIG_LOCATION);
+		JsonConfig defaultConfig = JsonConfig.readFile(BASE_CONFIG_LOCATION);
 		JsonConfig userSpecifiedConfig = JsonConfig.readFile(USER_CONFIG_LOCATION);
 		JsonConfig jsonConfig = defaultConfig.overrideUsing(userSpecifiedConfig);
-
+		LOGGER.info("Effective config: " + jsonConfig);
 
 		if (applicationTypes.contains(jsonConfig.getAppType())) {
 			for (int i = 0; i < jsonConfig.getBrowsers().size(); i++) {
