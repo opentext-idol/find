1beb3b3ca81a276497f22ebbd15305fe2aaa23e0
Matthew Williamson <matthew.williamson@hpe.com>
2015-10-28 15:41:03 +0000

    Dev Console tests base

---
+++ integration-tests-hosted/src/test/java/com/autonomy/abc/external/DevConsoleITCase.java
@@ -0,0 +1,48 @@
+package com.autonomy.abc.external;
+
+import com.autonomy.abc.config.ABCTestBase;
+import com.autonomy.abc.config.TestConfig;
+import com.autonomy.abc.selenium.config.ApplicationType;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Platform;
+import org.openqa.selenium.WebElement;
+
+import java.net.MalformedURLException;
+
+import static com.autonomy.abc.framework.ABCAssert.assertThat;
+import static org.hamcrest.CoreMatchers.containsString;
+
+public class DevConsoleITCase extends ABCTestBase {
+
+    public DevConsoleITCase(TestConfig config, String browser, ApplicationType type, Platform platform) {
+        super(config, browser, type, platform);
+    }
+
+    @Override
+    public void baseSetUp() throws MalformedURLException {
+        regularSetUp();
+        getDriver().get("http://search.preview.havenondemand.com");
+    }
+
+    @Test
+    public void testLaunchSearch() throws InterruptedException {
+        button("Search Admin").click();
+        Thread.sleep(2000);
+        tryLogIn();
+        assertThat(getDriver().getCurrentUrl(), containsString("search.preview.havenapps.io"));
+    }
+
+    public void testLaunchFind() throws InterruptedException {
+        button("Find").click();
+        Thread.sleep(2000);
+        tryLogIn();
+        assertThat(getDriver().getCurrentUrl(),containsString("find.preview.havenapps.io"));
+    }
+
+    public void testSignUp(){}
+
+    private WebElement button(String button){
+        return getDriver().findElement(By.xpath("//*[text()='"+button+"']/../a"));
+    }
+}
