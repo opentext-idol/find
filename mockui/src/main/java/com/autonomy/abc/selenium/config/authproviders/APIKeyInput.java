bd0ba66e0e7617e922a6555f08808640ec02661d
Fred Collman <frederick.collman@hpe.com>
2015-10-28 17:22:51 +0000

    Remove unused system property-dependent auth providers

--- mockui/src/main/java/com/autonomy/abc/selenium/config/authproviders/APIKeyInput.java
+++
@@ -1,9 +0,0 @@
-package com.autonomy.abc.selenium.config.authproviders;
-
-import com.hp.autonomy.frontend.selenium.sso.ApiKey;
-
-public class APIKeyInput extends ApiKey {
-    public APIKeyInput() {
-        super(System.getProperty("com.autonomy.apikey"));
-    }
-}
--- mockui/src/main/java/com/autonomy/abc/selenium/config/authproviders/Google.java
+++
@@ -1,9 +0,0 @@
-package com.autonomy.abc.selenium.config.authproviders;
-
-import com.hp.autonomy.frontend.selenium.sso.GoogleAuth;
-
-public class Google extends GoogleAuth {
-    public Google() {
-        super(System.getProperty("com.autonomy.username"), System.getProperty("com.autonomy.password"));
-    }
-}
