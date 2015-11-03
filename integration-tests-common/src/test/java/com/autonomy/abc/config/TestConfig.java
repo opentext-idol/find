6869b26a2ff52f70a7a3be9813b53d650fecb609
Fred Collman <frederick.collman@hpe.com>
2015-11-03 16:08:04 +0000

    TestConfig should load configs depending on environment: Maven (possibly non-local compiled resource from test-jar) or IDE (local file, possibly not in test-jar, from disk)

--- integration-tests-common/src/test/java/com/autonomy/abc/config/TestConfig.java
+++ integration-tests-common/src/test/java/com/autonomy/abc/config/TestConfig.java
@@ -18,24 +18,11 @@ import java.util.Collection;
 import java.util.List;
 
 public class TestConfig {
-	private final static File BASE_CONFIG_LOCATION;
-	private final static File OVERRIDE_CONFIG_LOCATION;
+	private final static String BASE_CONFIG_LOCATION = System.getProperty("com.autonomy.baseConfig");
+	private final static String OVERRIDE_CONFIG_LOCATION = System.getProperty("com.autonomy.configFile");
+	// system property set in the POM
+	private final static boolean MAVEN = System.getProperty("com.autonomy.mavenFlag") != null;
 	private final static Logger LOGGER = LoggerFactory.getLogger(TestConfig.class);
-
-	static {
-		String baseConfig = System.getProperty("com.autonomy.baseConfig");
-		if (baseConfig == null) {
-			// running in IDE
-			BASE_CONFIG_LOCATION = new File("../config/default.json");
-		} else {
-			// running via mvn, or specified config
-			BASE_CONFIG_LOCATION = new File(baseConfig);
-		}
-		LOGGER.info("Using base config " + BASE_CONFIG_LOCATION);
-		String configFile = System.getProperty("com.autonomy.configFile");
-		OVERRIDE_CONFIG_LOCATION = (configFile == null) ? null : new File(configFile);
-		LOGGER.info("...overridden by " + OVERRIDE_CONFIG_LOCATION);
-	}
 
 	private final JsonConfig jsonConfig;
 	private final int index;
@@ -93,10 +80,38 @@ public class TestConfig {
 		return browser.createWebDriver(url, platform);
 	}
 
+	// used when running whole suite via mvn
+	private static JsonConfig getMavenConfig(String path) throws IOException {
+		if (path == null) {
+			return null;
+		}
+		LOGGER.info("using config " + path);
+		return JsonConfig.fromURL(TestConfig.class.getClassLoader().getResource(path));
+	}
+
+	// used when running single tests via IDE
+	private static JsonConfig getLocalConfig(String path) throws IOException {
+		if (path == null) {
+			return null;
+		}
+		LOGGER.info("using config " + path);
+		return JsonConfig.readFile(new File(path));
+	}
+
 	public static List<Object[]> readConfigs(final Collection<ApplicationType> applicationTypes) throws IOException {
 		List<Object[]> configs = new ArrayList<>();
-		JsonConfig defaultConfig = JsonConfig.readFile(BASE_CONFIG_LOCATION);
-		JsonConfig userSpecifiedConfig = JsonConfig.readFile(OVERRIDE_CONFIG_LOCATION);
+
+		JsonConfig defaultConfig;
+		JsonConfig userSpecifiedConfig;
+		if (MAVEN) {
+			defaultConfig = getMavenConfig(BASE_CONFIG_LOCATION);
+			userSpecifiedConfig = getMavenConfig(OVERRIDE_CONFIG_LOCATION);
+		} else {
+			String basePath = BASE_CONFIG_LOCATION == null ? "../config/default.json" : BASE_CONFIG_LOCATION;
+			defaultConfig = getLocalConfig(basePath);
+			userSpecifiedConfig = getLocalConfig(OVERRIDE_CONFIG_LOCATION);
+		}
+
 		JsonConfig jsonConfig = defaultConfig.overrideUsing(userSpecifiedConfig);
 		LOGGER.info("Effective config: " + jsonConfig);
 
@@ -116,7 +131,11 @@ public class TestConfig {
 	}
 
 	public static JsonNode getRawBaseConfig() throws IOException {
-		return new ObjectMapper().readTree(BASE_CONFIG_LOCATION);
+		if (MAVEN) {
+			return new ObjectMapper().readTree(TestConfig.class.getClassLoader().getResource(BASE_CONFIG_LOCATION));
+		} else {
+			return new ObjectMapper().readTree(new File(BASE_CONFIG_LOCATION));
+		}
 	}
 
 }
