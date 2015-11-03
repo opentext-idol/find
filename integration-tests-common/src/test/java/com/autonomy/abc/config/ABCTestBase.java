8d48c9172129b256539f9bc4e17dfa1e048c44f8
Fred Collman <frederick.collman@hpe.com>
2015-11-03 17:12:12 +0000

    Update configs to use dev environment (temp), maven config should override default rather than being used as a base

--- integration-tests-common/src/test/java/com/autonomy/abc/config/ABCTestBase.java
+++ integration-tests-common/src/test/java/com/autonomy/abc/config/ABCTestBase.java
@@ -7,8 +7,6 @@ import com.autonomy.abc.framework.statements.StatementArtifactHandler;
 import com.autonomy.abc.framework.statements.StatementLoggingHandler;
 import com.autonomy.abc.selenium.config.Application;
 import com.autonomy.abc.selenium.config.ApplicationType;
-import com.autonomy.abc.selenium.menu.SideNavBar;
-import com.autonomy.abc.selenium.menu.TopNavBar;
 import com.autonomy.abc.selenium.page.AppBody;
 import com.autonomy.abc.selenium.page.ElementFactory;
 import com.autonomy.abc.selenium.page.login.AbcHasLoggedIn;
@@ -121,8 +119,8 @@ public abstract class ABCTestBase {
 
 	// log in via dev console while the HSOD SSO page is broken
 	private void workaroundLogIn() {
-		currentUser = config.getUser("hp_passport");
-		getDriver().get("https://www.preview.havenondemand.com/login.html");
+		currentUser = config.getUser("twitter");
+		getDriver().get("https://www.int.havenondemand.com/login.html");
 		currentUser.getAuthProvider().login(getDriver());
 		new WebDriverWait(getDriver(),30).until(ExpectedConditions.visibilityOfElementLocated(By.className("navbar-inner")));
 		getDriver().get(config.getWebappUrl());
