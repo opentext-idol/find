a2e187fd16b7158ae03341650b025b37ee77a3e9
Fred Collman [IDOL QA] [Platform] <frederick.collman@hpe.com>
2015-11-03 16:50:53 +0000

    [abc] integration-tests minor tweaks [rev:matthew.williamson]
    [git-p4: depot-paths = "//depot/products/frontend/abc/master/": change = 1252001]

--- src/main/java/com/autonomy/abc/selenium/page/promotions/OPCreateNewPromotionsPage.java
+++ src/main/java/com/autonomy/abc/selenium/page/promotions/OPCreateNewPromotionsPage.java
@@ -1,10 +1,26 @@
 package com.autonomy.abc.selenium.page.promotions;
 
+import com.autonomy.abc.selenium.actions.wizard.OptionWizardStep;
+import com.autonomy.abc.selenium.actions.wizard.WizardStep;
+import com.autonomy.abc.selenium.promotions.SearchTriggerStep;
+import com.autonomy.abc.selenium.promotions.SpotlightPromotion;
 import org.openqa.selenium.WebDriver;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class OPCreateNewPromotionsPage extends CreateNewPromotionsPage {
     public OPCreateNewPromotionsPage(WebDriver driver) {
         super(driver);
+    }
+
+    @Override
+    public List<WizardStep> getWizardSteps(SpotlightPromotion promotion) {
+        return Arrays.asList(
+            new OptionWizardStep(this, "Promotion type", promotion.getTypeOption()),
+            new OptionWizardStep(this, "Promotion details", promotion.getSpotlightTypeOption()),
+            new SearchTriggerStep(this, promotion.getTrigger())
+        );
     }
 
     @Override
