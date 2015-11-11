29543142d3bcd3092d829d861cf70b5c4ed23f34
Fred Collman <frederick.collman@hpe.com>
2015-11-11 18:16:00 +0000

    Fix PromotionsToFindITCase using PromotionService, SearchActionFactory, shrink main @Test method

--- integration-tests-hosted/src/test/java/com/autonomy/abc/endtoend/PromotionsToFindITCase.java
+++ integration-tests-hosted/src/test/java/com/autonomy/abc/endtoend/PromotionsToFindITCase.java
@@ -5,30 +5,26 @@ import com.autonomy.abc.config.TestConfig;
 import com.autonomy.abc.selenium.config.ApplicationType;
 import com.autonomy.abc.selenium.find.FindPage;
 import com.autonomy.abc.selenium.find.Service;
-import com.autonomy.abc.selenium.menu.NavBarTabId;
 import com.autonomy.abc.selenium.page.HSOElementFactory;
-import com.autonomy.abc.selenium.page.promotions.CreateNewPromotionsPage;
 import com.autonomy.abc.selenium.page.promotions.PromotionsDetailPage;
 import com.autonomy.abc.selenium.page.promotions.PromotionsPage;
-import com.autonomy.abc.selenium.page.search.SearchPage;
+import com.autonomy.abc.selenium.promotions.PinToPositionPromotion;
+import com.autonomy.abc.selenium.promotions.Promotion;
+import com.autonomy.abc.selenium.promotions.PromotionService;
+import com.autonomy.abc.selenium.promotions.SpotlightPromotion;
+import com.autonomy.abc.selenium.search.SearchActionFactory;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-import org.openqa.selenium.By;
 import org.openqa.selenium.Platform;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.ExpectedCondition;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
-import org.slf4j.LoggerFactory;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static com.autonomy.abc.framework.ABCAssert.verifyThat;
 import static org.hamcrest.Matchers.containsInAnyOrder;
+import static org.hamcrest.Matchers.empty;
 import static org.hamcrest.Matchers.isIn;
-import static org.hamcrest.core.Is.is;
 
 //CSA-1566
 public class PromotionsToFindITCase extends ABCTestBase {
@@ -40,75 +36,65 @@ public class PromotionsToFindITCase extends ABCTestBase {
     List<String> browserHandles;
     FindPage find;
     Service service;
-    private CreateNewPromotionsPage createNewPromotionsPage;
+    private PromotionService promotionService;
+    private SearchActionFactory searchActionFactory;
+
+    @Override
+    public HSOElementFactory getElementFactory() {
+        return (HSOElementFactory) super.getElementFactory();
+    }
 
     @Before
     public void setUp(){
+        promotionService = getApplication().createPromotionService(getElementFactory());
+        searchActionFactory = new SearchActionFactory(getApplication(), getElementFactory());
+
         PromotionsPage promotions = getElementFactory().getPromotionsPage();
         browserHandles = promotions.createAndListWindowHandles();
-        getDriver().switchTo().window(browserHandles.get(1));
+        switchToFind();
         getDriver().get(config.getFindUrl());
         getDriver().manage().window().maximize();
-        find = ((HSOElementFactory) getElementFactory()).getFindPage();
+        find = getElementFactory().getFindPage();
         service = find.getService();
+        switchToSearch();
+    }
+
+    private void switchToSearch() {
         getDriver().switchTo().window(browserHandles.get(0));
+    }
+
+    private void switchToFind() {
+        getDriver().switchTo().window(browserHandles.get(1));
     }
 
     @Test
     public void testPromotionsToFind(){
-        body.getTopNavBar().search("Promotions");
         String searchTrigger = "search";
         String secondaryTrigger = "secondary";
+        Promotion pinPromotion = new PinToPositionPromotion(1, searchTrigger);
+        Promotion spotlightPromotion = new SpotlightPromotion(searchTrigger);
+        Promotion secondPinPromotion = new PinToPositionPromotion(6, searchTrigger);
 
-        SearchPage searchPage = getElementFactory().getSearchPage();
-        List<String> promotionTitles = searchPage.createAMultiDocumentPromotion(5);
-        pinToPosition(searchTrigger,1);
-        getElementFactory().getSearchPage();
+        List<String> promotionTitles = setUpPromotion(pinPromotion, "Promotions", 5);
 
-        getDriver().switchTo().window(browserHandles.get(1));
+        switchToFind();
         find.search(searchTrigger);
+        verifyPinToPosition(promotionTitles, 1, 5);
 
-        verifyPinToPosition(promotionTitles,1,5);
+        switchToSearch();
+        PromotionsDetailPage promotionsDetailPage = promotionService.goToDetails(pinPromotion);
+        promotionsDetailPage.pinPosition().setValueAndWait("6");
 
-        getDriver().switchTo().window(browserHandles.get(0));
-
-        getElementFactory().getSearchPage();
-        body.getSideNavBar().switchPage(NavBarTabId.PROMOTIONS);
-
-        PromotionsPage promotionsPage = getElementFactory().getPromotionsPage();
-        promotionsPage.getPromotionLinkWithTitleContaining(searchTrigger).click();
-        PromotionsDetailPage promotionsDetailPage = getElementFactory().getPromotionsDetailPage();
-        promotionsDetailPage.findElement(By.cssSelector(".promotion-position-edit .fa-pencil")).click();
-
-        for(int i = 0;i < 5; i++){
-            promotionsDetailPage.findElement(By.className("plus")).click();
-        }
-
-        promotionsDetailPage.findElement(By.cssSelector(".promotion-position-edit .fa-check")).click();
-
-        new WebDriverWait(getDriver(),60).until(ExpectedConditions.invisibilityOfElementLocated(By.cssSelector(".promotion-position-edit .fa-spin")));
-
-        getDriver().switchTo().window(browserHandles.get(1));
-
-        getDriver().navigate().refresh();
-        find = ((HSOElementFactory) getElementFactory()).getFindPage();
-        service = find.getService();
-
-        service.waitForSearchLoadIndicatorToDisappear(Service.Container.MIDDLE);
-
+        switchToFind();
+        refreshFind();
         verifyPinToPosition(promotionTitles, 6, 10);
 
-        getDriver().switchTo().window(browserHandles.get(0));
-
+        switchToSearch();
         promotionsDetailPage.triggerAddBox().setAndSubmit(secondaryTrigger);
-        final WebElement trigger = promotionsDetailPage.findElement(By.cssSelector(".promotion-view-match-terms [data-id='" + secondaryTrigger + "']"));
+        promotionsDetailPage.waitForTriggerRefresh();
 
-        new WebDriverWait(getDriver(),30).until(ExpectedConditions.invisibilityOfElementLocated(
-                By.cssSelector(".promotion-view-match-terms [data-id='" + secondaryTrigger + "'] .fa-spin")));
-
-        getDriver().switchTo().window(browserHandles.get(1));
+        switchToFind();
         find.search(secondaryTrigger);
-
         verifyPinToPosition(promotionTitles, 6, 10);
 
         service.filterByIndex("default_index");
@@ -116,102 +102,67 @@ public class PromotionsToFindITCase extends ABCTestBase {
 
         service.filterByIndex("default_index");
         service.filterByParametric("Source Connector", "SIMPSONSARCHIVE");
-
         verifyPinToPosition(promotionTitles, 6, 10);
 
-        getDriver().switchTo().window(browserHandles.get(0));
+        switchToSearch();
+        List<String> spotlightPromotionTitles = setUpPromotion(spotlightPromotion, "Tertiary", 2);
 
-        body.getTopNavBar().search("Tertiary");
-
-        searchPage = getElementFactory().getSearchPage();
-        List<String> spotlightPromotionTitles = searchPage.createAMultiDocumentPromotion(2);
-        createNewPromotionsPage = getElementFactory().getCreateNewPromotionsPage();
-        createNewPromotionsPage.addSpotlightPromotion("", searchTrigger);
-
-        final SearchPage finalSearchPage = getElementFactory().getSearchPage();
-        new WebDriverWait(getDriver(),30).until(new ExpectedCondition<Boolean>() {
-            @Override
-            public Boolean apply(WebDriver driver) {
-                return finalSearchPage.getPromotedResults().size() != 0;
-            }
-        });
-
-        getDriver().switchTo().window(browserHandles.get(1));
+        switchToFind();
         find.search(searchTrigger);
 
         verifyPinToPosition(promotionTitles, 6, 10);
         verifySpotlight(spotlightPromotionTitles);
 
-        getDriver().switchTo().window(browserHandles.get(0));
+        switchToSearch();
+        String singlePromoted = setUpPromotion(secondPinPromotion, "187", 1).get(0);
 
-        body.getTopNavBar().search("187");
-        searchPage = getElementFactory().getSearchPage();
-        String singlePTPPromotion = searchPage.createAPromotion();
-         pinToPosition(searchTrigger, 6);
-        getElementFactory().getSearchPage();
+        switchToFind();
+        refreshFind();
 
-        getDriver().switchTo().window(browserHandles.get(1));
-        getDriver().navigate().refresh();
-        find = ((HSOElementFactory) getElementFactory()).getFindPage();
-        service = find.getService();
+        verifyThat(singlePromoted, isIn(service.getResultTitles(6, 11)));
 
-        verifyThat(singlePTPPromotion, isIn(service.getResultTitles(6, 11)));
-
-        List<String> allPromotions = promotionTitles;
-        allPromotions.add(singlePTPPromotion);
-
+        List<String> allPromotions = new ArrayList<>(promotionTitles);
+        allPromotions.add(singlePromoted);
         verifyPinToPosition(allPromotions, 6, 11);
         verifySpotlight(spotlightPromotionTitles);
 
-        getDriver().switchTo().window(browserHandles.get(0));
-        body.getSideNavBar().switchPage(NavBarTabId.PROMOTIONS);
-        promotionsPage = getElementFactory().getPromotionsPage();
-        promotionsPage.deletePromotion("Spotlight for: " + searchTrigger);
+        switchToSearch();
+        promotionService.delete(spotlightPromotion);
 
-        getDriver().switchTo().window(browserHandles.get(1));
+        switchToFind();
         find.search("Other");
         find.search(searchTrigger);
 
-        verifyThat(service.getPromotionsTitles().isEmpty(), is(Boolean.TRUE));
-
-        LoggerFactory.getLogger(PromotionsToFindITCase.class).info("TEST FINISHED");
+        verifyThat(service.getPromotionsTitles(), empty());
     }
 
-    private void pinToPosition(String searchTrigger, int position) {
-        createNewPromotionsPage = getElementFactory().getCreateNewPromotionsPage();
-        createNewPromotionsPage.pinToPosition().click();
-        createNewPromotionsPage.loadOrFadeWait();
-        createNewPromotionsPage.continueButton().click();
-        createNewPromotionsPage.loadOrFadeWait();
+    private void refreshFind() {
+        getDriver().navigate().refresh();
+        find = getElementFactory().getFindPage();
+        service = find.getService();
+        service.waitForSearchLoadIndicatorToDisappear(Service.Container.MIDDLE);
+    }
 
-        for(int i = 1; i < position; i++){
-            createNewPromotionsPage.findElement(By.className("plus")).click();
-        }
-
-        createNewPromotionsPage.loadOrFadeWait();
-        createNewPromotionsPage.continueButton().click();
-        createNewPromotionsPage.loadOrFadeWait();
-        createNewPromotionsPage.addSearchTrigger(searchTrigger);
-        createNewPromotionsPage.finishButton().click();
+    private List<String> setUpPromotion(Promotion promotion, String searchTerm, int numberOfDocs) {
+        return promotionService.setUpPromotion(promotion,
+                searchActionFactory.makeSearch(searchTerm), numberOfDocs);
     }
 
     private void verifySpotlight(List<String> promotionTitles) {
-        verify(service.getPromotionsTitles(),promotionTitles);
+        verifyResultsContainPromoted(service.getPromotionsTitles(), promotionTitles);
     }
 
-    private void verify(List<String> search, List<String> promotion){
+    private void verifyResultsContainPromoted(List<String> search, List<String> promotion){
         verifyThat(search, containsInAnyOrder(promotion.toArray()));
     }
 
     private void verifyPinToPosition(List<String> promotionTitles, int start, int end) {
-        verify(service.getResultTitles(start, end), promotionTitles);
+        verifyResultsContainPromoted(service.getResultTitles(start, end), promotionTitles);
     }
-
 
     @After
     public void tearDown(){
-        getDriver().switchTo().window(browserHandles.get(0));
-        body.getSideNavBar().switchPage(NavBarTabId.PROMOTIONS);
-        getElementFactory().getPromotionsPage().deleteAllPromotions();
+        switchToSearch();
+        promotionService.deleteAll();
     }
 }
