55a770f40a4261a85a9612cd4a5cf5be49dfd6bc
Matthew Williamson <matthew.williamson@hpe.com>
2015-11-12 16:21:06 +0000

    Base for IndexesPageITCase

---
+++ integration-tests-hosted/src/test/java/com/autonomy/abc/indexes/IndexesITCase.java
@@ -0,0 +1,34 @@
+package com.autonomy.abc.indexes;
+
+import com.autonomy.abc.config.ABCTestBase;
+import com.autonomy.abc.config.TestConfig;
+import com.autonomy.abc.selenium.config.ApplicationType;
+import com.autonomy.abc.selenium.menu.NavBarTabId;
+import com.autonomy.abc.selenium.page.HSOElementFactory;
+import com.autonomy.abc.selenium.page.connections.ConnectionsPage;
+import org.junit.Before;
+import org.openqa.selenium.Platform;
+
+public class IndexesITCase extends ABCTestBase {
+
+    ConnectionsPage connectionsPage;
+    HSOElementFactory hsoElementFactory;
+
+    public IndexesITCase(TestConfig config, String browser, ApplicationType type, Platform platform) {
+        super(config, browser, type, platform);
+    }
+
+    @Before
+    @Override
+    public void baseSetUp() throws InterruptedException {
+        regularSetUp();
+        hostedLogIn("yahoo");
+        getElementFactory().getPromotionsPage();
+
+        body.getSideNavBar().switchPage(NavBarTabId.CONNECTIONS);
+
+        hsoElementFactory = (HSOElementFactory) getElementFactory();
+        connectionsPage = hsoElementFactory.getConnectionsPage();
+    }
+
+}
