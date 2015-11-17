0776bfb4201ec31e006fad1b8fff7399a618284b
Fred Collman <frederick.collman@hpe.com>
2015-11-17 16:30:43 +0000

    Remove unnecessary HSOSideNavBar

--- mockui/src/main/java/com/autonomy/abc/selenium/page/HSOAppBody.java
+++ mockui/src/main/java/com/autonomy/abc/selenium/page/HSOAppBody.java
@@ -1,6 +1,5 @@
 package com.autonomy.abc.selenium.page;
 
-import com.autonomy.abc.selenium.menu.HSO.HSOSideNavBar;
 import com.autonomy.abc.selenium.menu.HSO.HSOTopNavBar;
 import com.autonomy.abc.selenium.menu.SideNavBar;
 import com.autonomy.abc.selenium.menu.TopNavBar;
@@ -8,7 +7,7 @@ import org.openqa.selenium.WebDriver;
 
 public class HSOAppBody extends AppBody {
     public HSOAppBody(WebDriver driver){
-        this(driver, new HSOTopNavBar(driver), new HSOSideNavBar(driver));
+        this(driver, new HSOTopNavBar(driver), new SideNavBar(driver));
     }
 
     public HSOAppBody(WebDriver driver, TopNavBar topNavBar, SideNavBar sideNavBar) {
