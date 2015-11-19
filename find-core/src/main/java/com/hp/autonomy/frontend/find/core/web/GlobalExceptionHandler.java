5cdd4acbdd5f510f767c24175e08a24bc78a6c53
Miller <ivo.miller@hpe.com>
2015-11-19 10:38:00 +0000

    CCUK-3424 Find - implement Java services for ACI IDOL :: move global exception handling class to core module

--- find-core/src/main/java/com/hp/autonomy/frontend/find/core/web/GlobalExceptionHandler.java
+++ find-core/src/main/java/com/hp/autonomy/frontend/find/core/web/GlobalExceptionHandler.java
@@ -3,15 +3,11 @@
  * Licensed under the MIT License (the "License"); you may not use this file except in compliance with the License.
  */
 
-package com.hp.autonomy.frontend.find.hod.web;
+package com.hp.autonomy.frontend.find.core.web;
 
-import com.hp.autonomy.frontend.find.core.web.ErrorResponse;
-import com.hp.autonomy.hod.client.api.authentication.HodAuthenticationFailedException;
-import com.hp.autonomy.hod.client.error.HodErrorException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.annotation.AnnotationUtils;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -21,48 +17,26 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 
 @Slf4j
 @ControllerAdvice
-public class GlobalExceptionHandler {
+public abstract class GlobalExceptionHandler {
 
     @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
     @ResponseStatus(HttpStatus.METHOD_NOT_ALLOWED)
     @ResponseBody
-    public ErrorResponse requestMethodNotSupportedHandler(final HttpRequestMethodNotSupportedException exception) throws Exception {
+    public ErrorResponse requestMethodNotSupportedHandler(final HttpRequestMethodNotSupportedException exception) throws HttpRequestMethodNotSupportedException {
         return handler(exception);
     }
 
     @ExceptionHandler(HttpMessageNotReadableException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ResponseBody
-    public ErrorResponse messageNotReadableHandler(final HttpMessageNotReadableException exception) throws Exception {
+    public ErrorResponse messageNotReadableHandler(final HttpMessageNotReadableException exception) throws HttpMessageNotReadableException {
         return handler(exception);
-    }
-
-    @ExceptionHandler(HodAuthenticationFailedException.class)
-    @ResponseStatus(HttpStatus.UNAUTHORIZED)
-    @ResponseBody
-    public ErrorResponse authenticationFailedHandler(final HodAuthenticationFailedException exception) {
-        return new ErrorResponse("TOKEN HAS EXPIRED");
-    }
-
-    @ExceptionHandler(HodErrorException.class)
-    @ResponseBody
-    public ResponseEntity<HodErrorResponse> hodErrorHandler(final HodErrorException exception) {
-        final HodErrorResponse hodErrorResponse = new HodErrorResponse("HOD Error", exception.getErrorCode());
-        final ResponseEntity<HodErrorResponse> entity;
-
-        if (exception.isServerError()) {
-            entity = new ResponseEntity<>(hodErrorResponse, HttpStatus.INTERNAL_SERVER_ERROR);
-        } else {
-            entity = new ResponseEntity<>(hodErrorResponse, HttpStatus.BAD_REQUEST);
-        }
-
-        return entity;
     }
 
     @ExceptionHandler(Exception.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     @ResponseBody
-    public ErrorResponse handler(final Exception exception) throws Exception {
+    public <E extends Exception> ErrorResponse handler(final E exception) throws E {
         // boilerplate - see http://spring.io/blog/2013/11/01/exception-handling-in-spring-mvc
         if (AnnotationUtils.findAnnotation(exception.getClass(), ResponseStatus.class) != null) {
             throw exception;
