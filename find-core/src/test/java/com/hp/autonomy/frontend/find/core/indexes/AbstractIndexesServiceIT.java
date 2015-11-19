339bd59a1fc8f476e394725f205e53400d51367e
Miller <ivo.miller@hpe.com>
2015-11-19 12:06:50 +0000

    CCUK-3424 Find - implement Java services for ACI IDOL :: generify integration test

---
+++ find-core/src/test/java/com/hp/autonomy/frontend/find/core/indexes/AbstractIndexesServiceIT.java
@@ -0,0 +1,49 @@
+package com.hp.autonomy.frontend.find.core.indexes;
+
+import com.hp.autonomy.frontend.find.core.beanconfiguration.AppConfiguration;
+import com.hp.autonomy.types.IdolDatabase;
+import org.apache.commons.io.FileUtils;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+import static org.junit.Assert.assertFalse;
+
+@SuppressWarnings("SpringJavaAutowiringInspection")
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = AppConfiguration.class)
+@TestPropertySource(properties = {"hp.find.persistentState = INMEMORY", "hp.find.home = ./target/test", "find.https.proxyHost = web-proxy.sdc.hpecorp.net", "find.https.proxyPort: 8080", "find.iod.api = https://api.havenondemand.com", "find.hod.sso = https://dev.havenondemand.com/sso.html"})
+public abstract class AbstractIndexesServiceIT<S extends IndexesService<E>, E extends Exception> {
+    private static final String TEST_DIR = "./target/test";
+
+    @BeforeClass
+    public static void init() throws IOException {
+        System.setProperty("hp.find.home", TEST_DIR);
+        final File directory = new File(TEST_DIR);
+        FileUtils.forceMkdir(directory);
+        FileUtils.copyFileToDirectory(new File("./src/test/resources/config.json"), directory);
+    }
+
+    @AfterClass
+    public static void destroy() throws IOException {
+        FileUtils.forceDelete(new File(TEST_DIR));
+    }
+
+    @Autowired
+    protected S indexesService;
+
+    @Test
+    public void noExcludedIndexes() throws E {
+        final List<? extends IdolDatabase> databases = indexesService.listVisibleIndexes();
+        assertFalse(databases.isEmpty());
+    }
+}
