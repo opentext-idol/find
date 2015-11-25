23a53f40e84dcbf2810598c5902b10cda94f973d
Matthew Williamson <matthew.williamson@hpe.com>
2015-11-25 10:47:08 +0000

    Changes to Hosted Services in order to override methods in superclass and added a way to log out of hosted

--- mockui/src/main/java/com/autonomy/abc/selenium/page/HSOAppBody.java
+++ mockui/src/main/java/com/autonomy/abc/selenium/page/HSOAppBody.java
@@ -3,6 +3,7 @@ package com.autonomy.abc.selenium.page;
 import com.autonomy.abc.selenium.menu.HSO.HSOTopNavBar;
 import com.autonomy.abc.selenium.menu.SideNavBar;
 import com.autonomy.abc.selenium.menu.TopNavBar;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class HSOAppBody extends AppBody {
@@ -16,6 +17,7 @@ public class HSOAppBody extends AppBody {
 
     @Override
     public void logout() {
-        System.out.println("Cannot logout on hosted");
+        getTopNavBar().findElement(By.className("hp-settings")).click();
+        getTopNavBar().findElement(By.className("navigation-logout")).click();
     }
 }
