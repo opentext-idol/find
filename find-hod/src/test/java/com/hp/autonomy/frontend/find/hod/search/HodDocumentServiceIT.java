d0b17f5f634eb13907d14a974a70d3e9eb73b711
Miller <ivo.miller@hpe.com>
2015-11-30 08:48:17 +0000

    CCUK-3424 Find - implement Java services for ACI IDOL :: update copyright

--- find-hod/src/test/java/com/hp/autonomy/frontend/find/hod/search/HodDocumentServiceIT.java
+++ find-hod/src/test/java/com/hp/autonomy/frontend/find/hod/search/HodDocumentServiceIT.java
@@ -1,3 +1,8 @@
+/*
+ * Copyright 2015 Hewlett-Packard Development Company, L.P.
+ * Licensed under the MIT License (the "License"); you may not use this file except in compliance with the License.
+ */
+
 package com.hp.autonomy.frontend.find.hod.search;
 
 import com.hp.autonomy.frontend.find.core.search.AbstractDocumentServiceIT;
