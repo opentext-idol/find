b4585340b300c90dedfd3a7fdb1961c5f181d13c
Miller <ivo.miller@hpe.com>
2015-12-03 11:02:26 +0000

    CCUK-3424 Find - implement Java services for ACI IDOL :: modularise view-proxy-components [rev. alex.scown]

--- find-idol/src/main/java/com/hp/autonomy/frontend/find/idol/search/IdolDocumentsController.java
+++ find-idol/src/main/java/com/hp/autonomy/frontend/find/idol/search/IdolDocumentsController.java
@@ -8,8 +8,6 @@ package com.hp.autonomy.frontend.find.idol.search;
 import com.autonomy.aci.client.services.AciErrorException;
 import com.hp.autonomy.frontend.find.core.search.DocumentsController;
 import com.hp.autonomy.frontend.find.core.search.FindDocument;
-import com.hp.autonomy.hod.client.api.resource.ResourceIdentifier;
-import com.hp.autonomy.hod.client.error.HodErrorException;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
