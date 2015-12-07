6f73dfac7d8e1e94114c4eb93b284739995452c8
Miller <ivo.miller@hpe.com>
2015-12-07 14:47:43 +0000

    migration to spring-boot :: remove bower_components again

--- find-web/src/main/webapp/static/bower_components/hp-autonomy-js-parametric-refinement/travis.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if [[ $TRAVIS_BRANCH == 'master' ]]
-then
-  git config credential.helper "store --file=.git/credentials"
-  echo "https://${GH_TOKEN}:@github.com" > .git/credentials
-  grunt push-doc-travis
-fi
