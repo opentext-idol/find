c0a67269ffdb8aaa7a02f9ea844eddf2304ffc7e
Alex Scown <alex.scown@hpe.com>
2015-12-10 16:19:59 +0000

    Update lib-list with correct information.

--- find-idol/src/main/public/static/js/find/idol/app/page/about/lib-list.js
+++ find-idol/src/main/public/static/js/find/idol/app/page/about/lib-list.js
@@ -5,72 +5,47 @@
 
 define(function() {
 
-    // TODO: placeholders, use actual libraries
     return [
         {
             name: 'Backbone.js',
             url: 'http://backbonejs.org/',
-            version: '1.1.2',
+            version: '1.2.3',
             license: 'MIT',
             edited: false
         }, {
             name: 'Bootstrap',
             url: 'http://twitter.github.com/bootstrap/',
-            version: '3.1.1',
+            version: '3.3.5',
             license: 'Apache License v2.0',
             edited: true
+        }, {
+            name: 'Bootstrap Date Time Picker',
+            url: 'http://eonasdan.github.io/bootstrap-datetimepicker/',
+            version: '4.14.30',
+            license: 'MIT',
+            edited: false
         }, {
             name: 'Colorbox',
             url: 'http://www.jacklmoore.com/colorbox/',
             version: '1.5.10',
             license: 'MIT',
-            edited: true
+            edited: false
         }, {
             name: 'DataTables',
             url: 'http://www.datatables.net/download/',
             version: '1.10.2',
             license: 'BSD',
-            edited: true
-        }, {
-            name: 'Explorer Canvas',
-            url: 'http://excanvas.sourceforge.net/',
-            version: 'r73',
-            license: 'Apache License 2.0',
-            edited: false
-        }, {
-            name: 'Flot',
-            url: 'http://www.flotcharts.org/',
-            version: '0.8.2',
-            license: 'MIT',
             edited: false
         }, {
             name: 'Font Awesome by Dave Gandy',
             url: 'http://fontawesome.io',
-            version: '4.2.0',
+            version: '3.2.1',
             license: 'MIT/SIL OFL 1.1', // icons are SIL OFL 1.1, css is MIT
-            edited: true // remove parameter from css url
-        }, {
-            name: 'Gritter',
-            url: 'https://github.com/jboesch/Gritter',
-            version: '1.7.4',
-            license: 'MIT',
-            edited: false
-        }, {
-            name: 'iCheck',
-            url: 'http://git.io/arlzeA',
-            version: '1.0.2',
-            license: 'MIT',
-            edited: false
+            edited: false // remove parameter from css url
         }, {
             name: 'jQuery',
             url: 'http://jquery.com/',
             version: '1.10.2',
-            license: 'MIT',
-            edited: false
-        }, {
-            name: 'jQuery Transit',
-            url: 'http://github.com/rstacruz/jquery.transit',
-            version: '0.9.12',
             license: 'MIT',
             edited: false
         }, {
@@ -80,57 +55,21 @@ define(function() {
             license: 'Public Domain',
             edited: false
         }, {
-            name: 'Modernizr',
-            url: 'http://modernizr.com',
-            version: '2.8.3',
-            license: 'MIT',
-            edited: false
-        }, {
             name: 'Moment.js',
             url: 'http://momentjs.com',
-            version: '1.7.2',
-            license: 'MIT',
-            edited: false
-        }, {
-            name: 'Raphaël',
-            url: 'http://raphaeljs.com/',
-            version: '2.1.2',
+            version: '2.10.3',
             license: 'MIT',
             edited: false
         }, {
             name: 'RequireJS',
             url: 'http://requirejs.org/',
-            version: '2.1.9',
+            version: '2.1.15',
             license: 'BSD',
-            edited: false
-        }, {
-            name: 'store.js',
-            url: 'https://github.com/marcuswestin/store.js',
-            version: '1.3.6',
-            license: 'MIT',
-            edited: false
-        }, {
-            name: 'sockjs',
-            url: 'https://github.com/sockjs/sockjs-client',
-            version: '0.3.4',
-            license: 'MIT',
-            edited: false
-        }, {
-            name: 'stomp.js',
-            url: 'https://github.com/jmesnil/stomp-websocket',
-            version: '2.3.3',
-            license: 'Apache License v2.0',
             edited: false
         }, {
             name: 'Underscore.js',
             url: 'http://underscorejs.org/',
-            version: '1.5.2',
-            license: 'MIT',
-            edited: false
-        }, {
-            name: 'X-editable',
-            url: 'http://vitalets.github.com/x-editable/',
-            version: '1.5.1',
+            version: '1.8.3',
             license: 'MIT',
             edited: false
         }
