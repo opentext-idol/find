2bb1a8fa73527699081bc09eed27d6295587afd4
Matthew Williamson <matthew.williamson@hpe.com>
2015-12-17 13:43:16 +0000

    Updates to Gmail Signup Handler - UNTESTED

--- mockui/src/main/java/com/autonomy/abc/selenium/users/GmailSignupEmailHandler.java
+++ mockui/src/main/java/com/autonomy/abc/selenium/users/GmailSignupEmailHandler.java
@@ -9,10 +9,12 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 
 public class GmailSignupEmailHandler implements SignupEmailHandler {
     private final static String GMAIL_URL = "https://accounts.google.com/ServiceLogin?service=mail&continue=https://mail.google.com/mail/#identifier";
     private final GoogleAuth googleAuth;
+    private WebDriver driver;
 
     public GmailSignupEmailHandler(GoogleAuth auth) {
         this.googleAuth = auth;
@@ -20,15 +22,28 @@ public class GmailSignupEmailHandler implements SignupEmailHandler {
 
     @Override
     public void goToUrl(WebDriver driver) {
+        this.driver = driver;
         driver.get(GMAIL_URL);
         new GoogleAuth.GoogleLoginPage(driver).login(googleAuth);
-        waitForNewEmail(driver);
-        clickUnreadMessage(driver);
-        expandCollapsedMessage(driver);
-        clickLink(driver);
+        openMessage();
+        try {
+            clickLink();
+        } catch (NoSuchElementException e) {
+            /* Probably had an unread email */
+
+            driver.findElement(By.cssSelector("[data-tooltip='Back to Inbox']")).click();
+            openMessage();
+            clickLink();
+        }
     }
 
-    private void waitForNewEmail(WebDriver driver) {
+    private void openMessage(){
+        waitForNewEmail();
+        clickUnreadMessage();
+        expandCollapsedMessage();
+    }
+
+    private void waitForNewEmail() {
         new WebDriverWait(driver,60).until(new ExpectedCondition<Boolean>() {
             @Override
             public Boolean apply(WebDriver driver) {
@@ -45,11 +60,11 @@ public class GmailSignupEmailHandler implements SignupEmailHandler {
         });
     }
 
-    private void clickUnreadMessage(WebDriver driver){
+    private void clickUnreadMessage(){
         driver.findElement(By.cssSelector(".zA.zE")).click();
     }
 
-    private void expandCollapsedMessage(WebDriver driver) {
+    private void expandCollapsedMessage() {
         try {
             WebElement ellipses = new WebDriverWait(driver,10).until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("img.ajT")));
 
@@ -59,17 +74,28 @@ public class GmailSignupEmailHandler implements SignupEmailHandler {
         } catch (Exception e) { /* No Ellipses */ }
     }
 
-    private void clickLink(WebDriver driver) {
+    private void clickLink() {
         driver.findElement(By.partialLinkText("here")).click();
 
+        loadOrFadeWait();
+        String loginWindow = driver.getWindowHandles().toArray(new String[1])[0];
+
+        if(!driver.getCurrentUrl().contains("/verification/")){
+            driver.switchTo().window(loginWindow);
+            driver.close();
+            //Probably the wrong exception to throw but just to make things easier - happens when a link has already been used for auth
+            throw new org.openqa.selenium.NoSuchElementException("Incorrect link clicked");
+        }
+
+        driver.close();
+        driver.switchTo().window(loginWindow);
+    }
+
+    private void loadOrFadeWait() {
         try {
             Thread.sleep(1000);
         } catch (InterruptedException e) {
             /* NOOP */
         }
-
-        driver.close();
-        String loginWindow = driver.getWindowHandles().toArray(new String[1])[0];
-        driver.switchTo().window(loginWindow);
     }
 }
