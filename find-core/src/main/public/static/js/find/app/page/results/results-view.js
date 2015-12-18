9fbbf5fe536418adc83cf60c0a8bb302e4bd55cb
Will Byrne <will.byrne@hp.com>
2015-12-18 17:53:20 +0000

    [CSA-1947] Change Date display to the friendly name representation as exists in the mock [rev Matt.Gordon]

--- find-core/src/main/public/static/js/find/app/page/results/results-view.js
+++ find-core/src/main/public/static/js/find/app/page/results/results-view.js
@@ -291,7 +291,7 @@ define([
                 href: href,
                 summary: summary,
                 promotion: isPromotion,
-                date: model.has('date') ? model.get('date').format('YYYY/MM/DD HH:mm:ss') : null,
+                date: model.has('date') ? model.get('date').fromNow() : null,
                 contentType: getContentTypeClass(model)
             }));
 
