c430e8db78449e8ecfb991c60e91134c66e1aa26
Matthew Williamson <matthew.williamson@hpe.com>
2015-12-18 11:18:54 +0000

    Updates to DevConsole tests - NEED TO FIND A BETTER WAY TO LOG IN

--- integration-tests-hosted/src/test/java/com/autonomy/abc/external/DevConsoleITCase.java
+++ integration-tests-hosted/src/test/java/com/autonomy/abc/external/DevConsoleITCase.java
@@ -4,17 +4,22 @@ import com.autonomy.abc.config.HostedTestBase;
 import com.autonomy.abc.config.TestConfig;
 import com.autonomy.abc.selenium.config.ApplicationType;
 import com.autonomy.abc.selenium.users.User;
+import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import static com.autonomy.abc.framework.ABCAssert.assertThat;
 import static org.hamcrest.CoreMatchers.containsString;
 
 public class DevConsoleITCase extends HostedTestBase {
     private final User loginUser;
+    private WebElement launchAppBtn;
 
     public DevConsoleITCase(TestConfig config, String browser, ApplicationType type, Platform platform) {
         super(config, browser, type, platform);
@@ -23,26 +28,33 @@ public class DevConsoleITCase extends HostedTestBase {
         loginUser = config.getUser("twitter");
     }
 
+    @Before
+    public void setUp(){
+        getDriver().findElement(By.id("loginLogout")).click();
+        getElementFactory().getLoginPage();
+        try {
+            loginAs(loginUser);
+        } catch (NoSuchElementException e) {
+            /* This happens because it's expecting to sign into Search Optimizer */
+        }
+        launchAppBtn = new WebDriverWait(getDriver(), 30).until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//a[text()='Launch app']")));
+    }
+
     @Test
     public void testLaunchSearch() throws InterruptedException {
-        button("Admin").click();
-        getElementFactory().getLoginPage();
-        loginAs(loginUser);
+        launchAppBtn.click();
+        getElementFactory().getAnalyticsPage();
         assertThat(getDriver().getCurrentUrl(), containsString("search.havenapps.io"));
     }
 
     @Test
     public void testLaunchFind() throws InterruptedException {
-        button("Find").click();
-        getElementFactory().getFindLoginPage().loginWith(loginUser.getAuthProvider());
+        getDriver().findElement(By.className("hsod-find-button")).click();
+        getElementFactory().getFindPage();
         assertThat(getDriver().getCurrentUrl(), containsString("find.havenapps.io"));
     }
 
     @Test
     @Ignore
     public void testSignUp(){}
-
-    private WebElement button(String button){
-        return getDriver().findElement(By.xpath("//*[text()='"+button+"']/../a"));
-    }
 }
