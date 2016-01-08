9b05eb4fcfc7997b6ce39e721cae5e4976915ba4
Matthew Williamson <matthew.williamson@hpe.com>
2016-01-08 18:03:49 +0000

    Moved methods out of CreateNewIndexPage

--- integration-tests-hosted/src/test/java/com/autonomy/abc/connections/IndexWizardITCase.java
+++ integration-tests-hosted/src/test/java/com/autonomy/abc/connections/IndexWizardITCase.java
@@ -2,16 +2,19 @@ package com.autonomy.abc.connections;
 
 import com.autonomy.abc.config.HostedTestBase;
 import com.autonomy.abc.config.TestConfig;
+import com.autonomy.abc.selenium.actions.wizard.Wizard;
 import com.autonomy.abc.selenium.config.ApplicationType;
 import com.autonomy.abc.selenium.element.FormInput;
+import com.autonomy.abc.selenium.indexes.Index;
 import com.autonomy.abc.selenium.menu.NavBarTabId;
 import com.autonomy.abc.selenium.page.indexes.CreateNewIndexPage;
+import com.autonomy.abc.selenium.page.indexes.wizard.IndexConfigStepTab;
+import com.autonomy.abc.selenium.page.indexes.wizard.IndexNameWizardStepTab;
 import com.autonomy.abc.selenium.util.Errors;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.Platform;
-import org.openqa.selenium.WebElement;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -24,8 +27,6 @@ import static org.hamcrest.Matchers.*;
 public class IndexWizardITCase extends HostedTestBase {
 
     private CreateNewIndexPage createNewIndexPage;
-    private FormInput indexNameInput;
-    private FormInput displayNameInput;
 
     public IndexWizardITCase(TestConfig config, String browser, ApplicationType type, Platform platform) {
         super(config, browser, type, platform);
@@ -39,15 +40,15 @@ public class IndexWizardITCase extends HostedTestBase {
         body = getBody();
 
         createNewIndexPage = getElementFactory().getCreateNewIndexPage();
-        indexNameInput = createNewIndexPage.indexNameInput();
-        displayNameInput = createNewIndexPage.displayNameInput();
     }
 
     @Test
     //CSA-949 - test the input validator which supports only A-Za-Z0-9, space and underscore characters - shouldn't be valid
     public void testIndexDisplayNameValidatorsFail(){
-        indexNameInput.setValue("name");
-        displayNameInput.setValue("displayName #$%");
+        Index index = new Index("name", "displayName #$%");
+
+        index.makeWizard(createNewIndexPage).getCurrentStep().apply();
+        FormInput displayNameInput = createNewIndexPage.getIndexNameWizardStepTab().displayNameInput();
 
         verifyThat(displayNameInput.formGroup(), hasClass("has-error"));
         verifyThat(displayNameInput.getErrorMessage(), containsString(Errors.Index.DISPLAY_NAME));
@@ -56,8 +57,10 @@ public class IndexWizardITCase extends HostedTestBase {
     @Test
     //CSA-949 - test the input validator which supports only A-Za-Z0-9, space and underscore characters - should be valid
     public void testIndexDisplayNameValidatorsPass(){
-        indexNameInput.setValue("name");
-        displayNameInput.setValue("displayName 7894");
+        Index index = new Index("name", "displayName 7894");
+
+        index.makeWizard(createNewIndexPage).getCurrentStep().apply();
+        FormInput displayNameInput = createNewIndexPage.getIndexNameWizardStepTab().displayNameInput();
 
         verifyThat(displayNameInput.formGroup(), not(hasClass("has-error")));
         verifyThat(displayNameInput.getErrorMessage(), nullValue());
@@ -66,29 +69,33 @@ public class IndexWizardITCase extends HostedTestBase {
     @Test
     //CSA-949 - test that the index meta-data with the index display name is set properly according to the summary step
     public void testIndexDisplayNameOnSummary(){
-        String name = "name";
-        String displayName = "displayName 7894";
+        Index index = new Index("name", "displayName 7894");
 
-        indexNameInput.setValue(name);
-        displayNameInput.setValue(displayName);
+        Wizard wizard = index.makeWizard(createNewIndexPage);
+        IndexNameWizardStepTab indexNameWizardStepTab = createNewIndexPage.getIndexNameWizardStepTab();
 
-        verifyThat(indexNameInput.getErrorMessage(), nullValue());
-        verifyThat(displayNameInput.getErrorMessage(), nullValue());
+        wizard.getCurrentStep().apply();
+        verifyThat(indexNameWizardStepTab.indexNameInput().getErrorMessage(), nullValue());
+        verifyThat(indexNameWizardStepTab.displayNameInput().getErrorMessage(), nullValue());
 
-        createNewIndexPage.continueWizardButton().click();
-        createNewIndexPage.continueWizardButton().click();
+        wizard.next();
+        wizard.next();
 
-        WebElement summaryStepIndexDescriptionLabel = createNewIndexPage.summaryStepIndexDescriptionLabel();
-        String expectedSummary = "A new index named "+name+" with "+displayName+" as display name (standard flavor) will be created";
-        verifyThat(summaryStepIndexDescriptionLabel, containsText(expectedSummary));
+        String expectedSummary = "A new index named "+ index.getName() +" with " + index.getDisplayName() + " as display name (standard flavor) will be created";
+        verifyThat(createNewIndexPage.getIndexSummaryStepTab().indexDescriptionLabel(), containsText(expectedSummary));
     }
 
     @Test
     //CSA-1616
     public void testUppercaseFieldNames() {
-        indexNameInput.setValue("name");
+        Index index = new Index("name");
 
-        createNewIndexPage.continueWizardButton().click();
+        Wizard wizard = index.makeWizard(createNewIndexPage);
+
+        wizard.getCurrentStep().apply();
+        wizard.next();
+
+        IndexConfigStepTab indexConfigStepTab = createNewIndexPage.getIndexConfigStepTab();
 
         final List<String> capitals = new ArrayList<String>() {{
             add("London");
@@ -102,18 +109,18 @@ public class IndexWizardITCase extends HostedTestBase {
             }
         }};
 
-        createNewIndexPage.setIndexFields(capitals);
-        FormInput input = createNewIndexPage.indexFieldsInput();
+        indexConfigStepTab.setIndexFields(capitals);
+        FormInput input = indexConfigStepTab.indexFieldsInput();
         verifyThat(input.getErrorMessage(), containsString(Errors.Index.FIELD_NAMES));
 
-        createNewIndexPage.setIndexFields(lowercase);
+        indexConfigStepTab.setIndexFields(lowercase);
         verifyThat(input.getErrorMessage(), nullValue());
 
-        createNewIndexPage.setParametricFields(capitals);
-        input = createNewIndexPage.parametricFieldsInput();
+        indexConfigStepTab.setParametricFields(capitals);
+        input = indexConfigStepTab.parametricFieldsInput();
         verifyThat(input.getErrorMessage(), containsString(Errors.Index.FIELD_NAMES));
 
-        createNewIndexPage.setParametricFields(lowercase);
+        indexConfigStepTab.setParametricFields(lowercase);
         verifyThat(input.getErrorMessage(), nullValue());
     }
 
--- mockui/src/main/java/com/autonomy/abc/selenium/page/indexes/CreateNewIndexPage.java
+++ mockui/src/main/java/com/autonomy/abc/selenium/page/indexes/CreateNewIndexPage.java
@@ -1,20 +1,14 @@
 package com.autonomy.abc.selenium.page.indexes;
 
-import com.autonomy.abc.selenium.element.ChevronContainer;
-import com.autonomy.abc.selenium.element.Collapsible;
-import com.autonomy.abc.selenium.element.FormInput;
-import com.autonomy.abc.selenium.page.indexes.wizard.IndexConfigStepTab;
 import com.autonomy.abc.selenium.page.SAASPageBase;
+import com.autonomy.abc.selenium.page.indexes.wizard.IndexConfigStepTab;
 import com.autonomy.abc.selenium.page.indexes.wizard.IndexNameWizardStepTab;
-import com.autonomy.abc.selenium.util.ElementUtil;
-import org.apache.commons.lang3.StringUtils;
+import com.autonomy.abc.selenium.page.indexes.wizard.IndexSummaryStepTab;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-
-import java.util.List;
 
 public class CreateNewIndexPage extends SAASPageBase {
     public CreateNewIndexPage(WebDriver driver) {
@@ -26,79 +20,7 @@ public class CreateNewIndexPage extends SAASPageBase {
         return new CreateNewIndexPage(driver);
     }
 
-    /* index name step */
-    public FormInput indexNameInput() {
-        return new FormInput(findElement(By.cssSelector("[name='indexName']")), getDriver());
-    }
-
-    public FormInput displayNameInput() {
-        return new FormInput(findElement(By.cssSelector("[name='indexDisplayName']")), getDriver());
-    }
-
-    /* index configuration step */
-    public void setIndexFields(List<String> indexFields) {
-        advancedOptions().expand();
-        String joined = StringUtils.join(indexFields, ",");
-        indexFieldsInput().setValue(joined);
-    }
-
-    public void setParametricFields(List<String> parametricFields) {
-        advancedOptions().expand();
-        String joined = StringUtils.join(parametricFields, ",");
-        parametricFieldsInput().setValue(joined);
-    }
-
-    public FormInput indexFieldsInput() {
-        return fieldInput("indexFields");
-    }
-
-    public FormInput parametricFieldsInput() {
-        return fieldInput("parametricFields");
-    }
-
-    private FormInput fieldInput(String inputType) {
-        WebElement input = findElement(By.cssSelector("[for='" + inputType + "'] + div input"));
-        return new FormInput(input, getDriver());
-    }
-
-    public WebElement advancedIndexFieldsInformation() {
-        return findElement(By.cssSelector("[for='indexFields'] + div i"));
-    }
-
-    public WebElement advancedParametricFieldsInformation() {
-        return findElement(By.cssSelector("[for='parametricFields'] + div i"));
-    }
-
-    public Collapsible advancedOptions() {
-        WebElement panel = findElement(By.className("panel"));
-        return new ChevronContainer(ElementUtil.ancestor(panel, 1));
-    }
-
-    /* summary step */
-    public WebElement summaryStepIndexDescriptionLabel() {
-        return findElement(By.id("indexWizardSummaryStepDescription")).findElement(By.tagName("label"));
-    }
-
-    public WebElement summaryStepIndexConfigurationsLabel() {
-        return findElement(By.id("indexWizardSummaryStepConfigurations")).findElement(By.tagName("label"));
-    }
-
     /* navigation */
-    public WebElement chooseIndexNameHeader() {
-        return chooseTab(1);
-    }
-
-    public WebElement indexConfigurationHeader() {
-        return chooseTab(2);
-    }
-
-    public WebElement summaryHeader() {
-        return chooseTab(3);
-    }
-
-    private WebElement chooseTab(int tab) {
-        return findElement(By.cssSelector("[role='tablist'] li:nth-child(" + tab + ")"));
-    }
 
     public WebElement continueWizardButton() {
         return menuButton("Next");
@@ -126,5 +48,9 @@ public class CreateNewIndexPage extends SAASPageBase {
 
     public IndexConfigStepTab getIndexConfigStepTab() {
         return IndexConfigStepTab.make(getDriver());
+    }
+
+    public IndexSummaryStepTab getIndexSummaryStepTab() {
+        return IndexSummaryStepTab.make(getDriver());
     }
 }
