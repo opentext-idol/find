6bd5dc12f7b7a57a9858df634d3149a7dbd86372
Fred Collman <frederick.collman@hpe.com>
2016-01-18 12:03:47 +0000

    Move SearchOptimizer-specific code from ApplicationType to SearchOptimizerApplication using generic SafeClassLoader

--- mockui/src/main/java/com/autonomy/abc/selenium/application/ApplicationType.java
+++ mockui/src/main/java/com/autonomy/abc/selenium/application/ApplicationType.java
@@ -4,30 +4,20 @@ package com.autonomy.abc.selenium.application;
 // allows shared tests to do a simple "if hosted then x else y"
 // instead of excessively complicated polymorphic method calls
 public enum ApplicationType {
-	ON_PREM("On Premise", "com.autonomy.abc.selenium.application.OPISOApplication"),
-	HOSTED("Hosted", "com.autonomy.abc.selenium.application.HSOApplication");
+	ON_PREM("On Premise"),
+	HOSTED("Hosted");
 
 	private final String name;
-	private final String className;
 
-	ApplicationType(final String name, final String className) {
+	ApplicationType(final String name) {
 		this.name = name;
-		this.className = className;
 	}
 
 	public String getName() {
 		return name;
 	}
 
-	// instead exposed as factory method Application.ofType
-	SearchOptimizerApplication makeSearchApplication() {
-		try {
-			return (SearchOptimizerApplication) Class.forName(className).newInstance();
-		} catch (InstantiationException | IllegalAccessException | ClassNotFoundException e) {
-			throw new IllegalStateException("Could not create Application object - check that the correct mockui package is included in the POM", e);
-		}
-	}
-
+	// should only be used for parsing config
 	public static ApplicationType fromString(String type) {
 		return type.toLowerCase().equals("hosted") ? HOSTED : ON_PREM;
 	}
--- mockui/src/main/java/com/autonomy/abc/selenium/application/SearchOptimizerApplication.java
+++ mockui/src/main/java/com/autonomy/abc/selenium/application/SearchOptimizerApplication.java
@@ -6,8 +6,19 @@ import com.autonomy.abc.selenium.page.ElementFactory;
 import com.autonomy.abc.selenium.promotions.PromotionService;
 import com.autonomy.abc.selenium.search.SearchService;
 import com.autonomy.abc.selenium.users.UserService;
+import com.autonomy.abc.selenium.util.Factory;
+import com.autonomy.abc.selenium.util.SafeClassLoader;
+
+import java.util.EnumMap;
+import java.util.Map;
 
 public abstract class SearchOptimizerApplication<T extends ElementFactory> implements Application<T> {
+    private final static Map<ApplicationType, Factory<? extends SearchOptimizerApplication>> FACTORY_MAP = new EnumMap<>(ApplicationType.class);
+
+    static {
+        FACTORY_MAP.put(ApplicationType.HOSTED, new SafeClassLoader<>(SearchOptimizerApplication.class, "com.autonomy.abc.selenium.application.HSOApplication"));
+        FACTORY_MAP.put(ApplicationType.ON_PREM, new SafeClassLoader<>(SearchOptimizerApplication.class, "com.autonomy.abc.selenium.application.OPISOApplication"));
+    }
 
     public abstract PromotionService createPromotionService(ElementFactory elementFactory);
 
@@ -15,17 +26,17 @@ public abstract class SearchOptimizerApplication<T extends ElementFactory> imple
 
     public abstract UserConfigParser getUserConfigParser();
 
-    public abstract ApplicationType getType();
-
-    public static SearchOptimizerApplication ofType(ApplicationType type) {
-        return type.makeSearchApplication();
-    }
-
     public KeywordService createKeywordService(ElementFactory elementFactory) {
         return new KeywordService(this, elementFactory);
     }
 
     public SearchService createSearchService(ElementFactory elementFactory) {
         return new SearchService(this, elementFactory);
+    }
+
+    public abstract ApplicationType getType();
+
+    public static SearchOptimizerApplication ofType(ApplicationType type) {
+        return FACTORY_MAP.get(type).create();
     }
 }
