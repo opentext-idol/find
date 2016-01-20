862684fb686c831f4adbf8ec2da547b3e8ae0f49
Daniel Grayling <daniel.grayling@hp.com>
2016-01-20 16:42:51 +0000

    Switching implemented.

--- find-core/src/main/public/static/js/find/app/page/search/results/results-view-container.js
+++ find-core/src/main/public/static/js/find/app/page/search/results/results-view-container.js
@@ -8,21 +8,49 @@ define([
     return Backbone.View.extend({
         template: _.template(template),
 
+        hideViews: function() {
+            _.each(this.views, function(view) {
+                view.$el.addClass('hide');
+            });
+        },
+
+        selectView: function(name) {
+            this.hideViews();
+            this.views[name].$el.removeClass('hide');
+        },
+
+        events: {
+            'click .result-view-type': function(e) {
+                this.selectView(($(e.currentTarget).attr('data-type')));
+            }
+        },
+
         ResultsView: $.noop,
 
         initialize: function(options) {
-            this.resultsView = new this.ResultsView({
-                documentsCollection: options.documentsCollection,
-                entityCollection: options.entityCollection,
-                indexesCollection: options.indexesCollection,
-                queryModel: options.queryModel
-            });
+            this.views = {
+                list: new this.ResultsView({
+                    documentsCollection: options.documentsCollection,
+                    entityCollection: options.entityCollection,
+                    indexesCollection: options.indexesCollection,
+                    queryModel: options.queryModel
+                }),
+                map: new this.ResultsView({
+                    documentsCollection: new Backbone.Collection(),
+                    entityCollection: new Backbone.Collection(),
+                    indexesCollection: new Backbone.Collection(),
+                    queryModel: new Backbone.Model(),
+                })
+            };
         },
 
         render: function() {
             this.$el.html(this.template);
 
-            this.resultsView.setElement(this.$('.results-list-container')).render();
+            this.views.list.setElement(this.$('.results-list-container')).render();
+            this.views.map.setElement(this.$('.results-map-container')).render();
+
+            this.selectView('list');
         }
 
     });
