68d853607d5f9861f37a6465c08077f988e426f0
Jon Soul <jonathan.soul@hpe.com>
2016-01-27 18:40:56 +0000

    [BIFHI-52] Add better integration test coverage for saved queries [rev: Dan Grayling]

--- find-core/src/test/java/com/hp/autonomy/frontend/find/core/savedsearches/AbstractSavedQueryServiceIT.java
+++ find-core/src/test/java/com/hp/autonomy/frontend/find/core/savedsearches/AbstractSavedQueryServiceIT.java
@@ -6,6 +6,7 @@
 package com.hp.autonomy.frontend.find.core.savedsearches;
 
 import com.hp.autonomy.frontend.find.core.test.AbstractFindIT;
+import org.joda.time.DateTime;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.annotation.DirtiesContext;
@@ -18,13 +19,13 @@ import static org.hamcrest.collection.IsEmptyCollection.empty;
 import static org.hamcrest.core.Is.isA;
 import static org.junit.Assert.*;
 
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 public abstract class AbstractSavedQueryServiceIT extends AbstractFindIT {
 
     @SuppressWarnings("SpringJavaAutowiringInspection")
     @Autowired private SavedQueryService savedQueryService;
 
     @Test
-    @DirtiesContext
     public void createFetchDelete() {
         final String title = "Any old saved search";
         final SavedQuery savedQuery = new SavedQuery.Builder()
@@ -51,5 +52,58 @@ public abstract class AbstractSavedQueryServiceIT extends AbstractFindIT {
     @Transactional
     public void getAllReturnsNothing() throws Exception {
         assertThat(savedQueryService.getAll(), is(empty()));
+    }
+
+    @Test
+    public void checkUserAuditDataInserted() {
+        final SavedQuery savedQuery = new SavedQuery.Builder()
+                .setTitle("title")
+                .build();
+
+        savedQueryService.create(savedQuery);
+
+        assertNotNull(savedQuery.getId());
+        assertThat(savedQuery.getUser().getUserId(), isA(Long.class));
+        assertNotNull(savedQuery.getUser().getUserId());
+    }
+
+    @Test
+    public void checkTimeAuditDataInsertedUpdated() {
+        SavedQuery savedQuery = new SavedQuery.Builder()
+                .setTitle("title")
+                .build();
+
+        savedQuery = savedQueryService.create(savedQuery);
+        assertNotNull(savedQuery.getId());
+        assertThat(savedQuery.getDateCreated(), isA(DateTime.class));
+        assertThat(savedQuery.getDateModified(), isA(DateTime.class));
+        assertTrue(savedQuery.getDateCreated().isEqual(savedQuery.getDateModified().toInstant()));
+        // Safe to assume completed in a day
+        // TODO: mock out the datetime service used by spring auditing to check this properly
+        assertTrue(savedQuery.getDateCreated().plusHours(1).isAfterNow());
+
+        savedQuery.setQueryText("*");
+        savedQuery = savedQueryService.update(savedQuery);
+        assertThat(savedQuery.getDateCreated(), isA(DateTime.class));
+        assertThat(savedQuery.getDateModified(), isA(DateTime.class));
+        assertTrue(savedQuery.getDateModified().isAfter(savedQuery.getDateCreated().toInstant()));
+    }
+
+    @Test
+    public void checkUserNotDuplicated() {
+        final SavedQuery savedQuery1 = new SavedQuery.Builder()
+                .setTitle("title1")
+                .build();
+
+        final SavedQuery savedQuery2 = new SavedQuery.Builder()
+                .setTitle("title2")
+                .build();
+
+        savedQueryService.create(savedQuery1);
+        savedQueryService.create(savedQuery2);
+
+        assertNotNull(savedQuery1.getUser().getUserId());
+        assertNotNull(savedQuery2.getUser().getUserId());
+        assertEquals(savedQuery1.getUser().getUserId(), savedQuery2.getUser().getUserId());
     }
 }
