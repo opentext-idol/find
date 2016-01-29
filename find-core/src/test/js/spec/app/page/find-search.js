b5c3d61c3470047a8dfd7e3536ad284e461d3514
Matthew Gordon <matthew.gordon2@hp.com>
2016-01-29 18:12:18 +0000

    Removed unit tests which should have been removed when we made the search page animation one way. [rev: none]

--- find-core/src/test/js/spec/app/page/find-search.js
+++ find-core/src/test/js/spec/app/page/find-search.js
@@ -20,32 +20,12 @@ define([
             expect($tabbedSearchContainer.css('display')).not.toEqual('none');
         });
 
-        it('should hide the service view when the inputText is the empty string', function() {
-            this.findSearch.render();
-
-            this.findSearch.searchModel.set('inputText', '');
-
-            var $tabbedSearchContainer = this.findSearch.$('.tabbed-search-container');
-
-            expect($tabbedSearchContainer.css('display')).toEqual('none');
-        });
-
         it('should animate the input view when the inputText is not the empty string', function() {
             this.findSearch.render();
 
             this.findSearch.searchModel.set('inputText', 'text');
 
             var $animatedContainer = this.findSearch.$('.animated-container');
-
-            expect($animatedContainer).not.toEqual([]);
-        });
-
-        it('should reverse-animate the input view when the inputText is the empty string', function() {
-            this.findSearch.render();
-
-            this.findSearch.searchModel.set('inputText', '');
-
-            var $animatedContainer = this.findSearch.$('.reverse-animated-container');
 
             expect($animatedContainer).not.toEqual([]);
         });
