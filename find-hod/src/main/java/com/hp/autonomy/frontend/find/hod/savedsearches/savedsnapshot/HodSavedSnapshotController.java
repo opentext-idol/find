838370d283324b9951c9348a2b2d323e7170b67a
Daniel Grayling <daniel.grayling@hp.com>
2016-02-01 18:54:18 +0000

    Moved subclasses to the right modules.

--- find-hod/src/main/java/com/hp/autonomy/frontend/find/hod/savedsearches/savedsnapshot/HodSavedSnapshotController.java
+++ find-hod/src/main/java/com/hp/autonomy/frontend/find/hod/savedsearches/savedsnapshot/HodSavedSnapshotController.java
@@ -1,11 +1,14 @@
-package com.hp.autonomy.frontend.find.core.savedsearches.savedsnapshot;
+package com.hp.autonomy.frontend.find.hod.savedsearches.savedsnapshot;
 
 import com.hp.autonomy.frontend.find.core.savedsearches.EmbeddableIndex;
+import com.hp.autonomy.frontend.find.core.savedsearches.savedsnapshot.SavedSnapshot;
+import com.hp.autonomy.frontend.find.core.savedsearches.savedsnapshot.SavedSnapshotController;
+import com.hp.autonomy.frontend.find.core.savedsearches.savedsnapshot.SavedSnapshotService;
 import com.hp.autonomy.hod.client.api.resource.ResourceIdentifier;
 import com.hp.autonomy.hod.client.error.HodErrorException;
 import com.hp.autonomy.searchcomponents.core.search.DocumentsService;
+import com.hp.autonomy.searchcomponents.hod.search.HodQueryRestrictions;
 import com.hp.autonomy.searchcomponents.hod.search.HodSearchResult;
-import com.hp.autonomy.searchcomponents.idol.search.IdolQueryRestrictions;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,18 +19,18 @@ import java.util.Set;
 
 @Controller
 @RequestMapping(SavedSnapshotController.PATH)
-public class IdolSavedSnapshotController extends SavedSnapshotController {
+public class HodSavedSnapshotController extends SavedSnapshotController<ResourceIdentifier, HodSearchResult, HodErrorException> {
 
     @Autowired
-    public IdolSavedSnapshotController(SavedSnapshotService service, DocumentsService<ResourceIdentifier, HodSearchResult, HodErrorException> documentsService) {
+    public HodSavedSnapshotController(SavedSnapshotService service, DocumentsService<ResourceIdentifier, HodSearchResult, HodErrorException> documentsService) {
         super(service, documentsService);
     }
 
-    private List<String> getDatabases(Set<EmbeddableIndex> indexes) {
-        List<String> databases = new ArrayList<>();
+    private List<ResourceIdentifier> getDatabases(Set<EmbeddableIndex> indexes) {
+        List<ResourceIdentifier> databases = new ArrayList<>();
 
         for(EmbeddableIndex index: indexes) {
-            databases.add(index.getName());
+            databases.add(new ResourceIdentifier(index.getDomain(), index.getName()));
         }
 
         return databases;
@@ -35,7 +38,7 @@ public class IdolSavedSnapshotController extends SavedSnapshotController {
 
     @Override
     protected String getStateToken(final SavedSnapshot snapshot) throws Exception {
-        IdolQueryRestrictions.Builder queryRestrictionsBuilder = new IdolQueryRestrictions.Builder()
+        HodQueryRestrictions.Builder queryRestrictionsBuilder = new HodQueryRestrictions.Builder()
                 .setDatabases(this.getDatabases(snapshot.getIndexes()))
                 .setQueryText(this.getQueryText(snapshot)).setFieldText(this.getFieldText(snapshot.getParametricValues()))
                 .setMaxDate(snapshot.getMaxDate())
