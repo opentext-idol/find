a2d1c256a15b8b7dac3355a2b8fa6cc2d0927ea5
Fred Collman <frederick.collman@hpe.com>
2016-02-02 13:53:37 +0000

    Allow HSODFind to have its window changed
    This is only temporary - eventually will remove Application.inWindow(window)

--- mockui/src/main/java/com/autonomy/abc/selenium/application/Application.java
+++ mockui/src/main/java/com/autonomy/abc/selenium/application/Application.java
@@ -6,5 +6,6 @@ import org.openqa.selenium.WebDriver;
 public interface Application<T> {
     T elementFactory();
     ApplicationType getType();
+    // TODO: remove this once Applications are initialized with a Window
     Application<T> inWindow(Window window);
 }
--- mockui/src/main/java/com/autonomy/abc/selenium/application/HSODFind.java
+++ mockui/src/main/java/com/autonomy/abc/selenium/application/HSODFind.java
@@ -4,12 +4,11 @@ import com.autonomy.abc.selenium.control.Window;
 import com.autonomy.abc.selenium.page.HSOElementFactory;
 
 public class HSODFind implements Application<HSOElementFactory> {
-    private final Window window;
-    private final HSOElementFactory factory;
+    private Window window;
+    private HSOElementFactory factory;
 
     public HSODFind(Window window) {
-        this.window = window;
-        this.factory = new HSOElementFactory(window.getSession().getDriver());
+        inWindow(window);
     }
 
     @Override
@@ -24,6 +23,8 @@ public class HSODFind implements Application<HSOElementFactory> {
 
     @Override
     public Application<HSOElementFactory> inWindow(Window window) {
+        this.window = window;
+        this.factory = new HSOElementFactory(window.getSession().getDriver());
         return this;
     }
 }
