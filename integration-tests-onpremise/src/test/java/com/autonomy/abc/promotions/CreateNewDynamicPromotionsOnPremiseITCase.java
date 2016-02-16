8d21d24e1c3b0e6dd05356a58920a856e3992a93
Fred Collman <frederick.collman@hpe.com>
2016-02-16 13:44:32 +0000

    Split testDynamicPromotionCreation into hosted/on-prem
    Hosted version requires checking the dial is present, on-prem version requires spotlight types

--- integration-tests-onpremise/src/test/java/com/autonomy/abc/promotions/CreateNewDynamicPromotionsOnPremiseITCase.java
+++ integration-tests-onpremise/src/test/java/com/autonomy/abc/promotions/CreateNewDynamicPromotionsOnPremiseITCase.java
@@ -26,6 +26,8 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import java.util.List;
 
 import static com.autonomy.abc.framework.ABCAssert.assertThat;
+import static com.autonomy.abc.matchers.CommonMatchers.containsItems;
+import static com.autonomy.abc.matchers.ElementMatchers.disabled;
 import static org.hamcrest.Matchers.*;
 import static org.junit.Assert.fail;
 import static org.openqa.selenium.lift.Matchers.displayed;
@@ -40,6 +42,7 @@ public class CreateNewDynamicPromotionsOnPremiseITCase extends ABCTestBase {
 	private CreateNewPromotionsPage dynamicPromotionsPage;
 	private PromotionService promotionService;
     private SearchService searchService;
+	private TriggerForm triggerForm;
 
 	@Before
 	public void setUp() throws InterruptedException {
@@ -175,5 +178,59 @@ public class CreateNewDynamicPromotionsOnPremiseITCase extends ABCTestBase {
 
 		PromotionsDetailPage promotionsDetailPage = promotionService.goToDetails("phrase");
 		assertThat("Promotion has been created in the wrong language", promotionsDetailPage.getLanguage(), is(Language.URDU.toString()));
+	}
+
+	@Test
+	public void testDynamicPromotionCreation() {
+		searchPage = searchService.search(new SearchQuery("lapin").withFilter(new LanguageFilter(Language.FRENCH)));
+
+		final String firstDocTitle = searchPage.getSearchResult(1).getTitleString();
+		searchPage.promoteThisQueryButton().click();
+		dynamicPromotionsPage = getElementFactory().getCreateNewPromotionsPage();
+		Waits.loadOrFadeWait();
+
+		assertThat("Wrong URL", getDriver().getCurrentUrl().contains("promotions/create-dynamic/"));
+		assertThat(dynamicPromotionsPage.spotlightType(Promotion.SpotlightType.HOTWIRE), displayed());
+		assertThat(dynamicPromotionsPage.getCurrentStepTitle(), containsString("Spotlight type"));
+		assertThat(dynamicPromotionsPage.continueButton(), disabled());
+
+		dynamicPromotionsPage.spotlightType(Promotion.SpotlightType.TOP_PROMOTIONS).click();
+		dynamicPromotionsPage.continueButton().click();
+		Waits.loadOrFadeWait();
+		triggerForm = dynamicPromotionsPage.getTriggerForm();
+
+		assertThat(triggerForm.addButton(), displayed());
+		assertThat(dynamicPromotionsPage.getCurrentStepTitle(), containsString("Trigger words"));
+		assertThat(dynamicPromotionsPage.finishButton(), disabled());
+		assertThat(triggerForm.addButton(), disabled());
+		assertThat(triggerForm.getNumberOfTriggers(), is(0));
+
+		checkAddTrigger("rabbit");
+		checkAddTrigger("bunny");
+		checkAddTrigger("hare");
+
+		// Hare is not a word for bunny
+		triggerForm.removeTrigger("hare");
+		assertThat(triggerForm.getNumberOfTriggers(), is(2));
+		assertThat(triggerForm.getTriggersAsStrings(), hasItems("bunny", "rabbit"));
+		assertThat(triggerForm.getTriggersAsStrings(), not(hasItem("hare")));
+
+		dynamicPromotionsPage.finishButton().click();
+		Waits.loadOrFadeWait();
+		searchPage.waitForPromotionsLoadIndicatorToDisappear();
+
+		assertThat(searchPage.getHeadingSearchTerm(), is("bunny rabbit"));
+		assertThat(searchPage.getPromotedDocumentTitles(false).get(0), is(firstDocTitle));
+	}
+
+	private void checkAddTrigger(String trigger) {
+		List<String> beforeTriggers = triggerForm.getTriggersAsStrings();
+
+		triggerForm.addTrigger(trigger);
+		List<String> afterTriggers = triggerForm.getTriggersAsStrings();
+
+		assertThat(afterTriggers, hasSize(beforeTriggers.size() + 1));
+		assertThat(afterTriggers, hasItem(trigger));
+		assertThat(afterTriggers, containsItems(beforeTriggers));
 	}
 }
