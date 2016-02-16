585e817836d70270f1295cc3ed70deff3eade2bd
Matthew Gordon <matthew.gordon2@hp.com>
2016-02-16 15:15:38 +0000

    Added missing "abstract" keyword to integration test. [rev: jon.soul]
    The AbstractFindControllerITCase was not declared abstract. This meant
    that the core integration tests failed, since this test is not
    meant to be run outside of a concrete implementation. Adding abstract
    causes it to be ignored by the test runner.
    
    (Cherry picked into BIFHI from
    15462be5856d95fe443391f590272b0daf715ed9.)

--- find-core/src/test/java/com/hp/autonomy/frontend/find/core/web/AbstractFindControllerIT.java
+++ find-core/src/test/java/com/hp/autonomy/frontend/find/core/web/AbstractFindControllerIT.java
@@ -11,7 +11,7 @@ import org.junit.Test;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-public class AbstractFindControllerIT extends AbstractFindIT {
+public abstract class AbstractFindControllerIT extends AbstractFindIT {
     @Test
     public void index() throws Exception {
         mockMvc.perform(get("/"))
