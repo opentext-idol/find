4674037f20f295a3676c1be7b8046f343b89bdc5
Alex Scown <alex.scown@hpe.com>
2016-02-25 14:40:20 +0000

    [CCUK-3425] Split settings page
    Factor out code common to both pages. Create two settings pages
    that inherit from teh common code.
    
    [rev: ivo.miller]

---
+++ find-idol/src/main/public/static/js/find/app/page/find-settings-page.js
@@ -0,0 +1,22 @@
+/*
+ * Copyright 2014-2016 Hewlett-Packard Development Company, L.P.
+ * Licensed under the MIT License (the "License"); you may not use this file except in compliance with the License.
+ */
+
+define([
+    'find/app/page/abstract-find-settings-page'
+], function(SettingsPage) {
+
+    return SettingsPage.extend({
+        initializeWidgets: function() {
+            this.widgetGroups = [
+                [
+
+                ], [
+
+                ]
+            ];
+        }
+    });
+
+});
