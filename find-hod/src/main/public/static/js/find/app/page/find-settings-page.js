732576165d4d0f778896526cba9263e5eccb1f6b
Alex Scown <alex.scown@hpe.com>
2016-02-25 14:40:20 +0000

    [CCUK-3425] Split settings page
    Factor out code common to both pages. Create two settings pages
    that inherit from teh common code.
    
    [rev: ivo.miller]

--- find-hod/src/main/public/static/js/find/app/page/find-settings-page.js
+++ find-hod/src/main/public/static/js/find/app/page/find-settings-page.js
@@ -4,77 +4,12 @@
  */
 
 define([
-    'i18n!find/nls/bundle',
-    'find/app/vent',
-    'find/app/router',
-    'settings/js/settings-page',
-    'find/app/model/config',
+    'find/app/page/abstract-find-settings-page',
     'find/app/page/settings/iod-widget',
-    'settings/js/widgets/single-user-widget',
-    'find/app/page/settings/validate-on-save-modal',
-    'find/app/util/confirm-view'
-], function(i18n, vent, router, SettingsPage, configModel, IodWidget, SingleUserWidget, ValidateOnSaveModal, Confirm) {
-
-    var serverStrings = function() {
-        return {
-            databaseLabel: i18n['settings.database'],
-            hostPlaceholder: i18n['placeholder.hostname'],
-            portPlaceholder: i18n['placeholder.port'],
-            usernameLabel: i18n['settings.username'],
-            validateButton: i18n['settings.test'],
-            validateFailed: i18n['settings.test.failed'],
-            validateHostBlank: i18n['settings.test.hostBlank'],
-            validatePasswordBlank: i18n['settings.test.passwordBlank'],
-            validateUsernameBlank: i18n['settings.test.usernameBlank'],
-            validateSuccess: i18n['settings.test.ok']
-        };
-    };
-
-    var urlRoot = /\bconfig\/[\/]*$/.test(window.location.pathname) ? '../api/config/config/' : '../api/useradmin/config/';
+    'settings/js/widgets/single-user-widget'
+], function(SettingsPage, IodWidget, SingleUserWidget) {
 
     return SettingsPage.extend({
-        SaveModalConstructor: ValidateOnSaveModal,
-        configModel: configModel,
-        router: router,
-        routeRoot: 'find/settings',
-        scrollSelector: '.body',
-        vent: vent,
-        validateUrl: urlRoot + 'config-validation',
-        groupClass: 'col-md-4',
-        strings: {
-            cancelButton: i18n['settings.cancel'],
-            cancelCancel: i18n['app.cancel'],
-            cancelMessage: i18n['settings.cancel.message'],
-            cancelOk: i18n['app.ok'],
-            cancelTitle: i18n['settings.cancel.title'],
-            confirmUnload: i18n['settings.unload.confirm'],
-            requiredFields: i18n['settings.requiredFields'],
-            restoreButton: i18n['settings.restoreChanges'],
-            saveButton: i18n['settings.save'],
-            title: i18n['app.settings'],
-            description: function(configEnv, configPath) {
-                return i18n['settings.description'](configEnv, configPath);
-            },
-            saveModal: {
-                cancel: i18n['settings.cancel'],
-                close: i18n['settings.close'],
-                confirm: i18n['settings.save.confirm'],
-                errorThrown: i18n['settings.save.errorThrown'],
-                failure: i18n['settings.save.failure'],
-                failureAnd: i18n['settings.save.failure.and'],
-                failureValidationMessage: i18n['settings.save.failure.validation.message'],
-                failureText: i18n['settings.save.failure.text'],
-                retry: i18n['settings.retry'],
-                retypePass: i18n['settings.save.retypePassword'],
-                save: i18n['settings.save'],
-                saving: i18n['settings.save.saving'],
-                success: i18n['settings.save.success'],
-                successMessage: i18n['settings.save.success.message'],
-                title: i18n['settings.save.confirm.title'],
-                unknown: i18n['settings.save.unknown']
-            }
-        },
-
         initializeWidgets: function() {
             this.widgetGroups = [
                 [
@@ -82,7 +17,7 @@ define([
                         configItem: 'iod',
                         isOpened: true,
                         title: 'IoD settings',
-                        strings: _.extend(serverStrings(), {
+                        strings: _.extend(this.serverStrings(), {
                             application: i18n['settings.iod.application'],
                             apiKey: i18n['settings.iod.apiKey'],
                             domain: i18n['settings.iod.domain'],
@@ -115,27 +50,8 @@ define([
                     })
                 ]
             ];
-        },
-
-        handleCancelButton: function(e) {
-            e.preventDefault();
-
-            new Confirm({
-                cancelClass: 'btn-default',
-                cancelIcon: 'hp-icon hp-fw hp-close',
-                cancelText: this.strings.cancelCancel,
-                okText: this.strings.cancelOk,
-                okClass: 'btn-primary',
-                okIcon: 'hp-icon hp-fw hp-reset',
-                message: this.strings.cancelMessage,
-                title: this.strings.cancelTitle,
-                hiddenEvent: 'hidden.bs.modal',
-                okHandler: _.bind(function() {
-                    this.loadFromConfig();
-                }, this)
-            });
-
         }
+
     });
 
 });
