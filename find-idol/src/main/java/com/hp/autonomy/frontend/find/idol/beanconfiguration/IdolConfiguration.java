59932b4f737b567efc82624d37efdcc524f22af2
Alex Scown <alex.scown@hpe.com>
2016-02-26 12:49:05 +0000

    [CCUK-3425] Fix ObjectMappers
    We only want the configuration filter mixins when using the config file, or it blows up.
    
    [rev: ivo.miller]

--- find-idol/src/main/java/com/hp/autonomy/frontend/find/idol/beanconfiguration/IdolConfiguration.java
+++ find-idol/src/main/java/com/hp/autonomy/frontend/find/idol/beanconfiguration/IdolConfiguration.java
@@ -5,21 +5,31 @@
 
 package com.hp.autonomy.frontend.find.idol.beanconfiguration;
 
+import com.autonomy.aci.client.annotations.IdolAnnotationsProcessorFactory;
 import com.autonomy.aci.client.services.AciService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.databind.ser.FilterProvider;
+import com.hp.autonomy.frontend.configuration.Authentication;
+import com.hp.autonomy.frontend.configuration.CommunityAuthenticationValidator;
+import com.hp.autonomy.frontend.configuration.CommunityService;
+import com.hp.autonomy.frontend.configuration.CommunityServiceImpl;
 import com.hp.autonomy.frontend.configuration.ConfigService;
 import com.hp.autonomy.frontend.configuration.ConfigurationFilterMixin;
 import com.hp.autonomy.frontend.configuration.ServerConfig;
+import com.hp.autonomy.frontend.configuration.ServerConfigValidator;
+import com.hp.autonomy.frontend.find.idol.configuration.IdolAuthenticationMixins;
 import com.hp.autonomy.frontend.find.idol.configuration.IdolFindConfig;
 import com.hp.autonomy.frontend.find.idol.configuration.IdolFindConfigFileService;
 import com.hp.autonomy.idolutils.processors.AciResponseJaxbProcessorFactory;
+import com.hp.autonomy.searchcomponents.idol.configuration.QueryManipulationValidator;
 import com.hp.autonomy.searchcomponents.idol.view.configuration.ViewConfig;
+import com.hp.autonomy.searchcomponents.idol.view.configuration.ViewConfigValidator;
 import com.hp.autonomy.user.UserService;
 import com.hp.autonomy.user.UserServiceImpl;
 import org.jasypt.util.text.TextEncryptor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
@@ -29,25 +39,28 @@ public class IdolConfiguration {
     @Autowired
     private TextEncryptor textEncryptor;
 
-    @Autowired
-    private FilterProvider filterProvider;
-
     @SuppressWarnings("SpringJavaAutowiringInspection")
     @Bean
     @Autowired
     public ObjectMapper jacksonObjectMapper(final Jackson2ObjectMapperBuilder builder) {
         final ObjectMapper objectMapper = builder.createXmlMapper(false).build();
-        objectMapper.enable(SerializationFeature.INDENT_OUTPUT);
-        objectMapper.addMixIn(ServerConfig.class, ConfigurationFilterMixin.class);
-        objectMapper.addMixIn(ViewConfig.class, ConfigurationFilterMixin.class);
-        objectMapper.addMixIn(IdolFindConfig.class, ConfigurationFilterMixin.class);
+
+        objectMapper.addMixIn(Authentication.class, IdolAuthenticationMixins.class);
 
         return objectMapper;
     }
 
+    @SuppressWarnings("SpringJavaAutowiringInspection")
     @Bean
     @Autowired
-    public IdolFindConfigFileService configFileService(final ObjectMapper objectMapper) {
+    public IdolFindConfigFileService configFileService(final Jackson2ObjectMapperBuilder builder, final FilterProvider filterProvider) {
+        final ObjectMapper objectMapper = builder.createXmlMapper(false).build();
+        objectMapper.enable(SerializationFeature.INDENT_OUTPUT);
+        objectMapper.addMixIn(Authentication.class, IdolAuthenticationMixins.class);
+        objectMapper.addMixIn(ServerConfig.class, ConfigurationFilterMixin.class);
+        objectMapper.addMixIn(ViewConfig.class, ConfigurationFilterMixin.class);
+        objectMapper.addMixIn(IdolFindConfig.class, ConfigurationFilterMixin.class);
+
         final IdolFindConfigFileService configService = new IdolFindConfigFileService();
         configService.setConfigFileLocation("hp.find.home");
         configService.setConfigFileName("config.json");
