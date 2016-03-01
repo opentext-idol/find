653420dbf27632215e2775cc542e9ceb3ca3d27d
Alex Scown <alex.scown@hpe.com>
2016-03-01 17:38:47 +0000

    [CCUK-3425] Add stub file for tests

---
+++ find-core/src/main/public/static/js/find/app/page/find-settings-page.js
@@ -0,0 +1,7 @@
+/*
+ * Copyright 2014-2016 Hewlett-Packard Development Company, L.P.
+ * Licensed under the MIT License (the "License"); you may not use this file except in compliance with the License.
+ */
+
+// this file needs to exist to keep the tests happy but is never used
+define({});
