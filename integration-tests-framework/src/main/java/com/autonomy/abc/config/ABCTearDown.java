492232c7c4c080995e5c7cf2b4e31d163b8c658e
Fred Collman <frederick.collman@hpe.com>
2016-03-04 10:29:17 +0000

    Add ABCTestBase.hasSetUp() to be used in tear down methods
    The NPE was not being raised by the service being null, but by the instance variable never being assigned.
    However the services can only be used if login has succeeded.

--- integration-tests-framework/src/main/java/com/autonomy/abc/config/ABCTearDown.java
+++ integration-tests-framework/src/main/java/com/autonomy/abc/config/ABCTearDown.java
@@ -1,25 +1,21 @@
 package com.autonomy.abc.config;
 
 import com.autonomy.abc.selenium.keywords.KeywordFilter;
-import com.autonomy.abc.selenium.keywords.KeywordService;
-import com.autonomy.abc.selenium.promotions.PromotionService;
 
 public enum ABCTearDown implements TearDown<ABCTestBase> {
     KEYWORDS {
         @Override
         public void tearDown(ABCTestBase test) {
-            KeywordService service = test.getApplication().keywordService();
-            if (service != null) {
-                service.deleteAll(KeywordFilter.ALL);
+            if (test.hasSetUp()) {
+                test.getApplication().keywordService().deleteAll(KeywordFilter.ALL);
             }
         }
     },
     PROMOTIONS {
         @Override
         public void tearDown(ABCTestBase test) {
-            PromotionService<?> service = test.getApplication().promotionService();
-            if (service != null) {
-                service.deleteAll();
+            if (test.hasSetUp()) {
+                test.getApplication().promotionService().deleteAll();
             }
         }
     };
