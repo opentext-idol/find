opentext-idol/find

commit 32913d19c958fd0703a20c5f34745708c26c6a4a

Use LoginService in tests instead of ABCTestBase methods

integration-tests-hosted/src/test/java/com/autonomy/abc/connections/ConnectionDetailPageITCase.java
@@ -7,6 +7,7 @@ import com.autonomy.abc.selenium.actions.wizard.Wizard;
 import com.autonomy.abc.selenium.connections.*;
 import com.autonomy.abc.selenium.indexes.Index;
 import com.autonomy.abc.selenium.indexes.IndexService;
+import com.autonomy.abc.selenium.users.User;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -26,11 +27,13 @@ public class ConnectionDetailPageITCase extends HostedTestBase {
     private ConnectionService connectionService;
     private ConnectionsDetailPage connectionsDetailPage;
     private Connector connector;
+    private User testUser;
 
     public ConnectionDetailPageITCase(TestConfig config) {
         super(config);
         // requires a separate account where indexes can safely be added and deleted
-        setInitialUser(config.getUser("index_tests"));
+        testUser = config.getUser("index_tests");
+        setInitialUser(testUser);
     }
 
     @Before
@@ -125,7 +128,7 @@ public class ConnectionDetailPageITCase extends HostedTestBase {
 
         verifyIndexNameForConnector();
 
-        indexService.deleteIndexViaAPICalls(indexOne, getCurrentUser(), getConfig().getApiUrl());
+        indexService.deleteIndexViaAPICalls(indexOne, testUser, getConfig().getApiUrl());
 
         indexService.goToIndexes();
 

integration-tests-hosted/src/test/java/com/autonomy/abc/indexes/IndexesPageITCase.java
@@ -18,6 +18,7 @@ import com.autonomy.abc.selenium.promotions.PromotionService;
 import com.autonomy.abc.selenium.promotions.PromotionsPage;
 import com.autonomy.abc.selenium.search.IndexFilter;
 import com.autonomy.abc.selenium.search.SearchQuery;
+import com.autonomy.abc.selenium.users.User;
 import com.autonomy.abc.selenium.util.ElementUtil;
 import com.autonomy.abc.selenium.util.PageUtil;
 import org.junit.After;
@@ -49,11 +50,13 @@ public class IndexesPageITCase extends HostedTestBase {
     private final static Logger LOGGER = LoggerFactory.getLogger(IndexesPageITCase.class);
     private IndexService indexService;
     private IndexesPage indexesPage;
+    private User testUser;
 
     public IndexesPageITCase(TestConfig config) {
         super(config);
         // requires a separate account where indexes can safely be added and deleted
-        setInitialUser(config.getUser("index_tests"));
+        testUser = config.getUser("index_test");
+        setInitialUser(testUser);
     }
 
     @Before
@@ -216,7 +219,7 @@ public class IndexesPageITCase extends HostedTestBase {
     @KnownBug("CSA-1886")
     @Ignore("Breaking too many tests")
     public void testDeletingDefaultIndex(){
-        indexService.deleteIndexViaAPICalls(Index.DEFAULT, getCurrentUser(), getConfig().getApiUrl());
+        indexService.deleteIndexViaAPICalls(Index.DEFAULT, testUser, getConfig().getApiUrl());
 
         getDriver().navigate().refresh();
         indexesPage = getElementFactory().getIndexesPage();
@@ -227,7 +230,7 @@ public class IndexesPageITCase extends HostedTestBase {
     @Test
     @Ignore("Breaking too many tests")
     public void testDeletingSearchDefaultIndex(){
-        indexService.deleteIndexViaAPICalls(new Index("search_default_index"), getCurrentUser(), getConfig().getApiUrl());
+        indexService.deleteIndexViaAPICalls(new Index("search_default_index"), testUser, getConfig().getApiUrl());
         getDriver().navigate().refresh();
 
         verifyThat(getApplication().switchTo(PromotionsPage.class), containsText("There are no promotions..."));

integration-tests-framework/src/main/java/com/autonomy/abc/config/ABCTestBase.java
@@ -2,14 +2,11 @@ package com.autonomy.abc.config;
 
 import com.autonomy.abc.selenium.application.SearchOptimizerApplication;
 import com.autonomy.abc.selenium.navigation.SOElementFactory;
-import com.autonomy.abc.selenium.users.SSOFailureException;
 import com.autonomy.abc.selenium.users.User;
-import com.hp.autonomy.frontend.selenium.login.LoginPage;
 import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
-import org.openqa.selenium.WebDriver;
 
 import static org.junit.Assert.fail;
 
@@ -17,7 +14,6 @@ import static org.junit.Assert.fail;
 @RunWith(Parameterized.class)
 public abstract class ABCTestBase extends SeleniumTest<SearchOptimizerApplication<?>, SOElementFactory> {
 	private User initialUser;
-	private User currentUser;
 
 	public ABCTestBase(final TestConfig config) {
 		super(config, SearchOptimizerApplication.ofType(config.getType()));
@@ -35,7 +31,7 @@ public abstract class ABCTestBase extends SeleniumTest<SearchOptimizerApplicatio
 	public final void abcSetUp() {
 		if (!initialUser.equals(User.NULL)) {
 			try {
-				loginAs(initialUser);
+				getApplication().loginService().login(initialUser);
 				postLogin();
 			} catch (Exception e) {
 				LOGGER.error("Unable to login");
@@ -47,37 +43,5 @@ public abstract class ABCTestBase extends SeleniumTest<SearchOptimizerApplicatio
 
 	protected final void setInitialUser(User user) {
 		initialUser = user;
-	}
-
-	protected final void loginAs(User user) {
-		loginTo(getElementFactory().getLoginPage(), getDriver(), user);
-		currentUser = user;
-	}
-
-	protected final void loginTo(LoginPage loginPage, WebDriver webDriver, User user) {
-		String redirectUrl = extractRedirectUrl(webDriver.getCurrentUrl()).replace("%23","#");
-		try {
-			loginPage.loginWith(user.getAuthProvider());
-		} catch (SSOFailureException e) {
-			LOGGER.info("Login failed, redirecting to " + redirectUrl);
-			webDriver.get(redirectUrl);
-		}
-	}
-
-	private String extractRedirectUrl(String fullUrl) {
-		int startIndex = fullUrl.indexOf("redirect_url=") + "redirect_url=".length();
-		String redirectUrl = fullUrl.substring(startIndex);
-		return redirectUrl
-				.replaceAll("%3A", ":")
-				.replaceAll("%2F", "/");
-	}
-
-	protected final void logout() {
-		getElementFactory().getTopNavBar().logOut();
-		currentUser = User.NULL;
-	}
-
-	protected final User getCurrentUser() {
-		return currentUser;
 	}
 }

integration-tests-framework/src/main/java/com/autonomy/abc/topnavbar/on_prem_options/UsersPageTestBase.java
@@ -33,6 +33,7 @@ public class UsersPageTestBase<T extends NewUser> extends ABCTestBase {
     protected UsersPage usersPage;
     protected UserService<?> userService;
     protected SignupEmailHandler emailHandler;
+    private LoginService loginService;
 
     public UsersPageTestBase(TestConfig config) {
         super(config);
@@ -43,6 +44,7 @@ public class UsersPageTestBase<T extends NewUser> extends ABCTestBase {
 
     @Before
     public void setUp() throws MalformedURLException, InterruptedException {
+        loginService = getApplication().loginService();
         userService = getApplication().userService();
         usersPage = userService.goToUsers();
         userService.deleteOtherUsers();
@@ -83,9 +85,7 @@ public class UsersPageTestBase<T extends NewUser> extends ABCTestBase {
             Waits.waitForGritterToClear();
         } catch (InterruptedException e) { /**/ }
 
-        logout();
-
-        getDriver().get(getAppUrl());
+        logoutAndNavigateToWebApp();
 
         try {
             loginAs(user);
@@ -113,8 +113,12 @@ public class UsersPageTestBase<T extends NewUser> extends ABCTestBase {
     }
 
     protected void logoutAndNavigateToWebApp(){
-        logout();
+        loginService.logout();
         getDriver().get(getAppUrl());
+    }
+
+    protected LoginService getLoginService() {
+        return loginService;
     }
 
     protected void verifyCreateDeleteInTable(NewUser newUser) {
@@ -126,5 +130,9 @@ public class UsersPageTestBase<T extends NewUser> extends ABCTestBase {
 
         deleteAndVerify(user);
         verifyThat(usersPage.getTable(), not(containsText(username)));
+    }
+
+    protected void loginAs(User user) {
+        loginService.login(user);
     }
 }

integration-tests-hosted/src/test/java/com/autonomy/abc/topnavbar/notifications/NotificationsDropDownHostedITCase.java
@@ -190,7 +190,7 @@ public class NotificationsDropDownHostedITCase extends NotificationsDropDownTest
             HSODApplication secondApplication = new HSODApplication().inWindow(secondSession.getActiveWindow());
             HSODElementFactory secondFactory = secondApplication.elementFactory();
 
-            loginTo(secondFactory.getLoginPage(), secondSession.getDriver(), user);
+            secondApplication.loginService().login(user);
             secondFactory.getPromotionsPage();
 
             secondApplication.keywordService().addSynonymGroup("Messi", "Campbell");

integration-tests-hosted/src/test/java/com/autonomy/abc/usermanagement/UserManagementHostedITCase.java
@@ -3,11 +3,14 @@ package com.autonomy.abc.usermanagement;
 import com.autonomy.abc.config.TestConfig;
 import com.autonomy.abc.framework.KnownBug;
 import com.autonomy.abc.framework.RelatedTo;
+import com.autonomy.abc.selenium.application.SearchOptimizerApplication;
+import com.autonomy.abc.selenium.control.Window;
 import com.autonomy.abc.selenium.element.GritterNotice;
 import com.autonomy.abc.selenium.error.ErrorPage;
 import com.autonomy.abc.selenium.error.Errors;
 import com.autonomy.abc.selenium.find.HSODFind;
 import com.autonomy.abc.selenium.find.HSODFindElementFactory;
+import com.autonomy.abc.selenium.hsod.HSODApplication;
 import com.autonomy.abc.selenium.users.*;
 import com.autonomy.abc.selenium.util.ElementUtil;
 import com.autonomy.abc.selenium.util.Waits;
@@ -172,27 +175,28 @@ public class UserManagementHostedITCase extends UsersPageTestBase<HSODNewUser> {
         verifyThat(usersPage.getStatusOf(user), is(Status.CONFIRMED));
 
         // TODO: use a single driver once 401 page has logout button
-        WebDriver secondDriver = getConfig().getWebDriverFactory().create();
+        SearchOptimizerApplication<?> secondApp = SearchOptimizerApplication.ofType(getConfig().getType());
+        Window secondWindow = launchInNewSession(secondApp).getActiveWindow();
         try {
-            secondDriver.get(getAppUrl());
-            LoginPage loginPage = new HSOLoginPage(secondDriver, new AbcHasLoggedIn(secondDriver));
-
+            secondWindow.goTo(getAppUrl());
             try {
-                loginTo(loginPage, secondDriver, user);
+                secondApp.loginService().login(user);
             } catch (NoSuchElementException e) {
                 /* Happens when it's trying to log in for the second time */
             }
 
+            WebDriver secondDriver = secondWindow.getSession().getDriver();
+
             verify401(secondDriver);
 
-            secondDriver.navigate().to(getAppUrl().split("/searchoptimizer")[0]);
+            secondWindow.goTo(getAppUrl().split("/searchoptimizer")[0]);
             verify401(secondDriver);
 
-            secondDriver.navigate().to(getConfig().getAppUrl(new HSODFind()));
+            secondWindow.goTo(getConfig().getAppUrl(new HSODFind()));
             Waits.loadOrFadeWait();
             verifyThat(secondDriver.findElement(By.className("error-body")), containsText("401"));
         } finally {
-            secondDriver.quit();
+            secondWindow.close();
         }
     }
 
@@ -264,14 +268,13 @@ public class UserManagementHostedITCase extends UsersPageTestBase<HSODNewUser> {
         final User user = userService.createNewUser(getConfig().generateNewUser(), Role.ADMIN);
         user.authenticate(getConfig().getWebDriverFactory(), emailHandler);
 
-        logout();
-        getDriver().get(getConfig().getAppUrl(new HSODFind()));
-
-        HSODFindElementFactory findFactory = new HSODFind(getMainSession().getActiveWindow()).elementFactory();
+        getApplication().loginService().logout();
+        HSODFind findApp = new HSODFind().inWindow(getWindow());
+        getDriver().get(getConfig().getAppUrl(findApp));
 
         boolean success = true;
         try {
-            loginTo(findFactory.getLoginPage(), getDriver(), user);
+            findApp.loginService().login(user);
         } catch (Exception e) {
             success = false;
         }

integration-tests-onpremise/src/test/java/com/autonomy/abc/topnavbar/login/LoginPageOnPremiseITCase.java
@@ -53,7 +53,7 @@ public class LoginPageOnPremiseITCase extends ABCTestBase {
 		assertThat(usersPage, modalIsDisplayed());
 		usersPage.createNewUser("admin", "qwerty", "Admin");
 		usersPage.closeModal();
-		logout();
+		getApplication().loginService().logout();
 		loginPage = getElementFactory().getLoginPage();
 	}
 

integration-tests-onpremise/src/test/java/com/autonomy/abc/topnavbar/on_prem_options/UsersPageOnPremITCase.java
@@ -116,7 +116,7 @@ public class UsersPageOnPremITCase extends UsersPageTestBase<NewUser> {
     @Test
     //TO BE MOVED BACK TO COMMON IF FUNCTIONALITY IS IMPLEMENTED
     public void testWontDeleteSelf() {
-        assertThat(usersPage.deleteButton(getCurrentUser()), disabled());
+        assertThat(usersPage.deleteButton(getLoginService().getCurrentUser()), hasClass("not-clickable"));
     }
 
     @Test

commit 826e7f3ae5a2ffc390fa7a367a7c6c875c14c06c

Extract index test user setup
Means single point of change, and avoiding hard-coded string reduces
chance of typos.
Fixes IndexesPageITCase, broken by a typo in
32913d19c958fd0703a20c5f34745708c26c6a4a.

integration-tests-hosted/src/test/java/com/autonomy/abc/config/HostedTestBase.java
@@ -3,6 +3,7 @@ package com.autonomy.abc.config;
 import com.autonomy.abc.selenium.application.ApplicationType;
 import com.autonomy.abc.selenium.hsod.HSODApplication;
 import com.autonomy.abc.selenium.hsod.HSODElementFactory;
+import com.autonomy.abc.selenium.users.User;
 import org.junit.Ignore;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
@@ -33,5 +34,16 @@ public abstract class HostedTestBase extends ABCTestBase {
     @Override
     public HSODApplication getApplication() {
         return (HSODApplication) super.getApplication();
+    }
+
+    /**
+     * Use the user for index/connection tests
+     *
+     * Must be called in constructor of test
+     * A separate account is needed to create/delete
+     * indexes due to resource limits
+     */
+    protected void useIndexTestsUser() {
+        setInitialUser(getConfig().getUser("index_tests"));
     }
 }

integration-tests-hosted/src/test/java/com/autonomy/abc/connections/ConnectionDetailPageITCase.java
@@ -30,9 +30,8 @@ public class ConnectionDetailPageITCase extends HostedTestBase {
 
     public ConnectionDetailPageITCase(TestConfig config) {
         super(config);
-        // requires a separate account where indexes can safely be added and deleted
-        testUser = config.getUser("index_tests");
-        setInitialUser(testUser);
+        useIndexTestsUser();
+        testUser = getInitialUser();
     }
 
     @Before

integration-tests-hosted/src/test/java/com/autonomy/abc/connections/ConnectionsCoreITCase.java
@@ -23,7 +23,7 @@ public class ConnectionsCoreITCase extends HostedTestBase {
 
     public ConnectionsCoreITCase(TestConfig config) {
         super(config);
-        setInitialUser(config.getUser("index_tests"));
+        useIndexTestsUser();
     }
 
     @Before

integration-tests-hosted/src/test/java/com/autonomy/abc/connections/ConnectionsPageITCase.java
@@ -31,8 +31,7 @@ public class ConnectionsPageITCase extends HostedTestBase {
 
     public ConnectionsPageITCase(TestConfig config) {
         super(config);
-        // requires a separate account where indexes can safely be added and deleted
-        setInitialUser(config.getUser("index_tests"));
+        useIndexTestsUser();
     }
 
     @Before

integration-tests-hosted/src/test/java/com/autonomy/abc/endtoend/ConnectionToSearchITCase.java
@@ -35,7 +35,7 @@ public class ConnectionToSearchITCase extends HostedTestBase {
 
     public ConnectionToSearchITCase(TestConfig config) {
         super(config);
-        setInitialUser(config.getUser("index_tests"));
+        useIndexTestsUser();
     }
 
     @Before

integration-tests-hosted/src/test/java/com/autonomy/abc/endtoend/ConnectionsToFindITCase.java
@@ -41,7 +41,7 @@ public class ConnectionsToFindITCase extends HostedTestBase {
 
     public ConnectionsToFindITCase(TestConfig config) {
         super(config);
-        setInitialUser(config.getUser("index_tests"));
+        useIndexTestsUser();
     }
 
     @Before

integration-tests-hosted/src/test/java/com/autonomy/abc/endtoend/IndexFieldsITCase.java
@@ -53,7 +53,7 @@ public class IndexFieldsITCase extends HostedTestBase {
 
     public IndexFieldsITCase(TestConfig config) {
         super(config);
-        setInitialUser(config.getUser("index_tests"));
+        useIndexTestsUser();
 
         String indexName = UUID.randomUUID().toString().replace('-','a');
         index = new Index(indexName)

integration-tests-hosted/src/test/java/com/autonomy/abc/endtoend/IndexSetUpITCase.java
@@ -36,7 +36,7 @@ public class IndexSetUpITCase extends HostedTestBase {
 
     public IndexSetUpITCase(TestConfig config) {
         super(config);
-        setInitialUser(config.getUser("index_tests"));
+        useIndexTestsUser();
 
         String indexName = UUID.randomUUID().toString().replace('-','a');
         index = new Index(indexName);

integration-tests-hosted/src/test/java/com/autonomy/abc/indexes/IndexCoreITCase.java
@@ -19,7 +19,7 @@ public class IndexCoreITCase extends HostedTestBase {
 
     public IndexCoreITCase(TestConfig config) {
         super(config);
-        setInitialUser(config.getUser("index_tests"));
+        useIndexTestsUser();
     }
 
     @Before

integration-tests-hosted/src/test/java/com/autonomy/abc/indexes/IndexDetailsPageITCase.java
@@ -40,8 +40,7 @@ public class IndexDetailsPageITCase extends HostedTestBase {
 
     public IndexDetailsPageITCase(TestConfig config) {
         super(config);
-        // requires a separate account where indexes can safely be added and deleted
-        setInitialUser(config.getUser("index_tests"));
+        useIndexTestsUser();
     }
 
     @Before

integration-tests-hosted/src/test/java/com/autonomy/abc/indexes/IndexDisplayNameITCase.java
@@ -40,7 +40,7 @@ public class IndexDisplayNameITCase extends HostedTestBase {
 
     public IndexDisplayNameITCase(TestConfig config) {
         super(config);
-        setInitialUser(config.getUser("index_tests"));
+        useIndexTestsUser();
     }
 
     @Before

integration-tests-hosted/src/test/java/com/autonomy/abc/indexes/IndexesPageITCase.java
@@ -56,9 +56,8 @@ public class IndexesPageITCase extends HostedTestBase {
 
     public IndexesPageITCase(TestConfig config) {
         super(config);
-        // requires a separate account where indexes can safely be added and deleted
-        testUser = config.getUser("index_test");
-        setInitialUser(testUser);
+        useIndexTestsUser();
+        testUser = getInitialUser();
     }
 
     @Before