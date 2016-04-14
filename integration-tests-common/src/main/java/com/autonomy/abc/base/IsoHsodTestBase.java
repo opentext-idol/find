7a50b7c4e948e07141e0ed65b749d97ecf7b4f7a
Fred Collman <frederick.collman@hpe.com>
2016-04-14 11:50:50 +0100

    Filter IsoHsodTestBase to only run on hosted

--- integration-tests-common/src/main/java/com/autonomy/abc/base/IsoHsodTestBase.java
+++ integration-tests-common/src/main/java/com/autonomy/abc/base/IsoHsodTestBase.java
@@ -3,10 +3,14 @@ package com.autonomy.abc.base;
 import com.autonomy.abc.fixtures.IsoPostLoginHook;
 import com.autonomy.abc.selenium.hsod.IsoHsodApplication;
 import com.autonomy.abc.selenium.hsod.IsoHsodElementFactory;
+import com.hp.autonomy.frontend.selenium.application.ApplicationType;
 import com.hp.autonomy.frontend.selenium.config.TestConfig;
 import org.junit.Ignore;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
+
+import java.io.IOException;
+import java.util.Collections;
 
 @Ignore
 @RunWith(Parameterized.class)
@@ -14,5 +18,10 @@ public abstract class IsoHsodTestBase extends HybridAppTestBase<IsoHsodApplicati
     protected IsoHsodTestBase(TestConfig config) {
         super(config, new IsoHsodApplication());
         setPostLoginHook(new IsoPostLoginHook(getApplication()));
+    }
+
+    @Parameterized.Parameters
+    public static Iterable<Object[]> parameters() throws IOException {
+        return parameters(Collections.singleton(ApplicationType.HOSTED));
     }
 }
