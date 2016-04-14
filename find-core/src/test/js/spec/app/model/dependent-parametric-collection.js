6ff3ae7eda4f02fe935612ca8db0de337ee28d10
Will Byrne <will.byrne@hp.com>
2016-04-14 15:05:44 +0100

    [FIND-9] Fix broken test [rev Matt Gordon]

--- find-core/src/test/js/spec/app/model/dependent-parametric-collection.js
+++ find-core/src/test/js/spec/app/model/dependent-parametric-collection.js
@@ -24,21 +24,23 @@ define([
                     {field: [], count: '131', value: 'USA TODAY'},
                     {field: [], count: '933', value: 'WONDER WALL'}
                 ])).toEqual([
-                    {children: [], count: 32, text: 'THE ORANGE COUNTY REGISTER'},
-                    {children: [], count: 131, text: 'USA TODAY'},
-                    {children: [], count: 389, text: 'ARIZONA DAILY STAR'},
-                    {children: [], count: 738, text: 'AP WIRE'},
-                    {children: [], count: 933, text: 'WONDER WALL'},
-                    {children: [], count: 2122, text: 'ALBUQUERQUE JOURNAL'},
-                    {children: [], count: 2893, text: 'THE MIAMI HERALD'},
-                    {children: [], count: 5396, text: 'THE WASHINGTON POST'},
-                    {children: [], count: 8082, text: 'BBC'},
-                    {children: [], count: 8803, text: 'ABC NEWS'}
+                    {children: [], count: 1, text: 'THE DAILY HERALD', hidden: true},
+                    {children: [], count: 2, text: 'THE AUSTRALIAN', hidden: true},
+                    {children: [], count: 32, text: 'THE ORANGE COUNTY REGISTER', hidden: true},
+                    {children: [], count: 131, text: 'USA TODAY', hidden: true},
+                    {children: [], count: 389, text: 'ARIZONA DAILY STAR', hidden: true},
+                    {children: [], count: 738, text: 'AP WIRE', hidden: true},
+                    {children: [], count: 933, text: 'WONDER WALL', hidden: true},
+                    {children: [], count: 2122, text: 'ALBUQUERQUE JOURNAL', hidden: false},
+                    {children: [], count: 2893, text: 'THE MIAMI HERALD', hidden: false},
+                    {children: [], count: 5396, text: 'THE WASHINGTON POST', hidden: false},
+                    {children: [], count: 8082, text: 'BBC', hidden: false},
+                    {children: [], count: 8803, text: 'ABC NEWS', hidden: false}
                 ]);
             });
 
             it('parses the response for two field names', function() {
-                expect(DependentParametricCollection.prototype.parse([
+                var output = DependentParametricCollection.prototype.parse([
                     {count: '2', value: 'THE AUSTRALIAN', field: [
                         {field: [], count: '2', value: 'BUSINESS'}
                     ]},
@@ -73,7 +75,7 @@ define([
                     {count: '131', value: 'USA TODAY', field: [
                         {field: [], count: '131', value: 'BUSINESS'}
                     ]},
-                    {count: '933', value: 'WONDER WALL', field: [
+                    {count: '970', value: 'WONDER WALL', field: [
                         {field: [], count: '23', value: 'ACTORS'},
                         {field: [], count: '51', value: 'FOOD'},
                         {field: [], count: '52', value: 'DRINK'},
@@ -86,47 +88,58 @@ define([
                         {field: [], count: '75', value: 'AMERICAS'},
                         {field: [], count: '112', value: 'CARS '}
                     ]}
-                ])).toEqual([
-                    {text: 'THE ORANGE COUNTY REGISTER', count: 32, children: [
-                        {text: 'BUSINESS', count: 32}
-                    ]},
-                    {text: 'USA TODAY', count: 131, children: [
-                        {text: 'BUSINESS', count: 131}
-                    ]},
-                    {text: 'ARIZONA DAILY STAR', count: 389, children: [
-                        {text: 'BUSINESS', count: 389}
-                    ]},
-                    {text: 'AP WIRE', count: 738, children: [
-                        {text: 'BUSINESS', count: 738}
-                    ]},
-                    {text: 'WONDER WALL', count: 933, children: [
-                        {text: 'UK', count: 35},
-                        {text: 'FOOD', count: 51},
-                        {text: 'DRINK',count: 52},
-                        {text: 'TECHNOLOGY', count: 57},
-                        {text: 'WORLD', count: 58},
-                        {text: 'AMERICAS', count: 75},
-                        {text: 'CARS ', count: 112},
-                        {text: 'ANIMALS', count: 125},
-                        {text: 'SCIENCE', count: 180},
-                        {text: 'EUROPE', count: 225}
-                    ]},
-                    {text: 'ALBUQUERQUE JOURNAL', count: 2122, children: [
-                        {text: 'BUSINESS', count: 22}, {text: 'LIVING PEOPLE', count: 2100}
-                    ]},
-                    {text: 'THE MIAMI HERALD', count: 2893, children: [
-                        {text: 'BUSINESS', count: 2893}
-                    ]},
-                    {text: 'THE WASHINGTON POST', count: 5396, children: [
-                        {text: 'BUSINESS', count: 5396}
-                    ]},
-                    {text: 'BBC', count: 8082, children: [
-                        {text: 'BUSINESS', count: 8082}
-                    ]},
-                    {text: 'ABC NEWS', count: 8803, children: [
-                        {text: 'BUSINESS', count: 8803}
-                    ]}
                 ]);
+
+                var expected = [
+                    {text: 'THE DAILY HERALD', count: 1, children: [
+                        {hidden: false, count: 1, text: 'BUSINESS'}
+                    ], hidden: true},
+                    {text: 'THE AUSTRALIAN', count: 2, children: [
+                        {hidden: false, count: 2, text: 'BUSINESS'}
+                    ], hidden: true},
+                    {text: 'THE ORANGE COUNTY REGISTER', count: 32, children: [
+                        {hidden: false, text: 'BUSINESS', count: 32}
+                    ], hidden: true},
+                    {text: 'USA TODAY', count: 131, children: [
+                        {hidden: false, text: 'BUSINESS', count: 131}
+                    ], hidden: true},
+                    {text: 'ARIZONA DAILY STAR', count: 389, children: [
+                        {hidden: false, text: 'BUSINESS', count: 389}
+                    ], hidden: true},
+                    {text: 'AP WIRE', count: 738, children: [
+                        {hidden: false, text: 'BUSINESS', count: 738}
+                    ], hidden: true},
+                    {text: 'WONDER WALL', count: 970, children: [
+                        {hidden: true, text: 'ACTORS', count: 23},
+                        {hidden: true, text: 'UK', count: 35},
+                        {hidden: false, text: 'FOOD', count: 51},
+                        {hidden: false, text: 'DRINK',count: 52},
+                        {hidden: false, text: 'TECHNOLOGY', count: 57},
+                        {hidden: false, text: 'WORLD', count: 58},
+                        {hidden: false, text: 'AMERICAS', count: 75},
+                        {hidden: false, text: 'CARS ', count: 112},
+                        {hidden: false, text: 'ANIMALS', count: 125},
+                        {hidden: false, text: 'SCIENCE', count: 180},
+                        {hidden: false, text: 'EUROPE', count: 225}
+                    ], hidden: true},
+                    {text: 'ALBUQUERQUE JOURNAL', count: 2122, children: [
+                        {hidden: true, text: 'BUSINESS', count: 22}, {hidden: false, text: 'LIVING PEOPLE', count: 2100}
+                    ], hidden: false},
+                    {text: 'THE MIAMI HERALD', count: 2893, children: [
+                        {hidden: false, text: 'BUSINESS', count: 2893}
+                    ], hidden: false},
+                    {text: 'THE WASHINGTON POST', count: 5396, children: [
+                        {hidden: false, text: 'BUSINESS', count: 5396}
+                    ], hidden: false},
+                    {text: 'BBC', count: 8082, children: [
+                        {hidden: false, text: 'BUSINESS', count: 8082}
+                    ], hidden: false},
+                    {text: 'ABC NEWS', count: 8803, children: [
+                        {hidden: false, text: 'BUSINESS', count: 8803}
+                    ], hidden: false}
+                ];
+
+                expect(output).toEqual(expected);
             });
 
             it('returns an empty array when given an empty array', function() {
