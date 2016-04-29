866129b11941cdbe793e163dc385067eadf2dc47
Fred Collman <frederick.collman@hpe.com>
2016-04-29 17:53:29 +0100

    Move giant test to its own class

--- integration-tests-iso/src/test/java/com/autonomy/abc/topnavbar/notifications/NotificationsDropDownITCase.java
+++ integration-tests-iso/src/test/java/com/autonomy/abc/topnavbar/notifications/NotificationsDropDownITCase.java
@@ -1,8 +1,6 @@
 package com.autonomy.abc.topnavbar.notifications;
 
 import com.autonomy.abc.base.HybridIsoTestBase;
-import com.autonomy.abc.selenium.analytics.DashboardBase;
-import com.autonomy.abc.selenium.application.IsoApplication;
 import com.autonomy.abc.selenium.keywords.KeywordFilter;
 import com.autonomy.abc.selenium.keywords.KeywordService;
 import com.autonomy.abc.selenium.keywords.KeywordWizardType;
@@ -16,18 +14,12 @@ import com.autonomy.abc.selenium.promotions.PromotionService;
 import com.autonomy.abc.selenium.promotions.SpotlightPromotion;
 import com.autonomy.abc.shared.NotificationTestHelper;
 import com.hp.autonomy.frontend.selenium.config.TestConfig;
-import com.hp.autonomy.frontend.selenium.control.Window;
-import com.hp.autonomy.frontend.selenium.element.GritterNotice;
-import com.hp.autonomy.frontend.selenium.framework.logging.KnownBug;
 import org.junit.Before;
 import org.junit.Test;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.Arrays;
-import java.util.List;
 
 import static com.hp.autonomy.frontend.selenium.framework.state.TestStateAssert.assertThat;
-import static com.hp.autonomy.frontend.selenium.framework.state.TestStateAssert.verifyThat;
 import static org.hamcrest.Matchers.*;
 
 /*
@@ -92,98 +84,6 @@ public class NotificationsDropDownITCase extends HybridIsoTestBase {
 			assertThat("5 notifications after page refresh", notifications.countNotifications(), is(5));
 		} finally {
 			keywordService.deleteAll(KeywordFilter.ALL);
-		}
-	}
-
-	@Test
-	@KnownBug("CSA-1542")
-	public void testNotificationsOverTwoWindows() throws InterruptedException {
-		keywordService.goToKeywords();
-
-		topNavBar.notificationsDropdown();
-		notifications = topNavBar.getNotifications();
-		assertThat(notifications.countNotifications(), is(0));
-
-		final Window mainWindow = getWindow();
-		final IsoApplication<?> secondApp = IsoApplication.ofType(getConfig().getType());
-		final Window secondWindow = launchInNewWindow(secondApp);
-		secondWindow.activate();
-		TopNavBar topNavBarWindowTwo = secondApp.elementFactory().getTopNavBar();
-
-		secondApp.keywordService().goToKeywords();
-		topNavBarWindowTwo.notificationsDropdown();
-		NotificationsDropDown notificationsDropDownWindowTwo = topNavBarWindowTwo.getNotifications();
-		assertThat(notificationsDropDownWindowTwo.countNotifications(), is(0));
-
-		try {
-			mainWindow.activate();
-			keywordService.addSynonymGroup("Animal Beast");
-			keywordService.goToKeywords();
-
-			topNavBar.notificationsDropdown();
-			notifications = topNavBar.getNotifications();
-			assertThat(notifications.countNotifications(), is(1));
-			String windowOneNotificationText = notifications.notificationNumber(1).getText();
-
-			secondWindow.activate();
-			assertThat(notificationsDropDownWindowTwo.countNotifications(), is(1));
-			assertThat(notificationsDropDownWindowTwo.notificationNumber(1).getText(), is(windowOneNotificationText));
-			topNavBarWindowTwo.notificationsDropdown();
-			KeywordsPage keywordsPageWindowTwo = getElementFactory().getKeywordsPage();
-			keywordsPageWindowTwo.deleteSynonym("Animal", "Animal");
-			topNavBarWindowTwo.notificationsDropdown();
-			assertThat(notificationsDropDownWindowTwo.countNotifications(), is(2));
-			List<String> notificationMessages = notificationsDropDownWindowTwo.getAllNotificationMessages();
-
-			mainWindow.activate();
-			assertThat(notifications.countNotifications(), is(2));
-			assertThat(notifications.getAllNotificationMessages(), contains(notificationMessages.toArray()));
-
-			getApplication().switchTo(DashboardBase.class);
-			newBody();
-			topNavBar.notificationsDropdown();
-			notifications = topNavBar.getNotifications();
-			assertThat(notifications.countNotifications(), is(2));
-			assertThat(notifications.getAllNotificationMessages(), contains(notificationMessages.toArray()));
-
-			secondWindow.activate();
-
-			promotionService.setUpPromotion(new SpotlightPromotion("wheels"), "cars", 3);
-
-			new WebDriverWait(getDriver(), 5).until(GritterNotice.notificationAppears());
-			topNavBarWindowTwo.notificationsDropdown();
-			notificationsDropDownWindowTwo = topNavBarWindowTwo.getNotifications();
-
-			assertThat(notificationsDropDownWindowTwo.countNotifications(), is(3));
-			assertThat(notificationsDropDownWindowTwo.notificationNumber(1).getText(), containsString("promotion"));
-
-			notificationMessages = notificationsDropDownWindowTwo.getAllNotificationMessages();
-
-			mainWindow.activate();
-
-			notifications = topNavBar.getNotifications();
-			assertThat(notifications.countNotifications(), is(3));
-			assertThat(notifications.getAllNotificationMessages(), contains(notificationMessages.toArray()));
-
-			int notificationsCount = 3;
-			for(int i = 0; i < 6; i += 2) {
-				secondWindow.activate();
-				keywordService.addSynonymGroup(i + " " + (i + 1));
-				keywordService.goToKeywords();
-
-				secondApp.elementFactory().getTopNavBar().notificationsDropdown();
-				verifyThat(notificationsDropDownWindowTwo.countNotifications(), is(Math.min(++notificationsCount, 5)));
-				notificationMessages = notificationsDropDownWindowTwo.getAllNotificationMessages();
-
-				mainWindow.activate();
-				verifyThat(notifications.countNotifications(), is(Math.min(notificationsCount, 5)));
-				verifyThat(notifications.getAllNotificationMessages(), contains(notificationMessages.toArray()));
-			}
-		} finally {
-			secondWindow.activate();
-			topNavBarWindowTwo.closeNotifications();
-			keywordService.deleteAll(KeywordFilter.ALL);
-			promotionService.deleteAll();
 		}
 	}
 
