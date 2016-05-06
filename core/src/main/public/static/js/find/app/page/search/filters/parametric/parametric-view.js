opentext-idol/find

commit 075e254a86598b000f176ffb22d7dd528c761f3e

[FIND-18] Dates are filtered by the bundle word for dates and an empty message is display for no filters being matched. [matt.gordon, alex.scown]

core/src/main/public/static/js/find/app/page/search/filters/parametric/parametric-view.js
@@ -73,14 +73,8 @@ define([
                 this.model.set('empty', options.parametricCollection.isEmpty());
             });
 
-            this.displayCollection = new DisplayCollection([], {
-                parametricCollection: options.parametricCollection,
-                selectedParametricValues: this.selectedParametricValues,
-                filterModel: options.filterModel
-            });
-
             this.fieldNamesListView = new ListView({
-                collection: this.displayCollection,
+                collection: options.displayCollection,
                 ItemView: FieldView
             });
         },

core/src/main/public/static/js/find/app/page/search/abstract-query-left-side-view.js
@@ -5,34 +5,49 @@
 
 define([
     'backbone',
+    'jquery',
     'find/app/page/search/filters/date/dates-filter-view',
     'find/app/page/search/filters/parametric/parametric-view',
     'find/app/util/text-input',
     'find/app/util/collapsible',
+    'parametric-refinement/display-collection',
     'i18n!find/nls/bundle',
     'i18n!find/nls/indexes'
-], function(Backbone, DateView, ParametricView, TextInput, Collapsible, i18n, i18nIndexes) {
+], function(Backbone, $, DateView, ParametricView, TextInput, Collapsible, ParametricDisplayCollection, i18n, i18nIndexes) {
+
+    var datesTitle = i18n['search.dates'];
+
+    function searchMatches(text, search) {
+        return text.toLowerCase().indexOf(search.toLowerCase()) > -1;
+    }
 
     return Backbone.View.extend({
         // Abstract
         IndexesView: null,
 
         initialize: function(options) {
-            var filterModel = new Backbone.Model();
+            this.filterModel = new Backbone.Model();
 
             this.filterInput = new TextInput({
-                model: filterModel,
+                model: this.filterModel,
                 modelAttribute: 'text',
                 templateOptions: {
                     placeholder: i18n['search.filters.filter']
                 }
             });
 
+            this.indexesEmpty = false;
+
             var indexesView = new this.IndexesView({
                 queryModel: options.queryModel,
                 indexesCollection: options.indexesCollection,
                 selectedDatabasesCollection: options.queryState.selectedIndexes,
-                filterModel: filterModel
+                filterModel: this.filterModel,
+                visibleIndexesCallback: _.bind(function(indexes) {
+                    this.indexesEmpty = indexes.length === 0;
+                    this.updateIndexesVisibility();
+                    this.updateEmptyMessage();
+                }, this)
             });
 
             var dateView = new DateView({
@@ -40,12 +55,21 @@ define([
                 savedSearchModel: options.savedSearchModel
             });
 
+            this.parametricDisplayCollection = new ParametricDisplayCollection([], {
+                parametricCollection: options.parametricCollection,
+                selectedParametricValues: options.queryState.selectedParametricValues,
+                filterModel: this.filterModel
+            });
+
+            this.listenTo(this.parametricDisplayCollection, 'update reset', this.updateEmptyMessage);
+
             this.parametricView = new ParametricView({
                 queryModel: options.queryModel,
                 queryState: options.queryState,
-                filterModel: filterModel,
+                filterModel: this.filterModel,
                 indexesCollection: options.indexesCollection,
-                parametricCollection: options.parametricCollection
+                parametricCollection: options.parametricCollection,
+                displayCollection: this.parametricDisplayCollection
             });
 
             this.indexesViewWrapper = new Collapsible({
@@ -57,13 +81,21 @@ define([
             this.dateViewWrapper = new Collapsible({
                 view: dateView,
                 collapsed: false,
-                title: i18n['search.dates']
+                title: datesTitle
             });
+
+            this.listenTo(this.filterModel, 'change', function() {
+                this.updateDatesVisibility();
+                this.updateEmptyMessage();
+            });
+
+            this.$emptyMessage = $('<p class="hide">' + i18n['search.filters.empty'] + '</p>');
         },
 
         render: function() {
             this.$el.empty()
                 .append(this.filterInput.$el)
+                .append(this.$emptyMessage)
                 .append(this.indexesViewWrapper.$el)
                 .append(this.dateViewWrapper.$el)
                 .append(this.parametricView.$el);
@@ -72,6 +104,10 @@ define([
             this.indexesViewWrapper.render();
             this.parametricView.render();
             this.dateViewWrapper.render();
+
+            this.updateDatesVisibility();
+            this.updateIndexesVisibility();
+            this.updateEmptyMessage();
 
             return this;
         },
@@ -84,6 +120,24 @@ define([
             ], 'remove');
 
             Backbone.View.prototype.remove.call(this);
+        },
+
+        updateEmptyMessage: function() {
+            var noFiltersMatched = !(this.indexesEmpty && this.hideDates && this.parametricDisplayCollection.length === 0);
+
+            this.parametricView.$el.toggleClass('hide', !noFiltersMatched);
+            this.$emptyMessage.toggleClass('hide', noFiltersMatched);
+        },
+
+        updateDatesVisibility: function() {
+            var search = this.filterModel.get('text');
+            this.hideDates = !(!search || searchMatches(datesTitle, search));
+
+            this.dateViewWrapper.$el.toggleClass('hide', this.hideDates);
+        },
+
+        updateIndexesVisibility: function() {
+            this.indexesViewWrapper.$el.toggleClass('hide', this.indexesEmpty);
         }
     });
 

core/src/main/public/static/js/find/app/page/search/filters/indexes/indexes-view.js
@@ -73,13 +73,12 @@ define([
         },
 
         initialize: function(options) {
-            this.filterModel = options.filterModel;
-
             DatabasesView.prototype.initialize.call(this, {
                 databasesCollection: options.indexesCollection,
                 emptyMessage: i18n['search.indexes.empty'],
                 selectedDatabasesCollection: options.selectedDatabasesCollection,
-                filterModel: this.filterModel,
+                filterModel: options.filterModel,
+                visibleIndexesCallback: options.visibleIndexesCallback,
                 topLevelDisplayName: i18n['search.indexes.all'],
                 childCategories: this.getIndexCategories(),
                 listViewOptions: {

core/src/main/public/static/js/find/nls/root/bundle.js
@@ -111,6 +111,7 @@ define([
         'search.error.parametric' : 'An error occurred retrieving additional filters',
         'search.filters': 'Filters',
         'search.filters.filter': 'Search filters...',
+        'search.filters.empty': 'No filters matched',
         'search.indexes': 'Indexes',
         'search.indexes.all': 'All',
         'search.indexes.publicIndexes': 'Public Indexes',

commit c6ee3bbae1d33646ded7e8000ab369300cb3a44b

[FIND-76] Fix bug in delete saved search tab [rev: dan.grayling]
Bug introduced during refactoring in commit 075e254a86598b00.

core/src/main/public/static/js/find/app/page/search/filters/parametric/parametric-view.js
@@ -73,8 +73,14 @@ define([
                 this.model.set('empty', options.parametricCollection.isEmpty());
             });
 
+            this.displayCollection = new DisplayCollection([], {
+                parametricCollection: options.parametricCollection,
+                selectedParametricValues: this.selectedParametricValues,
+                filterModel: options.filterModel
+            });
+
             this.fieldNamesListView = new ListView({
-                collection: options.displayCollection,
+                collection: this.displayCollection,
                 ItemView: FieldView
             });
         },