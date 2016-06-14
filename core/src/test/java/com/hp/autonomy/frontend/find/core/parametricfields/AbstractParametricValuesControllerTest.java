fddb535c355352d53c5cc6182cf95067c03e7ad9
Rosie Taylor <rosie.taylor@hpe.com>
2016-06-14 16:04:03 +0100

    [FIND-73] Modularise the parametric values backend [for: ivo.miller]

--- core/src/test/java/com/hp/autonomy/frontend/find/core/parametricfields/AbstractParametricValuesControllerTest.java
+++ core/src/test/java/com/hp/autonomy/frontend/find/core/parametricfields/AbstractParametricValuesControllerTest.java
@@ -5,10 +5,10 @@
 
 package com.hp.autonomy.frontend.find.core.parametricfields;
 
-import com.hp.autonomy.searchcomponents.core.parametricvalues.BucketingParams;
 import com.hp.autonomy.searchcomponents.core.parametricvalues.ParametricRequest;
 import com.hp.autonomy.searchcomponents.core.parametricvalues.ParametricValuesService;
 import com.hp.autonomy.searchcomponents.core.search.QueryRestrictions;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Matchers;
@@ -18,16 +18,13 @@ import org.springframework.beans.factory.ObjectFactory;
 
 import java.io.Serializable;
 import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
 
-import static org.mockito.Matchers.eq;
 import static org.mockito.Mockito.verify;
 
 @RunWith(MockitoJUnitRunner.class)
-public abstract class AbstractParametricValuesControllerTest<Q extends QueryRestrictions<S>, R extends ParametricRequest<S>, S extends Serializable, E extends Exception> {
-    private static final String PARAMETRIC_FIELD = "SomeNumericParametricField";
-    private static final int TARGET_NUMBER_OF_BUCKETS = 35;
+public abstract class AbstractParametricValuesControllerTest<C extends ParametricValuesController<Q, R, S, E>, Q extends QueryRestrictions<S>, R extends ParametricRequest<S>, S extends Serializable, E extends Exception> {
+    protected static final String PARAMETRIC_FIELD = "SomeNumericParametricField";
+    protected static final int TARGET_NUMBER_OF_BUCKETS = 35;
 
     @Mock
     protected ParametricValuesService<R, S, E> parametricValuesService;
@@ -36,63 +33,18 @@ public abstract class AbstractParametricValuesControllerTest<Q extends QueryRest
     @Mock
     protected ObjectFactory<ParametricRequest.Builder<R, S>> parametricRequestBuilderFactory;
 
-    protected ParametricValuesController<Q, R, S, E> parametricValuesController;
+    protected C parametricValuesController;
 
-    @Test
-    public void getParametricValues() throws E {
-        parametricValuesController.getParametricValues(Collections.singletonList("SomeParametricField"), "Some query text", null, Collections.<S>emptyList(), null, null, 0, null);
-        verify(parametricValuesService).getAllParametricValues(Matchers.<R>any());
-    }
+    protected abstract C newControllerInstance();
 
-    @Test
-    public void getNumericParametricValuesInBuckets() throws E {
-        callGetNumericParametricValuesInBuckets(null, null);
-
-        final HashMap<String, BucketingParams> expectedBucketingParamsPerField = new HashMap<>();
-        expectedBucketingParamsPerField.put(PARAMETRIC_FIELD, new BucketingParams(TARGET_NUMBER_OF_BUCKETS, null, null));
-
-        verify(parametricValuesService).getNumericParametricValuesInBuckets(Matchers.<R>any(), eq(expectedBucketingParamsPerField));
-    }
-
-    @Test
-    public void getNumericParametricValuesInBucketsWithoutMin() throws E {
-        callGetNumericParametricValuesInBuckets(null, Collections.singletonList(2.5));
-
-        final HashMap<String, BucketingParams> expectedBucketingParamsPerField = new HashMap<>();
-        expectedBucketingParamsPerField.put(PARAMETRIC_FIELD, new BucketingParams(TARGET_NUMBER_OF_BUCKETS, null, 2.5));
-
-        verify(parametricValuesService).getNumericParametricValuesInBuckets(Matchers.<R>any(), eq(expectedBucketingParamsPerField));
-    }
-
-    @Test
-    public void getNumericParametricValuesInBucketsWithoutMax() throws E {
-        callGetNumericParametricValuesInBuckets(Collections.singletonList(1.5), null);
-
-        final HashMap<String, BucketingParams> expectedBucketingParamsPerField = new HashMap<>();
-        expectedBucketingParamsPerField.put(PARAMETRIC_FIELD, new BucketingParams(TARGET_NUMBER_OF_BUCKETS, 1.5, null));
-
-        verify(parametricValuesService).getNumericParametricValuesInBuckets(Matchers.<R>any(), eq(expectedBucketingParamsPerField));
+    @Before
+    public void setUp() {
+        parametricValuesController = newControllerInstance();
     }
 
     @Test
     public void getDependentParametricValues() throws E {
         parametricValuesController.getDependentParametricValues(Collections.singletonList("SomeParametricField"), "Some query text", null, Collections.<S>emptyList(), null, null, 0, null);
         verify(parametricValuesService).getDependentParametricValues(Matchers.<R>any());
-    }
-
-    private void callGetNumericParametricValuesInBuckets(final List<Double> bucketMin, final List<Double> bucketMax) throws E {
-        parametricValuesController.getNumericParametricValuesInBuckets(
-                Collections.singletonList("SomeNumericParametricField"),
-                "Some query text",
-                null,
-                Collections.<S>emptyList(),
-                null,
-                null,
-                0,
-                null,
-                Collections.singletonList(35),
-                bucketMin,
-                bucketMax
-        );
     }
 }
