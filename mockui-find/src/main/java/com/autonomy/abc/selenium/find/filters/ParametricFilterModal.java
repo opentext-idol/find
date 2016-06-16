d4fbcf9e322fe899913a12f4245f49d532ae430f
Alex Scown <alex.scown@hpe.com>
2016-06-16 16:05:26 +0100

    [FIND-125] General tidy up
    [rev: trivial]

--- mockui-find/src/main/java/com/autonomy/abc/selenium/find/filters/ParametricFilterModal.java
+++ mockui-find/src/main/java/com/autonomy/abc/selenium/find/filters/ParametricFilterModal.java
@@ -13,46 +13,57 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import java.util.ArrayList;
 import java.util.List;
 
-public class ParametricFilterModal extends ModalView{
-    ParametricFilterModal(final WebElement element, final WebDriver driver){super(element,driver);}
-
-    public static ParametricFilterModal getParametricModal(final WebDriver driver){
-        final WebElement $el = (WebElement)(new WebDriverWait(driver, 30)).until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".parametric-modal")));
-        final ParametricFilterModal view = new ParametricFilterModal($el, driver);
-        return view;
+public class ParametricFilterModal extends ModalView {
+    ParametricFilterModal(final WebElement element, final WebDriver driver) {
+        super(element, driver);
     }
 
-    public WebElement cancelButton(){return findElement(new Locator()
+    public static ParametricFilterModal getParametricModal(final WebDriver driver) {
+        final WebElement $el = (new WebDriverWait(driver, 30)).until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".parametric-modal")));
+        return new ParametricFilterModal($el, driver);
+    }
+
+    public WebElement cancelButton() {
+        return findElement(new Locator()
             .withTagName("button")
             .containingText("Cancel")
-    );}
+        );
+    }
 
-    public WebElement applyButton(){return findElement(new Locator()
+    public WebElement applyButton() {
+        return findElement(new Locator()
             .withTagName("button")
             .containingText("Apply")
-    );}
+        );
+    }
 
-    public List<WebElement> tabs(){
+    public List<WebElement> tabs() {
         return findElements(By.cssSelector(".category-title"));
     }
 
-    public WebElement activeTab(){
+    public WebElement activeTab() {
         return findElement(By.cssSelector(".category-title .active"));
     }
-    public String activeTabName(){return findElement(By.cssSelector("li.category-title.active span")).getText();}
+
+    public String activeTabName() {
+        return findElement(By.cssSelector("li.category-title.active span")).getText();
+    }
 
     //input 0-indexed like panel
-    public void goToTab(final int tabNumber){
-        findElement(By.cssSelector(".category-title:nth-child("+ CssUtil.cssifyIndex(tabNumber)+")"));
+    public void goToTab(final int tabNumber) {
+        findElement(By.cssSelector(".category-title:nth-child(" + CssUtil.cssifyIndex(tabNumber) + ')'));
     }
 
-    public WebElement activePane(){return findElement(By.cssSelector(".tab-pane.active"));
+    public WebElement activePane() {
+        return findElement(By.cssSelector(".tab-pane.active"));
     }
 
-    public List<WebElement> activeFieldList(){return activePane().findElements(By.cssSelector(".checkbox.parametric-field-label"));}
+    public List<WebElement> activeFieldList() {
+        return activePane().findElements(By.cssSelector(".checkbox.parametric-field-label"));
+    }
 
-    public String checkCheckBoxInActivePane(final int i){
-        final ParametricModalCheckbox box = new ParametricModalCheckbox(activeFieldList().get(i),getDriver());
+    public String checkCheckBoxInActivePane(final int i) {
+        final ParametricModalCheckbox box = new ParametricModalCheckbox(activeFieldList().get(i), getDriver());
         box.check();
         return box.getName();
     }
@@ -64,5 +75,5 @@ public class ParametricFilterModal extends ModalView{
             allCheckedFields.addAll(ElementUtil.getTexts(activePane().findElements(By.cssSelector(".icheckbox-hp.checked + span"))));
         }
         return allCheckedFields;
-        }
+    }
 }
