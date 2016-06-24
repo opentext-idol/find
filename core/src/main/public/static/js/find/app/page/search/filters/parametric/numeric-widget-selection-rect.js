9010c233abcd9012e107e0059ac8703bae33dfac
Matthew Gordon <matthew.gordon2@hp.com>
2016-06-24 11:16:00 +0100

    [FIND-297] Re-coloured the numeric widget selection box.
    After we re-coloured the bars, the selection became indistinguishable from the bars.
    
    [rev: alyona.ivanova]

--- core/src/main/public/static/js/find/app/page/search/filters/parametric/numeric-widget-selection-rect.js
+++ core/src/main/public/static/js/find/app/page/search/filters/parametric/numeric-widget-selection-rect.js
@@ -3,19 +3,19 @@
  * Licensed under the MIT License (the "License"); you may not use this file except in compliance with the License.
  */
 
-define([], function () {
-    "use strict";
-    
+define(function() {
+    'use strict';
+
     return {
         element: null,
         previousElement: null,
         currentX: 0,
         originX: 0,
-        setElement: function (ele) {
+        setElement: function(ele) {
             this.previousElement = this.element;
             this.element = ele;
         },
-        getNewAttributes: function () {
+        getNewAttributes: function() {
             let x = this.currentX < this.originX ? this.currentX : this.originX;
             let width = Math.abs(this.currentX - this.originX);
             return {
@@ -23,16 +23,16 @@ define([], function () {
                 width: width
             };
         },
-        getCurrentAttributes: function () {
-            let x = +this.element.attr("x");
-            let width = +this.element.attr("width");
+        getCurrentAttributes: function() {
+            let x = +this.element.attr('x');
+            let width = +this.element.attr('width');
             return {
                 x1: x,
                 x2: x + width
             };
         },
-        init: function (chart, height, newX) {
-            let rectElement = chart.append("rect")
+        init: function(chart, height, newX) {
+            let rectElement = chart.append('rect')
                 .attr({
                     rx: 4,
                     ry: 4,
@@ -41,29 +41,31 @@ define([], function () {
                     width: 0,
                     height: height
                 })
-                .classed("selection", true);
+                .classed('selection', true);
+
             this.setElement(rectElement);
             this.originX = newX;
             this.update(newX);
             this.removePrevious();
         },
-        update: function (newX) {
+        update: function(newX) {
             this.currentX = newX;
             this.element.attr(this.getNewAttributes());
         },
-        focus: function () {
+        focus: function() {
             this.element
-                .style("stroke", "#01a982")
-                .style("stroke-width", "2.5");
+                .style('stroke', '#01a982')
+                .style('stroke', 'rgba(97, 71, 103, 0.6)');
         },
-        remove: function () {
+        remove: function() {
             this.element.remove();
             this.element = null;
         },
-        removePrevious: function () {
+        removePrevious: function() {
             if (this.previousElement) {
                 this.previousElement.remove();
             }
         }
-    }
+    };
+
 });
