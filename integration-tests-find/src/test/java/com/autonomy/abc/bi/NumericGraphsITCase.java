e069fd07aab5b33f1dd6f00d4bcb13b62d182f2d
Aishling Cooke <aishling.cooke@hpe.com>
2016-07-19 16:13:55 +0100

    New class (nothing in it yet) for NumericGraphs

---
+++ integration-tests-find/src/test/java/com/autonomy/abc/bi/NumericGraphsITCase.java
@@ -0,0 +1,15 @@
+package com.autonomy.abc.bi;
+
+import com.autonomy.abc.base.IdolFindTestBase;
+import com.hp.autonomy.frontend.selenium.config.TestConfig;
+
+public class NumericGraphsITCase extends IdolFindTestBase{
+    public NumericGraphsITCase(final TestConfig config) {
+        super(config);
+    }
+
+
+
+
+
+}
