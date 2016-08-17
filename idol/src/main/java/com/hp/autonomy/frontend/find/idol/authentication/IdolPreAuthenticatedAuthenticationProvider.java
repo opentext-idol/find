e3dd7fa88a57d392398823b9af380c1cc37f870f
Brian Beckett <brian.beckett@hpe.com>
2016-08-17 11:21:04 +0100

    Change default reverse-proxy user role to BI user for BIFHI 11.1 demo
    When accessing Find behind an authenticating reverse proxy, we automatically create users if they don't exist in Community already.  When we create them, we add a default user role - FindUser.  This currently isn't configurable, so we need to hard-code a change for a reverse-proxied demo of BIFHI 11.1 features.
    
    [rev: trivial]

--- idol/src/main/java/com/hp/autonomy/frontend/find/idol/authentication/IdolPreAuthenticatedAuthenticationProvider.java
+++ idol/src/main/java/com/hp/autonomy/frontend/find/idol/authentication/IdolPreAuthenticatedAuthenticationProvider.java
@@ -61,7 +61,7 @@ public class IdolPreAuthenticatedAuthenticationProvider implements Authenticatio
 
             if(USER_NOT_FOUND_ERROR_ID.equals(e.getErrorId())) {
                 // use empty password so that auto created users cannot be authenticated against
-                this.userService.addUser(username, "", UserConfiguration.IDOL_USER_ROLE);
+                this.userService.addUser(username, "", UserConfiguration.IDOL_BI_ROLE);
 
                 user = userService.getUser(username);
             } else {
