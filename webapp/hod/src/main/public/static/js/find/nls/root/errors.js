0a3c0e589c3002a2814a1ab014c576f02a011272
Wojciech Kaluza <wojciech.kaluza@hpe.com>
2016-08-31 14:32:04 +0100

    [FIND-221] Internationalisation changes
    [rev: Matthew Gordon]

--- webapp/hod/src/main/public/static/js/find/nls/root/errors.js
+++ webapp/hod/src/main/public/static/js/find/nls/root/errors.js
@@ -2,24 +2,14 @@
  * Copyright 2015 Hewlett-Packard Development Company, L.P.
  * Licensed under the MIT License (the "License"); you may not use this file except in compliance with the License.
  */
-
 define([
     'js-whatever/js/substitution'
-], function (substitution) {
+], function(substitution) {
     'use strict';
 
+    // Caution: only use this file for errors that the user can understand and solve. If an error is listed here, by default the app will not advise the user to contact support.
     return substitution({
-        'error.default.message.uuid': 'Unknown error with UUID: {0}, please contact support.',
-        'error.default.message': 'Unknown error, please contact support.',
-        'hod.error.INVALID_QUERY_TEXT': 'All terms were invalid, through being stopwords, too short, or incorrectly formatted',
-        'hod.error.NO_IGNORE_SPECIALS': 'Find did not understand your search text',
-        'hod.error.QUERY_PROFILE_NAME_INVALID': 'Query profile name is invalid, please contact support',
-        'hod.error.QUERY_PROFILE_NAME_NOT_PROVIDED': 'Query profile name missing, please contact support',
-        'hod.error.QUERY_MANIPULATION_INDEX_INVALID': 'Query manipulation index is invalid, please contact support',
-        'hod.error.QUERY_MANIPULATION_INDEX_MISSING': 'Query manipulation index is missing, please contact support',
-        'hod.error.QUERY_MANIPULATION_RULE_INVALID': 'Query manipulation rule is invalid, please contact support',
-        'hod.error.QUERY_MANIPULATION_RULE_MISSING': 'Query manipulation rule is missing, please contact support',
-        'hod.error.QUERY_MANIPULATION_TEXT_INVALID': 'Query manipulation text is invalid, please contact support',
-        'hod.error.QUERY_MANIPULATION_TEXT_MISSING': 'Query manipulation text is missing, please contact support'
+        'error.code.INVALID_QUERY_TEXT': 'Invalid query text: all terms were stopwords, too short, or incorrectly formatted',
+        'error.code.NO_IGNORE_SPECIALS': 'Find did not understand your search text'
     });
 });
--- webapp/idol/src/main/public/static/js/find/nls/root/errors.js
+++ webapp/idol/src/main/public/static/js/find/nls/root/errors.js
@@ -2,14 +2,58 @@
  * Copyright 2015 Hewlett-Packard Development Company, L.P.
  * Licensed under the MIT License (the "License"); you may not use this file except in compliance with the License.
  */
-
 define([
     'js-whatever/js/substitution'
-], function (substitution) {
+], function(substitution) {
     'use strict';
 
+    // Caution: only use this file for errors that the user can understand and solve. If an error is listed here, by default the app will not advise the user to contact support.
     return substitution({
-        'error.default.message.uuid': 'Unknown error with UUID: {0}, please contact support.',
-        'error.default.message': 'Unknown error, please contact support.'
+        // TODO: Copypasted from userErrors in IdolGlobalExceptionHandler.java to enable lookup by
+        // generate-error-support-message.js. This is used on the frontend to decide if it is
+        // a user error and if a 'contact support' message should be displayed. Establish what these errors mean and
+        // provide prettified user mesages.
+        // 'error.code.DAHQUERY502': '',
+        // 'error.code.AXEQUERY504': '',
+        // 'error.code.DAHQUERY504': '',
+        // 'error.code.AXEQUERY505': '',
+        // 'error.code.DAHQUERY505': '',
+        // 'error.code.AXEQUERY507': '',
+        // 'error.code.DAHQUERY507': '',
+        // 'error.code.AXEQUERY508': '',
+        // 'error.code.DAHQUERY508': '',
+        // 'error.code.AXEQUERY509': '',
+        // 'error.code.DAHQUERY509': '',
+        // 'error.code.AXEQUERY511': '',
+        // 'error.code.DAHQUERY511': '',
+        // 'error.code.DAHQUERY512': '',
+        // 'error.code.AXEQUERY513': '',
+        // 'error.code.DAHQUERY513': '',
+        // 'error.code.AXEGETQUERYTAGVALUES502': '',
+        // 'error.code.DAHGETQUERYTAGVALUES502': '',
+        // 'error.code.AXEGETQUERYTAGVALUES507': '',
+        // 'error.code.DAHGETQUERYTAGVALUES507': '',
+        // 'error.code.AXEGETQUERYTAGVALUES508': '',
+        // 'error.code.DAHGETQUERYTAGVALUES508': '',
+        // 'error.code.AXEGETQUERYTAGVALUES519': '',
+        // 'error.code.DAHGETQUERYTAGVALUES519': '',
+        // 'error.code.AXEGETQUERYTAGVALUES520': '',
+        // 'error.code.DAHGETQUERYTAGVALUES520': '',
+        // 'error.code.AXEGETQUERYTAGVALUES522': '',
+        // 'error.code.DAHGETQUERYTAGVALUES522': '',
+        // 'error.code.AXEGETQUERYTAGVALUES538': '',
+        // 'error.code.DAHGETQUERYTAGVALUES538': '',
+        // 'error.code.QMSQUERY-2147435967': '',
+        // 'error.code.QMSQUERY-2147435888': '',
+        // 'error.code.AXEGETQUERYTAGVALUES504': '',
+        // 'error.code.DAHGETQUERYTAGVALUES504': '',
+        // 'error.code.AXEGETQUERYTAGVALUES509': '',
+        // 'error.code.DAHGETQUERYTAGVALUES509': '',
+        // 'error.code.AXEGETQUERYTAGVALUES512': '',
+        // 'error.code.DAHGETQUERYTAGVALUES512': '',
+        // 'error.code.AXEGETQUERYTAGVALUES513': '',
+        // 'error.code.DAHGETQUERYTAGVALUES513': '',
+        'error.code.AXEQUERY502': 'Invalid query text: all terms were stopwords, too short, or incorrectly formatted',
+        'error.code.AXEQUERY512': 'Find did not understand your search text'
     });
 });
