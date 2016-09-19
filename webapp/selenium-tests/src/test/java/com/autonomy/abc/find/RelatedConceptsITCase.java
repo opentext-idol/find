8877f86a9cadea6cb9450d61538ffb5c41da06b5
Aishling Cooke <aishling.cooke@hpe.com>
2016-09-19 15:20:55 +0100

    Annotate RelatedConceptsITCase to run with findUser only
    Currently no related concepts panel for BI users, thus skip the related concepts tests for them.

--- webapp/selenium-tests/src/test/java/com/autonomy/abc/find/RelatedConceptsITCase.java
+++ webapp/selenium-tests/src/test/java/com/autonomy/abc/find/RelatedConceptsITCase.java
@@ -34,6 +34,7 @@ import static org.hamcrest.Matchers.*;
 import static org.hamcrest.core.Is.is;
 
 @RelatedTo("CSA-2091")
+@Role(UserRole.FIND)
 public class RelatedConceptsITCase extends FindTestBase {
     private FindService findService;
     private FindTopNavBar navBar;
