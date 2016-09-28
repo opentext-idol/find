60769c60037f37208ec0115ac0e7de75efa61830
Wojciech Kaluza <wojciech.kaluza@hpe.com>
2016-09-28 18:32:19 +0100

    [FIND-578] Formatting and copyright strings
    [rev: Alexander Scown]

--- webapp/core/src/main/public/static/js/find/app/model/query-text-model.js
+++ webapp/core/src/main/public/static/js/find/app/model/query-text-model.js
@@ -2,7 +2,6 @@
  * Copyright 2016 Hewlett-Packard Development Company, L.P.
  * Licensed under the MIT License (the "License"); you may not use this file except in compliance with the License.
  */
-
 define([
     'backbone'
 ], function (Backbone) {
@@ -12,5 +11,4 @@ define([
             inputText: ''
         }
     });
-
 });
