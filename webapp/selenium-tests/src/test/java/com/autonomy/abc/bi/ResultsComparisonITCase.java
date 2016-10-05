d6816db3807eb52f83d64042151071051c3f4619
Rosie Taylor <rosie.taylor@hpe.com>
2016-10-05 13:43:19 +0100

    [FIND-642] Fix selenium tests after getting comparison working again
    [rev: aishling.cooke]

--- webapp/selenium-tests/src/test/java/com/autonomy/abc/bi/ResultsComparisonITCase.java
+++ webapp/selenium-tests/src/test/java/com/autonomy/abc/bi/ResultsComparisonITCase.java
@@ -7,26 +7,18 @@ import com.autonomy.abc.selenium.find.IdolFindPage;
 import com.autonomy.abc.selenium.find.application.BIIdolFindElementFactory;
 import com.autonomy.abc.selenium.find.application.UserRole;
 import com.autonomy.abc.selenium.find.bi.TopicMapView;
-import com.autonomy.abc.selenium.find.comparison.ResultsComparisonView;
 import com.autonomy.abc.selenium.find.save.SavedSearchService;
-import com.autonomy.abc.selenium.find.save.SearchOptionsBar;
-import com.autonomy.abc.selenium.find.save.SearchTabBar;
 import com.autonomy.abc.selenium.find.save.SearchType;
 import com.hp.autonomy.frontend.selenium.config.TestConfig;
-import com.hp.autonomy.frontend.selenium.framework.logging.ActiveBug;
 import com.hp.autonomy.frontend.selenium.framework.logging.ResolvedBug;
 import com.hp.autonomy.frontend.selenium.util.Waits;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-import org.openqa.selenium.By;
 import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
 import static com.hp.autonomy.frontend.selenium.framework.state.TestStateAssert.verifyThat;
-import static org.hamcrest.Matchers.hasSize;
 import static org.junit.Assert.fail;
 
 //The result comparisons for non-list view
@@ -36,7 +28,6 @@ public class ResultsComparisonITCase extends IdolFindTestBase {
     private SavedSearchService savedSearchService;
     private BIIdolFindElementFactory elementFactory;
 
-    private ResultsComparisonView resultsComparison;
     private IdolFindPage findPage;
 
     public ResultsComparisonITCase(final TestConfig config) {
@@ -52,6 +43,7 @@ public class ResultsComparisonITCase extends IdolFindTestBase {
         findService.search("careful now");
 
         try {
+            findPage = getElementFactory().getFindPage();
             findPage.waitUntilSearchTabsLoaded();
             savedSearchService.deleteAll();
             elementFactory.getConceptsPanel().removeAllConcepts();
@@ -62,31 +54,10 @@ public class ResultsComparisonITCase extends IdolFindTestBase {
 
     @After
     public void tearDown() {
-        findService.search("back to results");
+        findPage = getElementFactory().getFindPage();
+        findPage.goBackToSearch();
         savedSearchService.deleteAll();
     }
-
-
-    private void saveTwoSearches(final String searchName1, final String searchName2) {
-        search("yellow",searchName1,SearchType.QUERY);
-        savedSearchService.openNewTab();
-        search("red",searchName2,SearchType.QUERY);
-    }
-
-    @Test
-    public void testDeletingATab() {
-        saveTwoSearches("mellow","unmellow");
-
-        SearchTabBar bar = elementFactory.getSearchTabBar();
-        final String title = bar.currentTab().getTitle();
-
-        final SearchOptionsBar options = elementFactory.getSearchOptionsBar();
-        options.delete();
-
-        verifyThat("Deleted search is gone",bar.tabs(),hasSize(1));
-        bar.waitUntilTabGone(title);
-    }
-
 
     @Test
     @ResolvedBug("FIND-370")
@@ -98,9 +69,9 @@ public class ResultsComparisonITCase extends IdolFindTestBase {
 
         Waits.loadOrFadeWait();
 
-        TopicMapView mapView = elementFactory.getTopicMap();
+        final TopicMapView mapView = elementFactory.getTopicMap();
         mapView.waitForMapLoaded();
-        WebElement mapEntity = mapView.mapEntities().get(0);
+        final WebElement mapEntity = mapView.mapEntities().get(0);
         mapView.speedVsAccuracySlider().dragBy(100);
         mapView.waitForMapLoaded();
         try {
@@ -111,9 +82,15 @@ public class ResultsComparisonITCase extends IdolFindTestBase {
         }
     }
 
+    private void saveTwoSearches(final String searchName1, final String searchName2) {
+        search("yellow",searchName1,SearchType.QUERY);
+        savedSearchService.openNewTab();
+        search("red",searchName2,SearchType.QUERY);
+    }
+
     private void search(final String query, final String saveAs, final SearchType saveType) {
         findService.search(query);
-        new WebDriverWait(getDriver(), 30L).withMessage("Buttons should become active").until(ExpectedConditions.presenceOfElementLocated(By.cssSelector(".service-view-container:not(.hide) .save-button:not(.disabled)")));
+        findPage.waitUntilSaveButtonsActive();
         savedSearchService.saveCurrentAs(saveAs, saveType);
     }
 }
