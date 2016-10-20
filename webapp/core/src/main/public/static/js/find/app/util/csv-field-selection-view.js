8c803a28ccb123e700fea5a45d9cfeed7a5e5770
Will Byrne <will.byrne@hpe.com>
2016-10-20 11:34:30 +0100

    [FIND-508] Pass stringified fieldText for export
    We were passing the object of the fieldText node to the backend for the
    CSV export function, we now pass the stringified version if fieldText
    exists or empty string if not.
    
    [rev Alyona Ivanova]

--- webapp/core/src/main/public/static/js/find/app/util/csv-field-selection-view.js
+++ webapp/core/src/main/public/static/js/find/app/util/csv-field-selection-view.js
@@ -74,7 +74,7 @@ define([
             var searchRequest = JSON.stringify({
                 queryRestrictions: {
                     text: this.queryModel.get('queryText'),
-                    field_text: this.queryModel.get('fieldText'),
+                    field_text: this.queryModel.get('fieldText') ? this.queryModel.get('fieldText').toString() : '',
                     indexes: this.queryModel.get('indexes'),
                     min_date: this.queryModel.getIsoDate('minDate'),
                     max_date: this.queryModel.getIsoDate('maxDate'),
