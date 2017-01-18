d43805ccb09cb80cd38568ab573f5d5a0e4d401f
Ivo Miller <ivo.miller@hpe.com>
2017-01-18 17:12:55 +0000

    Ensure the output of the metrics endpoint is nicely sorted (FIND-790, sub-task FIND-811)
    [rev. matthew.gordon]

--- webapp/core/src/main/java/com/hp/autonomy/frontend/find/core/metrics/MetricsConfiguration.java
+++ webapp/core/src/main/java/com/hp/autonomy/frontend/find/core/metrics/MetricsConfiguration.java
@@ -2,12 +2,16 @@ package com.hp.autonomy.frontend.find.core.metrics;
 
 import org.springframework.boot.actuate.autoconfigure.ExportMetricReader;
 import org.springframework.boot.actuate.autoconfigure.MetricRepositoryAutoConfiguration;
+import org.springframework.boot.actuate.metrics.Metric;
 import org.springframework.boot.actuate.metrics.buffer.BufferCounterService;
 import org.springframework.boot.actuate.metrics.buffer.BufferMetricReader;
 import org.springframework.boot.actuate.metrics.buffer.CounterBuffers;
 import org.springframework.boot.actuate.metrics.buffer.GaugeBuffers;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
 
 /**
  * Customised copy of {@link MetricRepositoryAutoConfiguration}
@@ -28,7 +32,13 @@ public class MetricsConfiguration {
     @ExportMetricReader
     public BufferMetricReader actuatorMetricReader(final CounterBuffers counters,
                                                    final GaugeBuffers gauges) {
-        return new BufferMetricReader(counters, gauges);
+        return new BufferMetricReader(counters, gauges) {
+            @Override
+            public Iterable<Metric<?>> findAll() {
+                final Iterable<Metric<?>> metrics = super.findAll();
+                return StreamSupport.stream(metrics.spliterator(), false).sorted((x, y) -> x.getName().compareTo(y.getName())).collect(Collectors.toList());
+            }
+        };
     }
 
     @Bean
--- webapp/core/src/test/java/com/hp/autonomy/frontend/find/core/metrics/FindGaugeServiceTest.java
+++ webapp/core/src/test/java/com/hp/autonomy/frontend/find/core/metrics/FindGaugeServiceTest.java
@@ -4,15 +4,13 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.actuate.metrics.GaugeService;
-import org.springframework.boot.actuate.metrics.buffer.GaugeBuffers;
+import org.springframework.boot.actuate.metrics.Metric;
+import org.springframework.boot.actuate.metrics.buffer.BufferMetricReader;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import static org.mockito.Matchers.anyString;
-import static org.mockito.Matchers.eq;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
 
 @SuppressWarnings("SpringJavaAutowiredMembersInspection")
 @RunWith(SpringRunner.class)
@@ -20,12 +18,15 @@ import static org.mockito.Mockito.verify;
 public class FindGaugeServiceTest {
     @Autowired
     private GaugeService gaugeService;
-    @MockBean
-    private GaugeBuffers buffers;
+    @Autowired
+    private BufferMetricReader metricReader;
 
     @Test
     public void submit() {
-        gaugeService.submit("someName", 0.1);
-        verify(buffers, times(3)).set(anyString(), eq(0.1));
+        final String metricName = "someName";
+        final double metricValue = 0.1;
+        gaugeService.submit(metricName, metricValue);
+        final Iterable<Metric<?>> metrics = metricReader.findAll();
+        assertThat(metrics, hasItem(hasProperty("name", startsWith("someName"))));
     }
 }
