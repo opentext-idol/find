06528dc895547db988562745b8b5ea0c8527e0c7
Ivo Miller <ivo.miller@hpe.com>
2017-01-23 15:47:36 +0000

    Add metrics endpoint for recording frontend metrics (FIND-790, sub-task FIND-815)
    [rev. matthew.gordon]

---
+++ webapp/idol/src/test/java/com/hp/autonomy/frontend/find/idol/metrics/IdolMetricsControllerIT.java
@@ -0,0 +1,6 @@
+package com.hp.autonomy.frontend.find.idol.metrics;
+
+import com.hp.autonomy.frontend.find.core.metrics.MetricsControllerIT;
+
+public class IdolMetricsControllerIT extends MetricsControllerIT {
+}
