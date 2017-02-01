23155e1fe8801037fb7fc7e76b5d67b2764120e4
Tung Jin Chew <tung-jin.chew@hpe.com>
2017-02-01 13:41:27 +0000

    The topic map changes have been made into a pull request upstream which Alex has merged; so we can just update to that version and delete the local variants.

--- webapp/core/src/main/public/static/js/require-config.js
+++ webapp/core/src/main/public/static/js/require-config.js
@@ -37,7 +37,7 @@ require.config({
         slider: '../bower_components/seiyria-bootstrap-slider/dist',
         text: '../bower_components/requirejs-text/text',
         sunburst: '../bower_components/hp-autonomy-sunburst/src',
-        topicmap: 'hp-autonomy-topic-map/src',
+        topicmap: '../bower_components/hp-autonomy-topic-map/src',
         underscore: '../bower_components/underscore/underscore',
         typeahead: '../bower_components/corejs-typeahead/dist/typeahead.jquery',
         'flot': '../bower_components/Flot/jquery.flot',
