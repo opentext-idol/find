51a2a2fc8831890f487dfdf918905598b46d10bf
Tung Jin Chew <tung-jin.chew@hpe.com>
2017-02-13 19:49:26 +0000

    Separately package the powerpoint report-generation backend into its own Maven artifact.

--- webapp/core/src/main/java/com/hp/autonomy/frontend/reports/powerpoint/dto/ComposableElement.java
+++
@@ -1,9 +0,0 @@
-/*
- * Copyright 2017 Hewlett-Packard Development Company, L.P.
- * Licensed under the MIT License (the "License"); you may not use this file except in compliance with the License.
- */
-
-package com.hp.autonomy.frontend.reports.powerpoint.dto;
-
-public interface ComposableElement {
-}
