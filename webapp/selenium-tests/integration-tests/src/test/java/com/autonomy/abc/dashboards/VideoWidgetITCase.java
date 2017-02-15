fa8e165151b263b29fec6b6bbe206cf885ef4da8
Will Byrne <will.byrne@hpe.com>
2017-02-15 14:31:20 +0000

    [FIND-885] Video widget test
    rev Matt Gordon

---
+++ webapp/selenium-tests/integration-tests/src/test/java/com/autonomy/abc/dashboards/VideoWidgetITCase.java
@@ -0,0 +1,30 @@
+package com.autonomy.abc.dashboards;
+
+import com.hp.autonomy.frontend.selenium.config.TestConfig;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import static com.hp.autonomy.frontend.selenium.framework.state.TestStateAssert.assertThat;
+
+public class VideoWidgetITCase extends ClickableDashboardITCase {
+
+    public VideoWidgetITCase(final TestConfig config) {
+        super(config, 1, "Video Dashboard", "List", "rabbit");
+    }
+
+    @Test
+    public void testVideoElementExists() { // not sure if this test is any use
+        final WebElement webElement = page.getWidgets().get(0);
+        final WebElement videoElement = webElement.findElement(By.cssSelector(".video-container video"));
+        assertThat("class has not been rendered", videoElement != null);
+    }
+
+    @Test
+    public void testVideoElementAttributes() {
+        final WebElement webElement = page.getWidgets().get(0);
+        final WebElement videoElement = webElement.findElement(By.cssSelector(".video-container video"));
+        assertThat("video is not looping", Boolean.valueOf(videoElement.getAttribute("loop")));
+        assertThat("video is not muted", Boolean.valueOf(videoElement.getAttribute("muted")));
+    }
+}
