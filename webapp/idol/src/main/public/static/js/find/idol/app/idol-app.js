a22762bfd85108b40296ca33d0b5b83560cad522
aly <alyona.ivanova@hpe.com>
2017-02-21 10:38:18 +0000

    [FIND-808] Spit double navigation links into side and top menu links
    [rev: matthew.gordon2]

--- webapp/idol/src/main/public/static/js/find/idol/app/idol-app.js
+++ webapp/idol/src/main/public/static/js/find/idol/app/idol-app.js
@@ -49,7 +49,8 @@ define([
                     models: ['sidebarModel'],
                     title: i18n[dash.dashboardName] || dash.dashboardName,
                     order: index,
-                    constructorArguments: dash
+                    constructorArguments: dash,
+                    sideMenu: true
                 };
 
                 return acc;
@@ -71,13 +72,16 @@ define([
                             : []
                     ),
                     title: i18n['app.search'],
-                    order: dashboardCount
+                    order: dashboardCount,
+                    sideMenu: true
+
                 },
                 about: {
                     Constructor: AboutPage,
                     icon: 'hp-icon hp-fw hp-info',
                     title: i18n['app.about'],
-                    order: dashboardCount + 1
+                    order: dashboardCount + 1,
+                    dropdownMenu: true
                 }
             });
 
@@ -86,7 +90,8 @@ define([
                     Constructor: SettingsPage,
                     icon: 'hp-icon hp-fw hp-settings',
                     title: i18n['app.settings'],
-                    order: dashboardCount + 2
+                    order: dashboardCount + 2,
+                    dropdownMenu: true
                 };
             }
 
