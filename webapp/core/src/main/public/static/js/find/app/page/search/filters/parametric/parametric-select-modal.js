a8f77c29b4c8fbc99651a67ecff57b28fb6687b7
Tung Jin Chew <tung-jin.chew@hpe.com>
2017-02-22 12:16:40 +0000

    Fix 'See All' button on parametric modal when a date range filter is active.

--- webapp/core/src/main/public/static/js/find/app/page/search/filters/parametric/parametric-select-modal.js
+++ webapp/core/src/main/public/static/js/find/app/page/search/filters/parametric/parametric-select-modal.js
@@ -16,7 +16,7 @@ define([
 
     // Convert a selected value model to a field value pair
     function toAttributes(model) {
-        return model.pick('field', 'value');
+        return model.pick('field', 'value', 'range');
     }
 
     return Modal.extend({
