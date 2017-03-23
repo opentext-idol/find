eeba1585a5b96db2d803abcdc2c853d656afcec2
Wojciech Kaluza <wojciech.kaluza@hpe.com>
2017-03-23 00:48:33 +0000

    [FIND-983] Keep session alive in fullscreen dashboard
    [rev: Matthew Gordon]

--- webapp/idol/src/main/java/com/hp/autonomy/frontend/find/idol/dashboards/IdolDashboardController.java
+++ webapp/idol/src/main/java/com/hp/autonomy/frontend/find/idol/dashboards/IdolDashboardController.java
@@ -17,22 +17,23 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.UnsupportedEncodingException;
 import java.net.URI;
 import java.net.URLDecoder;
 import java.nio.charset.StandardCharsets;
 import java.util.Optional;
-import java.util.Set;
-import java.util.stream.Collectors;
 
 @Slf4j
 @Controller
 public class IdolDashboardController {
     static final String DASHBOARD_CONFIG_RELOAD_PATH = "/api/admin/dashboards/reload";
+    private static final String DASHBOARD_KEEP_ALIVE = "/api/bi/dashboards/keep-alive";
     private final ControllerUtils controllerUtils;
     private final IdolDashboardConfigService dashConfig;
 
@@ -54,33 +55,56 @@ public class IdolDashboardController {
         // Pull in config changes
         dashConfig.init();
 
-        // Get list of dashboard names.
-        final Set<String> newDashboardsNames = dashConfig.getConfigResponse().getConfig().getDashboards()
-                .stream()
-                .map(Dashboard::getDashboardName)
-                .collect(Collectors.toSet());
-
         final String newUrl = Optional
                 // "referer" will be null if the config was reloaded by typing the endpoint URL
                 // into the browser, rather than by clicking the reload button
                 .ofNullable(request.getHeader(HttpHeaders.REFERER))
                 // This is to handle the case if the user uploads a config in which the dashboard
                 // they are currently on has been renamed or deleted, and cannot be redirected to
-                .filter(s -> {
-                    final String[] split = s.split("/public/dashboards/");
-                    boolean dashboardExists;
-                    try {
-                        final String decodedName = URLDecoder.decode(split[split.length - 1], StandardCharsets.UTF_8.name());
-                        dashboardExists = newDashboardsNames.contains(decodedName);
-                    } catch(UnsupportedEncodingException e) {
-                        throw new IllegalStateException("UTF-8 should be supported on all JVMs", e);
-                    }
-
-                    return split.length == 1 || dashboardExists;
+                .filter(url -> {
+                    final Optional<String> decodedName = getDecodedDashboardNameFromUrl(url);
+                    return !decodedName.isPresent() || dashboardExists(decodedName.get());
                 })
                 .orElse("/");
 
         response.sendRedirect(newUrl);
+    }
+
+    // Endpoint for fullscreen dashboards to poll when they want to extend the session [FIND-983].
+    @RequestMapping(method = RequestMethod.POST, value = DASHBOARD_KEEP_ALIVE)
+    @ResponseBody
+    public int keepAlive(final HttpServletRequest request, HttpSession session) {
+        final String logMessage = Optional
+                // "referer" will be null if the config was reloaded by typing the endpoint URL
+                // into the browser, rather than by clicking the reload button
+                .ofNullable(request.getHeader(HttpHeaders.REFERER))
+                .flatMap(this::getDecodedDashboardNameFromUrl)
+                .map(dashboardName -> "Session extended by fullscreen dashboard " + dashboardName)
+                .orElse("Session extended by fullscreen dashboard.");
+
+        log.info(logMessage);
+
+        return session.getMaxInactiveInterval();
+    }
+
+    private Optional<String> getDecodedDashboardNameFromUrl(final String url) {
+        final String[] split = url.split("/public/dashboards/");
+        if(split.length == 1) {
+            return Optional.empty();
+        } else {
+            try {
+                return Optional.of(URLDecoder.decode(split[split.length - 1], StandardCharsets.UTF_8.name()));
+            } catch(UnsupportedEncodingException e) {
+                throw new IllegalStateException("UTF-8 should be supported on all JVMs", e);
+            }
+        }
+    }
+
+    private boolean dashboardExists(final String decodedName) {
+        return dashConfig.getConfigResponse().getConfig().getDashboards()
+                .stream()
+                .map(Dashboard::getDashboardName)
+                .anyMatch(name -> name.equals(decodedName));
     }
 
     @ExceptionHandler(Exception.class)
