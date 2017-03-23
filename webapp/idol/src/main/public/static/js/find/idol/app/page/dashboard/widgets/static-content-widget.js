32f2ad492b3893d743a6e9bd2c173a5dfdaa3ae7
Ivo Miller <ivo.miller@hpe.com>
2017-03-23 09:45:20 +0000

    Export static content (FIND-1031)
    [rev. matthew.gordon]

--- webapp/idol/src/main/public/static/js/find/idol/app/page/dashboard/widgets/static-content-widget.js
+++ webapp/idol/src/main/public/static/js/find/idol/app/page/dashboard/widgets/static-content-widget.js
@@ -4,8 +4,9 @@
  */
 
 define([
+    'underscore',
     './widget'
-], function(Widget) {
+], function(_, Widget) {
     'use strict';
 
     return Widget.extend({
@@ -19,6 +20,56 @@ define([
             Widget.prototype.render.apply(this);
 
             this.$content.html(this.html);
+        },
+
+        exportData: function(){
+            const nodes = [];
+
+            const fontScale = 10 / 16;
+
+            // Try to traverse the content in a similar way that HTML would parse it, making bold text etc.
+            // We can't just use $el.text() since that loses all the formatting.
+            function traverse($el) {
+                _.each(_.filter($el.contents(), function(dom){
+                    return dom.nodeType === Node.TEXT_NODE || dom.nodeType === Node.ELEMENT_NODE
+                }), function(dom){
+                    if (dom.nodeType === Node.TEXT_NODE) {
+                        const last = _.last(nodes);
+                        if (last && last.text.slice(-1) !== '\n' && $el.css('display') !== 'inline') {
+                            last.text += '\n';
+                        }
+
+                        const trimmedText = dom.nodeValue.trim();
+
+                        if (trimmedText) {
+                            // text node, add with the current CSS
+                            const fontFamily = $el.css('font-family');
+                            const fontWeight = $el.css('font-weight');
+                            nodes.push({
+                                // we want to coalesce all whitespace into a single whitespace (to match HTML)
+                                text: dom.nodeValue.replace(/\s+/, ' '),
+                                fontSize: Math.round(parseInt($el.css('font-size')) * fontScale),
+                                // font weight might be a number > 400, bold/bolder etc. or built into font family
+                                bold: fontWeight > 400 || /bold/.test(fontWeight) || /Bold/i.test(fontFamily),
+                                // italic may be a property of the font style, font weight or font-family
+                                italic: /italic/.test($el.css('font-style')) || /Italic/i.test(fontFamily)
+                            })
+                        }
+                    }
+                    else if (dom.nodeType === Node.ELEMENT_NODE) {
+                        traverse($(dom))
+                    }
+                })
+            }
+
+            traverse(this.$content);
+
+            return {
+                data: {
+                    text: nodes
+                },
+                type: 'text'
+            };
         }
     });
 });
