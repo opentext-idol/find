6a16d6e39412c45bbf40335f58626e1b80b6fc78
Rosie Taylor <rosie.taylor@hpe.com>
2017-04-05 11:29:45 +0100

    [FIND-846] Add trending widget backend files
    [rev: ivo.miller]

---
+++ webapp/idol/src/main/java/com/hp/autonomy/frontend/find/idol/dashboards/widgets/TrendingWidgetSettings.java
@@ -0,0 +1,67 @@
+/*
+ * Copyright 2017 Hewlett Packard Enterprise Development Company, L.P.
+ * Licensed under the MIT License (the "License"); you may not use this file except in compliance with the License.
+ */
+
+package com.hp.autonomy.frontend.find.idol.dashboards.widgets;
+
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
+import com.fasterxml.jackson.annotation.JsonAnySetter;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonPOJOBuilder;
+import com.hp.autonomy.frontend.configuration.ConfigException;
+import com.hp.autonomy.frontend.configuration.SimpleComponent;
+import com.hp.autonomy.types.requests.idol.actions.tags.TagName;
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+@SuppressWarnings("MismatchedQueryAndUpdateOfCollection")
+@Builder
+@Data
+@EqualsAndHashCode(callSuper = false)
+@JsonDeserialize(builder = TrendingWidgetSettings.TrendingWidgetSettingsBuilder.class)
+public class TrendingWidgetSettings extends SimpleComponent<TrendingWidgetSettings> implements WidgetSettings<TrendingWidgetSettings> {
+    private static final String SECTION = "Trending Dashboard";
+    private final Map<String, Object> widgetSettings;
+    private final TagName parametricField;
+    private final TagName dateField;
+    private final Integer maxValues;
+    private final Integer numberOfBuckets;
+
+    @Override
+    public void basicValidate(final String section) throws ConfigException {
+        if (parametricField == null) {
+            throw new ConfigException(SECTION, "Trending dashboard config must specify a parametric field");
+        }
+
+        super.basicValidate(section);
+    }
+
+    @Override
+    @JsonAnyGetter
+    public Map<String, Object> getWidgetSettings() {
+        return Collections.unmodifiableMap(widgetSettings);
+    }
+
+    @SuppressWarnings({"FieldMayBeFinal", "WeakerAccess", "unused"})
+    @JsonPOJOBuilder(withPrefix = "")
+    public static class TrendingWidgetSettingsBuilder {
+        private Map<String, Object> widgetSettings = new HashMap<>();
+        private TagName parametricField;
+        private TagName dateField;
+        private Integer maxValues;
+        private Integer numberOfBuckets;
+
+        @SuppressWarnings("unused")
+        @JsonAnySetter
+        public TrendingWidgetSettingsBuilder widgetSetting(final String key, final Object value) {
+            widgetSettings.put(key, value);
+            return this;
+        }
+    }
+}
