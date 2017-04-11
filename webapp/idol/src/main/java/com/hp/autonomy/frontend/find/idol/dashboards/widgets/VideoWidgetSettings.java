d60e44b8dd04003f1d7c6e3b658cdaaf15b9ecbe
Ivo Miller <ivo.miller@hpe.com>
2017-04-11 11:47:40 +0100

    Video widget validation (FIND-1071)
    [rev. matthew.gordon]

---
+++ webapp/idol/src/main/java/com/hp/autonomy/frontend/find/idol/dashboards/widgets/VideoWidgetSettings.java
@@ -0,0 +1,50 @@
+package com.hp.autonomy.frontend.find.idol.dashboards.widgets;
+
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
+import com.fasterxml.jackson.annotation.JsonAnySetter;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonPOJOBuilder;
+import com.hp.autonomy.frontend.configuration.SimpleComponent;
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+@SuppressWarnings("MismatchedQueryAndUpdateOfCollection")
+@Data
+@Builder
+@JsonDeserialize(builder = VideoWidgetSettings.VideoWidgetSettingsBuilder.class)
+@EqualsAndHashCode(callSuper = false)
+public class VideoWidgetSettings extends SimpleComponent<VideoWidgetSettings> implements WidgetSettings<VideoWidgetSettings> {
+    private final Map<String, Object> widgetSettings;
+    private final Boolean loop;
+    private final Boolean audio;
+    private final Integer searchResultNumber;
+    private final Boolean restrictSearch;
+
+    @Override
+    @JsonAnyGetter
+    public Map<String, Object> getWidgetSettings() {
+        return Collections.unmodifiableMap(widgetSettings);
+    }
+
+    @SuppressWarnings({"WeakerAccess", "FieldMayBeFinal", "unused"})
+    @JsonPOJOBuilder(withPrefix = "")
+    public static class VideoWidgetSettingsBuilder {
+        private Map<String, Object> widgetSettings = new HashMap<>();
+        private Boolean loop;
+        private Boolean audio;
+        private Integer searchResultNumber;
+        private Boolean restrictSearch;
+
+        @SuppressWarnings("unused")
+        @JsonAnySetter
+        public VideoWidgetSettingsBuilder widgetSetting(final String key, final Object value) {
+            widgetSettings.put(key, value);
+            return this;
+        }
+    }
+}
