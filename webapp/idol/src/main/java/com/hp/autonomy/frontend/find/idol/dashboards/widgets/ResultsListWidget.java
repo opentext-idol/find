42e30fed9be4458fffb418e328f13c71729aa67d
Ivo Miller <ivo.miller@hpe.com>
2017-04-11 11:30:38 +0100

    Results list widget validation (FIND-1071)
    [rev. matthew.gordon]

---
+++ webapp/idol/src/main/java/com/hp/autonomy/frontend/find/idol/dashboards/widgets/ResultsListWidget.java
@@ -0,0 +1,26 @@
+package com.hp.autonomy.frontend.find.idol.dashboards.widgets;
+
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonPOJOBuilder;
+import com.hp.autonomy.frontend.find.idol.dashboards.widgets.datasources.WidgetDatasource;
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+
+@SuppressWarnings("WeakerAccess")
+@Data
+@EqualsAndHashCode(callSuper = true)
+@ToString(callSuper = true)
+@JsonDeserialize(builder = ResultsListWidget.ResultsListWidgetBuilder.class)
+public class ResultsListWidget extends Widget<ResultsListWidget, ResultsListWidgetSettings> {
+    @SuppressWarnings("ConstructorWithTooManyParameters")
+    @Builder(toBuilder = true)
+    public ResultsListWidget(final String name, final String type, final Integer x, final Integer y, final Integer width, final Integer height, final WidgetDatasource<?, ?> datasource, final ResultsListWidgetSettings widgetSettings) {
+        super(name, type, x, y, width, height, datasource, widgetSettings);
+    }
+
+    @JsonPOJOBuilder(withPrefix = "")
+    public static class ResultsListWidgetBuilder {
+    }
+}
---
+++ webapp/idol/src/test/java/com/hp/autonomy/frontend/find/idol/dashboards/widgets/ResultsListWidgetTest.java
@@ -0,0 +1,136 @@
+/*
+ * Copyright 2017 Hewlett Packard Enterprise Development Company, L.P.
+ * Licensed under the MIT License (the "License"); you may not use this file except in compliance with the License.
+ */
+
+package com.hp.autonomy.frontend.find.idol.dashboards.widgets;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.hp.autonomy.frontend.configuration.ConfigurationComponentTest;
+import com.hp.autonomy.frontend.find.core.savedsearches.SavedSearchType;
+import com.hp.autonomy.frontend.find.idol.dashboards.widgets.datasources.SavedSearch;
+import com.hp.autonomy.frontend.find.idol.dashboards.widgets.datasources.SavedSearchConfig;
+import com.hp.autonomy.frontend.find.idol.dashboards.widgets.datasources.WidgetDatasource;
+import com.hp.autonomy.frontend.find.idol.dashboards.widgets.datasources.WidgetDatasourceMixins;
+import org.apache.commons.io.IOUtils;
+import org.springframework.boot.test.json.JacksonTester;
+import org.springframework.boot.test.json.JsonContent;
+import org.springframework.boot.test.json.ObjectContent;
+import org.springframework.core.ResolvableType;
+
+import java.io.IOException;
+
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class ResultsListWidgetTest extends ConfigurationComponentTest<ResultsListWidget> {
+    @Override
+    public void setUp() {
+        final ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.addMixIn(WidgetDatasource.class, WidgetDatasourceMixins.class);
+        json = new JacksonTester<>(getClass(), ResolvableType.forClass(getType()), objectMapper);
+    }
+
+    @Override
+    protected Class<ResultsListWidget> getType() {
+        return ResultsListWidget.class;
+    }
+
+    @Override
+    protected ResultsListWidget constructComponent() {
+        return ResultsListWidget.builder()
+                .name("Test Widget")
+                .type("ResultsListWidget")
+                .x(1)
+                .y(1)
+                .width(1)
+                .height(1)
+                .datasource(SavedSearch.builder()
+                        .source("SavedSearch")
+                        .config(SavedSearchConfig.builder()
+                                .id(123L)
+                                .type(SavedSearchType.QUERY)
+                                .build())
+                        .build())
+                .build();
+    }
+
+    @Override
+    protected String sampleJson() throws IOException {
+        return IOUtils.toString(
+                getClass().getResourceAsStream("/com/hp/autonomy/frontend/find/idol/dashboards/widgets/resultsListWidget.json")
+        );
+    }
+
+    @Override
+    protected void validateJson(final JsonContent<ResultsListWidget> jsonContent) {
+        jsonContent.assertThat()
+                .hasJsonPathStringValue("$.name", "Test Widget")
+                .hasJsonPathStringValue("$.type", "ResultsListWidget")
+                .hasJsonPathNumberValue("$.x", 1)
+                .hasJsonPathNumberValue("$.y", 1)
+                .hasJsonPathNumberValue("$.width", 1)
+                .hasJsonPathNumberValue("$.height", 1)
+                .hasJsonPathNumberValue("$.datasource.config.id", 123)
+                .hasJsonPathStringValue("$.datasource.config.type", "QUERY");
+    }
+
+    @Override
+    protected void validateParsedComponent(final ObjectContent<ResultsListWidget> objectContent) {
+        objectContent.assertThat().isEqualTo(
+                ResultsListWidget.builder()
+                        .name("List")
+                        .type("ResultsListWidget")
+                        .x(1)
+                        .y(1)
+                        .width(2)
+                        .height(2)
+                        .datasource(SavedSearch.builder()
+                                .source("SavedSearch")
+                                .config(SavedSearchConfig.builder()
+                                        .id(97L)
+                                        .type(SavedSearchType.QUERY)
+                                        .build())
+                                .build())
+                        .widgetSettings(ResultsListWidgetSettings.builder()
+                                .maxResults(6)
+                                .sort("random")
+                                .columnLayout(true)
+                                .widgetSetting("testing", "testing")
+                                .build())
+                        .build()
+        );
+    }
+
+    @Override
+    protected void validateMergedComponent(final ObjectContent<ResultsListWidget> objectContent) {
+        objectContent.assertThat().isEqualTo(
+                ResultsListWidget.builder()
+                        .name("Test Widget")
+                        .type("ResultsListWidget")
+                        .x(1)
+                        .y(1)
+                        .width(1)
+                        .height(1)
+                        .datasource(SavedSearch.builder()
+                                .source("SavedSearch")
+                                .config(SavedSearchConfig.builder()
+                                        .id(123L)
+                                        .type(SavedSearchType.QUERY)
+                                        .build())
+                                .build())
+                        .widgetSettings(ResultsListWidgetSettings.builder()
+                                .maxResults(6)
+                                .sort("random")
+                                .columnLayout(true)
+                                .widgetSetting("testing", "testing")
+                                .build())
+                        .build()
+        );
+    }
+
+    @Override
+    protected void validateString(final String s) {
+        assertThat(s, containsString("name"));
+    }
+}
