3b8e3f720820cb8829099fd0f01bc1f65467e413
Ivo Miller <ivo.miller@hpe.com>
2017-04-11 11:38:44 +0100

    TopicMap widget validation (FIND-1071)
    [rev. matthew.gordon]

---
+++ webapp/idol/src/main/java/com/hp/autonomy/frontend/find/idol/dashboards/widgets/TopicMapWidget.java
@@ -0,0 +1,26 @@
+package com.hp.autonomy.frontend.find.idol.dashboards.widgets;
+
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonPOJOBuilder;
+import com.hp.autonomy.frontend.find.idol.dashboards.widgets.datasources.WidgetDatasource;
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+
+@SuppressWarnings("WeakerAccess")
+@Data
+@EqualsAndHashCode(callSuper = true)
+@ToString(callSuper = true)
+@JsonDeserialize(builder = TopicMapWidget.TopicMapWidgetBuilder.class)
+public class TopicMapWidget extends Widget<TopicMapWidget, TopicMapWidgetSettings> {
+    @SuppressWarnings("ConstructorWithTooManyParameters")
+    @Builder(toBuilder = true)
+    public TopicMapWidget(final String name, final String type, final Integer x, final Integer y, final Integer width, final Integer height, final WidgetDatasource<?, ?> datasource, final TopicMapWidgetSettings widgetSettings) {
+        super(name, type, x, y, width, height, datasource, widgetSettings);
+    }
+
+    @JsonPOJOBuilder(withPrefix = "")
+    public static class TopicMapWidgetBuilder {
+    }
+}
