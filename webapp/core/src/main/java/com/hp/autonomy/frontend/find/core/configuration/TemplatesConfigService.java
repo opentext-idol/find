7acf2facf02c2cf1766309449dadcb45ed6cf49f
Matthew Gordon <matthew.gordon2@hpe.com>
2017-05-16 10:21:26 +0100

    [FIND-1282] Exposed customisation config directory as a constant.
    Also refactored customisation config services to reduce boiler
    plate and duplication.
    
    [rev: wojciech.kaluza]

--- webapp/core/src/main/java/com/hp/autonomy/frontend/find/core/configuration/TemplatesConfigService.java
+++ webapp/core/src/main/java/com/hp/autonomy/frontend/find/core/configuration/TemplatesConfigService.java
@@ -1,68 +1,17 @@
 package com.hp.autonomy.frontend.find.core.configuration;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
-import com.hp.autonomy.frontend.configuration.BaseConfigFileService;
-import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 
 @Service
-public class TemplatesConfigService extends BaseConfigFileService<TemplatesConfig>{
+public class TemplatesConfigService extends CustomisationConfigService<TemplatesConfig> {
     public TemplatesConfigService() throws IOException {
-        final ObjectMapper objectMapper = new Jackson2ObjectMapperBuilder()
-                .featuresToEnable(SerializationFeature.INDENT_OUTPUT)
-                .createXmlMapper(false)
-                .build();
-
-        setMapper(objectMapper);
-        setConfigFileLocation(FindConfigFileService.CONFIG_FILE_LOCATION);
-        setConfigFileName("customization/templates/templates.json");
-        setDefaultConfigFile("/defaultTemplatesConfigFile.json");
+        super(
+                "templates.json",
+                "defaultTemplatesConfigFile.json",
+                TemplatesConfig.class,
+                TemplatesConfig.builder().build()
+        );
     }
-
-    @Override
-    public void init() throws Exception {
-        Files.createDirectories(Paths.get(getConfigFileLocation()).getParent());
-        super.init();
-    }
-
-    @Override
-    public void postInitialise(final TemplatesConfig config) throws Exception {}
-
-    @Override
-    public Class<TemplatesConfig> getConfigClass() {
-        return TemplatesConfig.class;
-    }
-
-    @Override
-    public TemplatesConfig getEmptyConfig() {
-        return TemplatesConfig.builder().build();
-    }
-
-    @Override
-    public TemplatesConfig generateDefaultLogin(final TemplatesConfig config) {
-        return config;
-    }
-
-    @Override
-    public TemplatesConfig withoutDefaultLogin(final TemplatesConfig config) {
-        return config;
-    }
-
-    @Override
-    public TemplatesConfig withHashedPasswords(final TemplatesConfig config) {
-        return config;
-    }
-
-    @Override
-    public TemplatesConfig preUpdate(final TemplatesConfig config) {
-        return config;
-    }
-
-    @Override
-    public void postUpdate(final TemplatesConfig config) throws Exception {}
 }
--- webapp/idol/src/main/java/com/hp/autonomy/frontend/find/idol/dashboards/IdolDashboardConfigService.java
+++ webapp/idol/src/main/java/com/hp/autonomy/frontend/find/idol/dashboards/IdolDashboardConfigService.java
@@ -7,11 +7,9 @@ package com.hp.autonomy.frontend.find.idol.dashboards;
 
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.JsonDeserializer;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.google.common.collect.ImmutableMap;
-import com.hp.autonomy.frontend.configuration.BaseConfigFileService;
-import com.hp.autonomy.frontend.find.core.configuration.FindConfigFileService;
+import com.hp.autonomy.frontend.find.core.configuration.CustomizationConfigService;
 import com.hp.autonomy.frontend.find.idol.dashboards.widgets.TagNameSerializer;
 import com.hp.autonomy.frontend.find.idol.dashboards.widgets.Widget;
 import com.hp.autonomy.frontend.find.idol.dashboards.widgets.WidgetMixins;
@@ -22,70 +20,21 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 import org.springframework.stereotype.Service;
 
-import java.nio.file.Files;
-import java.nio.file.Paths;
-
 @Service
-public class IdolDashboardConfigService extends BaseConfigFileService<IdolDashboardConfig> {
+public class IdolDashboardConfigService extends CustomizationConfigService<IdolDashboardConfig> {
     @Autowired
     public IdolDashboardConfigService(final JsonDeserializer<TagName> tagNameDeserializer) {
-        final ObjectMapper objectMapper = new Jackson2ObjectMapperBuilder()
-                .mixIn(Widget.class, WidgetMixins.class)
-                .mixIn(WidgetDatasource.class, WidgetDatasourceMixins.class)
-                .deserializersByType(ImmutableMap.of(TagName.class, tagNameDeserializer))
-                .serializersByType(ImmutableMap.of(TagName.class, new TagNameSerializer()))
-                .featuresToEnable(SerializationFeature.INDENT_OUTPUT)
-                .featuresToDisable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, DeserializationFeature.ADJUST_DATES_TO_CONTEXT_TIME_ZONE)
-                .createXmlMapper(false)
-                .build();
-
-        setMapper(objectMapper);
-        setConfigFileLocation(FindConfigFileService.CONFIG_FILE_LOCATION);
-        setConfigFileName("customization/dashboards.json");
-        setDefaultConfigFile("/defaultDashboardsConfigFile.json");
+        super(
+                "dashboards.json",
+                "defaultDashboardsConfigFile.json",
+                IdolDashboardConfig.class,
+                IdolDashboardConfig.builder().build(),
+                new Jackson2ObjectMapperBuilder()
+                        .mixIn(Widget.class, WidgetMixins.class)
+                        .mixIn(WidgetDatasource.class, WidgetDatasourceMixins.class)
+                        .deserializersByType(ImmutableMap.of(TagName.class, tagNameDeserializer))
+                        .serializersByType(ImmutableMap.of(TagName.class, new TagNameSerializer()))
+                        .featuresToDisable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, DeserializationFeature.ADJUST_DATES_TO_CONTEXT_TIME_ZONE)
+        );
     }
-
-    @SuppressWarnings("ProhibitedExceptionDeclared")
-    @Override
-    public void init() throws Exception {
-        // need to make sure the sub directory exists
-        Files.createDirectories(Paths.get(getConfigFileLocation()).getParent());
-        super.init();
-    }
-
-    @Override
-    public IdolDashboardConfig getEmptyConfig() {
-        return IdolDashboardConfig.builder().build();
-    }
-
-    @Override
-    public Class<IdolDashboardConfig> getConfigClass() {
-        return IdolDashboardConfig.class;
-    }
-
-    @Override
-    public IdolDashboardConfig generateDefaultLogin(final IdolDashboardConfig config) {
-        return config;
-    }
-
-    @Override
-    public IdolDashboardConfig withoutDefaultLogin(final IdolDashboardConfig config) {
-        return config;
-    }
-
-    @Override
-    public IdolDashboardConfig withHashedPasswords(final IdolDashboardConfig config) {
-        return config;
-    }
-
-    @Override
-    public IdolDashboardConfig preUpdate(final IdolDashboardConfig idolDashboardConfig) {
-        return idolDashboardConfig;
-    }
-
-    @Override
-    public void postInitialise(final IdolDashboardConfig config) {}
-
-    @Override
-    public void postUpdate(final IdolDashboardConfig idolDashboardConfig) {}
 }
