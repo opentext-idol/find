f6b6be92ce4dc19c86a6624a569bf05986907364
Matthew Gordon <matthew.gordon2@hpe.com>
2017-05-16 11:42:20 +0100

    [FIND-1282] Use correct name for CustomizationConfigService.
    Fixes compilation error.
    
    [rev: will.byrne]

--- webapp/core/src/main/java/com/hp/autonomy/frontend/find/core/configuration/TemplatesConfigService.java
+++ webapp/core/src/main/java/com/hp/autonomy/frontend/find/core/configuration/TemplatesConfigService.java
@@ -2,11 +2,9 @@ package com.hp.autonomy.frontend.find.core.configuration;
 
 import org.springframework.stereotype.Service;
 
-import java.io.IOException;
-
 @Service
-public class TemplatesConfigService extends CustomisationConfigService<TemplatesConfig> {
-    public TemplatesConfigService() throws IOException {
+public class TemplatesConfigService extends CustomizationConfigService<TemplatesConfig> {
+    public TemplatesConfigService() {
         super(
                 "templates.json",
                 "defaultTemplatesConfigFile.json",
