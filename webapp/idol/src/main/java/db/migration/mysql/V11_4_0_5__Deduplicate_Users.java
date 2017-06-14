feb17ca832bf9fc557651565d97973b7db51e9f2
Rosie Taylor <rosie.taylor@hpe.com>
2017-06-14 15:51:04 +0100

    [FIND-1400] Remove duplicate users in database
    Reassign any searches for the deleted users to the remaining user with the same username.
    
    [rev: matt.gordon]

---
+++ webapp/idol/src/main/java/db/migration/mysql/V11_4_0_5__Deduplicate_Users.java
@@ -0,0 +1,12 @@
+/*
+ * Copyright 2017 Hewlett-Packard Enterprise Development Company, L.P.
+ * Licensed under the MIT License (the "License"); you may not use this file except in compliance with the License.
+ */
+
+package db.migration.mysql;
+
+import db.migration.AbstractDeduplicateUsers;
+
+@SuppressWarnings("unused")
+public class V11_4_0_5__Deduplicate_Users extends AbstractDeduplicateUsers {
+}
