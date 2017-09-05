5f1a96138fe0de81710c80c7b6fb660da577cf78
Tung Jin Chew <tung-jin.chew@hpe.com>
2017-09-05 11:47:32 +0100

    [FIND-1202] Fix the locations map in the expanded documents preview

--- webapp/core/src/main/public/static/js/find/app/page/search/document/location-tab.js
+++ webapp/core/src/main/public/static/js/find/app/page/search/document/location-tab.js
@@ -1,5 +1,5 @@
 /*
- * Copyright 2016 Hewlett-Packard Development Company, L.P.
+ * Copyright 2017 Hewlett-Packard Development Company, L.P.
  * Licensed under the MIT License (the "License"); you may not use this file except in compliance with the License.
  */
 
@@ -33,7 +33,7 @@ define([
         },
 
         render: function() {
-            var locations = this.model.get('locations');
+            var locationsMap = this.model.get('locations');
 
             this.$el.html(this.template({
                 i18n: i18n
@@ -41,23 +41,24 @@ define([
 
             this.mapResultsView.setElement(this.$('.location-tab-map').get(0)).render();
 
-            var markers = _.map(locations, function(location) {
-                var longitude = location.longitude;
-                var latitude = location.latitude;
-                var title = this.model.get('title');
+            var markers = _.flatten(_.map(locationsMap, function(locations){
+                return _.map(locations, function(location){
+                    var longitude = location.longitude;
+                    var latitude = location.latitude;
 
-                var popover = this.popoverTemplate({
-                    i18n: i18n,
-                    title: location.displayName,
-                    summary: addLinksToSummary(this.model.get('summary')),
-                    cidForClickRouting: null
-                });
+                    var popover = this.popoverTemplate({
+                        i18n: i18n,
+                        title: location.displayName,
+                        summary: addLinksToSummary(this.model.get('summary')),
+                        cidForClickRouting: null
+                    });
 
-                return this.mapResultsView.getMarker(latitude, longitude, this.getIcon(location.displayName), location.displayName, popover);
-            }, this);
+                    return this.mapResultsView.getMarker(latitude, longitude, this.getIcon(location.displayName), location.displayName, popover);
+                }, this)
+            }, this))
 
             this.mapResultsView.addMarkers(markers, false);
-            this.mapResultsView.loaded();
+            this.mapResultsView.fitMapToMarkerBounds();
         },
 
         getIcon: function(displayName) {
