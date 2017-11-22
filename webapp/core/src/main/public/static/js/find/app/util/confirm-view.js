683f82fab0689fa1e16c39833b358002db6f971e
Tung Jin Chew <tung-jin.chew@hpe.com>
2017-11-22 16:46:43 +0000

    (FIND-1492) Pressing the 'Escape' key now should close closable confirm views (e.g. when deleting searches, reverting configs, or deleting uploaded custom image assets)

--- webapp/core/src/main/public/static/js/find/app/util/confirm-view.js
+++ webapp/core/src/main/public/static/js/find/app/util/confirm-view.js
@@ -5,13 +5,24 @@
 
 define([
     'js-whatever/js/confirm-view',
+    'find/app/util/global-key-listener',
     'text!find/templates/app/page/settings/confirm-modal.html',
     'underscore'
-], function(Confirm, confirmTemplate, _) {
+], function(Confirm, globalKeyListener, confirmTemplate, _) {
     'use strict';
 
     return Confirm.extend({
         className: 'modal fade',
-        template: _.template(confirmTemplate)
+        template: _.template(confirmTemplate),
+
+        initialize: function(config){
+            Confirm.prototype.initialize.apply(this, arguments);
+
+            if (config.closable) {
+                this.listenTo(globalKeyListener, 'escape', function(){
+                    this.$el.modal('hide');
+                })
+            }
+        }
     });
 });
