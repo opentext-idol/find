3d76d9c6850173ce9a65b477e3c164cb482dd0a6
Tung Jin Chew <tung-jin.chew@hpe.com>
2017-11-24 16:19:43 +0000

    (DEMOS-72, CORE-4439) Make the entity search answer server configurable under the entity search section; instead of as a collection of custom properties.

--- webapp/idol/src/main/java/com/hp/autonomy/frontend/find/idol/answer/TempAnswerServerController.java
+++ webapp/idol/src/main/java/com/hp/autonomy/frontend/find/idol/answer/TempAnswerServerController.java
@@ -9,8 +9,10 @@ import com.autonomy.aci.client.services.AciService;
 import com.autonomy.aci.client.services.Processor;
 import com.autonomy.aci.client.transport.AciServerDetails;
 import com.autonomy.aci.client.util.AciParameters;
-import com.hp.autonomy.searchcomponents.idol.answer.ask.AskAnswerServerRequestBuilder;
-import com.hp.autonomy.searchcomponents.idol.answer.ask.AskAnswerServerService;
+import com.hp.autonomy.frontend.configuration.ConfigService;
+import com.hp.autonomy.frontend.find.idol.configuration.EntitySearchConfig;
+import com.hp.autonomy.frontend.find.idol.configuration.IdolFindConfig;
+import com.hp.autonomy.searchcomponents.idol.answer.configuration.AnswerServerConfig;
 import com.hp.autonomy.searchcomponents.idol.search.HavenSearchAciParameterHandler;
 import com.hp.autonomy.types.idol.marshalling.ProcessorFactory;
 import com.hp.autonomy.types.idol.responses.answer.AskAnswer;
@@ -20,15 +22,13 @@ import com.hp.autonomy.types.requests.idol.actions.answer.params.AskParams;
 import com.hp.autonomy.types.requests.idol.actions.answer.params.AskSortParam;
 import com.hp.autonomy.types.requests.idol.actions.query.params.QueryParams;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
-import javax.xml.xpath.XPath;
-import javax.xml.xpath.XPathExpression;
 import javax.xml.xpath.XPathExpressionException;
-import javax.xml.xpath.XPathFactory;
-import org.springframework.beans.factory.ObjectFactory;
+import org.apache.commons.collections.CollectionUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -43,58 +43,26 @@ import static com.hp.autonomy.frontend.find.idol.answer.AnswerServerController.T
 @RequestMapping(AnswerServerController.BASE_PATH)
 class TempAnswerServerController {
 
-    private final AskAnswerServerService askAnswerServerService;
-    private final ObjectFactory<AskAnswerServerRequestBuilder> requestBuilderFactory;
     private final AciService aciService;
-
     private final HavenSearchAciParameterHandler parameterHandler;
+    private final ConfigService<IdolFindConfig> configService;
 
     private final Processor<AskResponsedata> processor;
 
-    private final XPathExpression xAnswer;
-    private final XPathExpression xAnswerText;
-    private final XPathExpression xScore;
-    private final XPathExpression xSource;
-    private final XPathExpression xSystemName;
-    private final XPathExpression xInterpretation;
-
     @Autowired
-    TempAnswerServerController(final AskAnswerServerService askAnswerServerService,
-                               final ObjectFactory<AskAnswerServerRequestBuilder> requestBuilderFactory,
-                               final AciService aciService,
+    TempAnswerServerController(final AciService aciService,
                                final HavenSearchAciParameterHandler parameterHandler,
+                               final ConfigService<IdolFindConfig> configService,
                                final ProcessorFactory processorFactory) {
-        this.askAnswerServerService = askAnswerServerService;
-        this.requestBuilderFactory = requestBuilderFactory;
         this.aciService = aciService;
         this.parameterHandler = parameterHandler;
+        this.configService = configService;
         processor = processorFactory.getResponseDataProcessor(AskResponsedata.class);
-
-
-        final XPathFactory xPathFactory = XPathFactory.newInstance();
-        final XPath xPath = xPathFactory.newXPath();
-        try {
-            xAnswer = xPath.compile("/autnresponse/responsedata/answers/answer");
-            xAnswerText = xPath.compile("text");
-            xScore = xPath.compile("score");
-            xSource = xPath.compile("source");
-            xSystemName = xPath.compile("@system_name");
-            xInterpretation = xPath.compile("interpretation");
-        }
-        catch(XPathExpressionException e) {
-            throw new Error("Invalid XPaths", e);
-        }
-
     }
 
     @RequestMapping(value = "ask-demo", method = RequestMethod.GET)
     public List<AskAnswer> ask(
             @RequestParam(TEXT_PARAM) final String text,
-           @Value("${temp.answerserver.host}") final String host,
-           @Value("${temp.answerserver.port}") final int port,
-            @Value("${temp.answerserver.databaseMatch}") final String databaseMatch,
-            @Value("${temp.answerserver.printFields}") final String printFields,
-           @Value("${temp.answerserver.systemNames}") final List<String> systemNames,
             @RequestParam(value = MAX_RESULTS_PARAM, required = false)
             final Integer maxResults
     ) throws XPathExpressionException {
@@ -104,13 +72,16 @@ class TempAnswerServerController {
             return toReturn;
         }
 
-        final AciServerDetails details = new AciServerDetails();
-        details.setHost(host);
-        details.setPort(port);
+        final IdolFindConfig config = configService.getConfig();
 
-        for(String systemName : systemNames) {
-            // We deliberately do each systemname in sequence, rather than in parallel,
-            //   since FactBank is faster than PassageExtraction
+        final EntitySearchConfig entitySearch = config.getEntitySearch();
+        final AnswerServerConfig answerServer = entitySearch.getAnswerServer();
+        final AciServerDetails details = answerServer.toAciServerDetails();
+        final Collection<String> systemNames = answerServer.getSystemNames();
+
+        // We deliberately do each systemName in sequence since FactBank is faster than PassageExtraction.
+        // If the systemNames aren't listed, we'll use null, which means we'll try all configured answer server systems.
+        for(String systemName : CollectionUtils.isEmpty(systemNames) ? Collections.<String>singletonList(null) : systemNames) {
             final AciParameters params = new AciParameters("ask");
 
             params.add(AskParams.Sort.name(), AskSortParam.CONFIDENCE);
@@ -118,8 +89,8 @@ class TempAnswerServerController {
             params.add(AskParams.SystemNames.name(), systemName);
             // These have been added to the answer server config as
             // [Server] AllowedQueryParameters=DatabaseMatch,SecurityInfo,PrintFields
-            params.add(QueryParams.DatabaseMatch.name(), databaseMatch);
-            params.add(QueryParams.PrintFields.name(), printFields);
+            params.add(QueryParams.DatabaseMatch.name(), entitySearch.getAnswerServerDatabaseMatch());
+            params.add(QueryParams.PrintFields.name(), entitySearch.getAnswerServerPrintFields());
             parameterHandler.addSecurityInfo(params);
 
             if (maxResults != null) {
