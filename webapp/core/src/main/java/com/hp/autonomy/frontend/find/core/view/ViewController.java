442dc9faaaa9841bbb77beb7ced85a13aa6a3ad0
Tung Jin Chew <tung-jin.chew@hpe.com>
2018-03-20 11:10:54 +0000

    (FIND-1531) Cleanup: delete unnecessary TODO: profile updates are now done in the IdolViewController instead.

--- webapp/core/src/main/java/com/hp/autonomy/frontend/find/core/view/ViewController.java
+++ webapp/core/src/main/java/com/hp/autonomy/frontend/find/core/view/ViewController.java
@@ -46,8 +46,6 @@ public abstract class ViewController<R extends ViewRequest<S>, S extends Seriali
             @RequestParam(value = HIGHLIGHT_PARAM, required = false) final String highlightExpression,
             final HttpServletResponse response
     ) throws E, IOException {
-        // TODO: tag the document
-
         response.setContentType(MediaType.TEXT_HTML_VALUE);
         ViewContentSecurityPolicy.addContentSecurityPolicy(response);
         final R request = viewRequestBuilderFactory.getObject()
