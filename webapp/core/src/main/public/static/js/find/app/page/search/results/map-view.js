opentext-idol/find

commit 48ad7889598be5e59727f9588367b42398cc02b0

(FIND-1540) Add colour marker mappings for all the awesome-marker-icon-* colours; so that it uses the correct colour marker when exporting the map dashboard to PowerPoint. Previously it'd default to the blue marker if you used anything other than green/orange/red/blue.

webapp/core/src/main/public/static/js/find/app/page/search/results/map-view.js
@@ -22,10 +22,25 @@ define([
     const INITIAL_ZOOM = 3;
 
     const leafletMarkerColorMap = {
-        'green': '#70ad25',
-        'orange': '#f0932f',
         'red': '#d33d2a',
-        'blue': '#37a8da'
+        'orange': '#f0932f',
+        'green': '#70ad25',
+        'blue': '#37a8da',
+        'purple': '#c64daf',
+        'darkred': '#9f3235',
+        'darkblue': '#0066a2',
+        'darkgreen': '#6d7c22',
+        'darkpurple': '#543563',
+        'cadetblue': '#406471',
+        'lightred': '#ff8676',
+        'beige': '#ffbc74',
+        'lightgreen': '#b8f271',
+        'lightblue': '#7dd5ff',
+        'pink': '#f888e2',
+        'white': '#fbfbfb',
+        'lightgray': '#9e9e9e',
+        'gray': '#555555',
+        'black': '#2f2f2f',
     };
 
     function leftPadHex(str) {
@@ -295,7 +310,7 @@ define([
                     }
 
                     const $pathEl = $(layer._path);
-                    const color = d3.rgb($pathEl.css('fill')) + '';
+                    const color = hexColor($pathEl.css('fill'));
 
                     const latLngsRings = layer.getLatLngs();
 

commit 2b46d6ee52d1f8dc370f3c8c4e910c6aa2507910

(FIND-1533) Fix PPT export in IE, have to use a hex string so that the server can parse the colour; was a regression from 48ad788. For some reason IE was using a colour string 'red'.

webapp/core/src/main/public/static/js/find/app/page/search/results/map-view.js
@@ -14,8 +14,7 @@ define([
     'Leaflet.awesome-markers',
     'leaflet.markercluster',
     'leaflet.markercluster.layersupport',
-    'html2canvas',
-    'd3'
+    'html2canvas'
 ], function(_, $, Backbone, d3, configuration, vent, leaflet) {
     'use strict';
 
@@ -62,7 +61,7 @@ define([
         } else if(match = /rgb\((\d+),\s*(\d+),\s*(\d+)\)/.exec(str)) {
             return '#' + leftPadMatch(match);
         } else {
-            return str;
+            return d3.rgb(str).toString();
         }
     }
 
@@ -246,6 +245,7 @@ define([
 
             const map = this.map;
             const mapSize = map.getSize();
+            const mapBounds = leaflet.bounds(leaflet.point(0, 0), mapSize);
             const $mapEl = $(map.getContainer());
             const markers = [];
             const polygons = [];
@@ -317,35 +317,26 @@ define([
                     const $pathEl = $(layer._path);
                     const color = hexColor($pathEl.css('fill'));
 
-                    const latLngsRings = layer.getLatLngs();
-
-                    const projected = latLngsRings.map(function(latLngs){
-                        return latLngs.map(function(latLng){
+                    const pointRings = _.reduce(layer.getLatLngs(), function(pointRings, latLngRing){
+                        const clippedPoints = leaflet.PolyUtil.clipPolygon(latLngRing.map(function(latLng){
                             return map.latLngToContainerPoint(latLng);
-                        })
-                    });
-
-                    const mapBounds = leaflet.bounds(leaflet.point(0, 0), mapSize);
-
-                    const points = [];
-
-                    projected.forEach(function(projectedPoints){
-                        const list = [];
-
-                        const clippedPoints = leaflet.PolyUtil.clipPolygon(projectedPoints, mapBounds)
+                        }), mapBounds);
 
                         if (clippedPoints.length) {
-                            _.each(clippedPoints, function(pt){
-                                list.push(pt.x / mapSize.x, pt.y / mapSize.y);
-                            })
+                            const pointRing = _.reduce(clippedPoints, function(pointRing, point){
+                                pointRing.push(point.x / mapSize.x, point.y / mapSize.y);
+                                return pointRing;
+                            }, []);
 
-                            points.push(list);
+                            pointRings.push(pointRing);
                         }
-                    });
 
-                    if (points.length) {
+                        return pointRings;
+                    }, []);
+
+                    if (pointRings.length) {
                         polygons.push({
-                            points: points,
+                            points: pointRings,
                             text: text,
                             color: color
                         })