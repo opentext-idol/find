e16309ed21138b915e4ae4ce70834f1635113f72
Tung Jin Chew <tung-jin.chew@hpe.com>
2018-04-17 10:38:16 +0100

    (FIND-1542) Allow two-letter codes in the wikipedia URL for thumbnailing (used for languages, e.g. 'https://upload.wikimedia.org/wikipedia/en/2/2e/Developing_countries_map.png')
    (cherry picked from commit 18f9efc)

--- webapp/core/src/main/public/static/js/find/app/page/search/template-helpers/wiki-thumbnail-helper.js
+++ webapp/core/src/main/public/static/js/find/app/page/search/template-helpers/wiki-thumbnail-helper.js
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017 Micro Focus International plc.
+ * Copyright 2017-2018 Micro Focus International plc.
  * Licensed under the MIT License (the "License"); you may not use this file except in compliance with the License.
  */
 
@@ -8,7 +8,7 @@ define([
 ], function(_) {
 
     return function(value, size) {
-        return value && String(value).replace(/(\/wikipedia\/commons\/)([^/]+\/[^/]+\/([^/]+\.(jpg|jpeg|gif|png|svg)))/i, function(all, first, second, filename, extension){
+        return value && String(value).replace(/(\/wikipedia\/(?:commons|\w{2})\/)([^/]+\/[^/]+\/([^/]+\.(jpg|jpeg|gif|png|svg)))/i, function(all, first, second, filename, extension){
             const url = first + 'thumb/' + second + '/' + (size||300) + 'px-' + filename;
 
             if (extension && extension.toLowerCase() === 'svg') {
