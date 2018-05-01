14de068daece2a270ec528a65f36a61b55c23702
Tung Jin Chew <tung-jin.chew@hpe.com>
2018-05-01 10:33:49 +0100

    (WIP FIND-1545) The polygon spatial toolbar should be in its own directory

--- webapp/core/src/main/public/static/js/require-config.js
+++ webapp/core/src/main/public/static/js/require-config.js
@@ -40,7 +40,7 @@ require.config({
         'leaflet.draw': '../bower_components/leaflet-draw/dist/leaflet.draw-src',
         'leaflet.draw.i18n': 'leaflet.draw.i18n/leaflet.draw.i18n',
         'leaflet.draw.negate': 'leaflet.draw.negate/leaflet.draw.negate',
-        'leaflet.draw.polygonSpatial': 'leaflet.draw.negate/leaflet.draw.polygonSpatial',
+        'leaflet.draw.polygonSpatial': 'leaflet.draw.polygonSpatial/leaflet.draw.polygonSpatial',
         'leaflet.markercluster': '../bower_components/leaflet.markercluster/dist/leaflet.markercluster-src',
         'leaflet.markercluster.layersupport': '../bower_components/leaflet.markercluster.layersupport/src/layersupport',
         'leaflet.notransform': 'leaflet.notransform/leaflet.notransform',
