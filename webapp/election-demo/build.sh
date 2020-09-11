e99f4c38ec93c135f370d95af87975fb63744cf2
Joseph Lansdowne <joseph.lansdowne@microfocus.com>
2020-09-11 17:37:56 +0100

    move frontend to own repo

--- webapp/election-demo/build.sh
+++
@@ -1,9 +0,0 @@
-#! /usr/bin/env bash
-
-project_dir="$(dirname "$0")"
-
-FIND_BASE_URL=http://localhost:8080 \
-    MMAP_BASE_URL=http://demo.havendemo.com/mmap_1 \
-    node "$project_dir"/backend/build-data &&
-    cd "$project_dir"/frontend &&
-    npm run build
