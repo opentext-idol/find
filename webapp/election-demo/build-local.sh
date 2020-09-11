79ded2f5d4241cfc2067d24a9857299810ddb577
Joseph Lansdowne <joseph.lansdowne@microfocus.com>
2020-09-11 17:05:05 +0100

    adjust build scripts

--- webapp/election-demo/build-local.sh
+++ webapp/election-demo/build-local.sh
@@ -2,7 +2,8 @@
 
 project_dir="$(dirname "$0")"
 
-FIND_BASE_URL=http://localhost:8080 \
+FIND_BASE_URL=http://10.2.21.91:8080 \
+    MMAP_PROXY=http://web-proxy.uk.softwaregrp.net:8080 \
     MMAP_BASE_URL=http://demo.havendemo.com/mmap_1 \
     node "$project_dir"/backend/build-data &&
     cd "$project_dir"/frontend &&
