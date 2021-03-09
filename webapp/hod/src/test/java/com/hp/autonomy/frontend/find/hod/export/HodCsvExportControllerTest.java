0f2d670a441b55183f2d0e71b12d38fb8f490a94
Joseph Lansdowne <joseph.lansdowne@microfocus.com>
2021-03-09 12:29:42 +0000

    remove hod

--- webapp/hod/src/test/java/com/hp/autonomy/frontend/find/hod/export/HodCsvExportControllerTest.java
+++
@@ -1,7 +0,0 @@
-package com.hp.autonomy.frontend.find.hod.export;
-
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest(classes = HodCsvExportController.class, webEnvironment = SpringBootTest.WebEnvironment.NONE)
-public class HodCsvExportControllerTest extends HodExportControllerTest {
-}
