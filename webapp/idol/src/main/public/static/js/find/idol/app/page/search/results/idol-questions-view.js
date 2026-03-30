651a7b9841487963284c70f3c3a282749170527f
Joseph Lansdowne <jlansdowne@opentext.com>
2026-03-30 12:03:22 +0100

    [US613003] improve answer rendering
    - allow additional tags: ul, li, p, div
    - fix some bugs
    - share code between ask/conversation

--- webapp/idol/src/main/public/static/js/find/idol/app/page/search/results/idol-questions-view.js
+++ webapp/idol/src/main/public/static/js/find/idol/app/page/search/results/idol-questions-view.js
@@ -16,11 +16,12 @@ define([
     'underscore',
     'jquery',
     'backbone',
+    'find/idol/app/util/html',
     'find/idol/app/model/answer-bank/idol-answered-questions-collection',
     'js-whatever/js/list-view',
     'text!find/templates/app/page/search/results/questions-container.html',
     'i18n!find/nls/bundle'
-], function(_, $, Backbone, AnsweredQuestionsCollection, ListView, questionsTemplate, i18n) {
+], function(_, $, Backbone, HtmlUtil, AnsweredQuestionsCollection, ListView, questionsTemplate, i18n) {
     'use strict';
 
     const MAX_SIZE = 1;
@@ -28,50 +29,6 @@ define([
 
     function isLink(value) {
         return value && /^\s*https?:\/\/.+/.exec(value);
-    }
-
-    function autoLink(value) {
-        // Automatically convert plain HTTP/HTTPS links to <a> tags.
-        // We use lookahead to ignore the trailing 'dot' if present, since that's placed as punctuation in an
-        //  answer server response.
-        const regex = /(https?:\/\/\S+(?=\.?(\s|$)))/gi;
-
-        let lastIndex = 0, match, escaped = '';
-        while (match = regex.exec(value)) {
-            escaped += _.escape(value.slice(lastIndex, match.index));
-
-            const url = match[1];
-            escaped += '<a href="' + _.escape(url) + '" target="_blank">' + _.escape(url) + '</a>'
-
-            lastIndex = match.index + match[0].length
-        }
-
-        escaped += _.escape(value.slice(lastIndex));
-
-        return escaped;
-    }
-
-    function allowLinks(value) {
-        if (!value) {
-            return value;
-        }
-
-        let escaped = '';
-
-        const regex = /<a\s+href=(['"]?[^'"<>]+['"]?)\s*(?:target="_blank"\s*)?>([^<>]*)<\/a>/g;
-
-        let lastIndex = 0, match;
-        while (match = regex.exec(value)) {
-            escaped += autoLink(value.slice(lastIndex, match.index));
-
-            escaped += '<a href=' + match[1] + ' target="_blank">' + match[2] + '</a>';
-
-            lastIndex = match.index + match[0].length
-        }
-
-        escaped += autoLink(value.slice(lastIndex));
-
-        return escaped;
     }
 
     return Backbone.View.extend({
@@ -113,7 +70,7 @@ define([
                     croppedAnswer: croppedAnswer,
                     extendedAnswer: extendedAnswer,
                     showMoreButton: answeredQuestion.get('answer').length > CROPPED_SUMMARY_CHAR_LENGTH,
-                    allowLinks: allowLinks,
+                    sanitiseHTML: HtmlUtil.sanitiseHTML,
                     isLink: isLink
                 });
             }, this).join('');
